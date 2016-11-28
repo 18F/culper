ce3f7296a302a67bff721bbb79bce93ab71e06f0
Bryan Allred <bryan.allred@gmail.com>
2016-11-28 15:49:53 -0500

    truetandem/e-QIP-prototype#36 Saving point for Date component

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -3,4 +3,5 @@ import Textarea from './Textarea'
 import Dropdown from './Dropdown'
 import Checkbox from './Checkbox'
 import Radio from './Radio'
-export { Text, Textarea, Dropdown, Checkbox, Radio }
+import Date from './Date'
+export { Text, Textarea, Dropdown, Checkbox, Radio, Date }
