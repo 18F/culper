2e72fb7d7cab9209d08cb97ef8d0aec56436ebf3
Alan deLevie <adelevie@gmail.com>
2016-11-30 15:57:34 -0500

    Move before-deploy scripts into .travis.yml

--- bin/before-deploy.sh
+++
@@ -1,9 +0,0 @@
-echo "Running before-deploy script"
-
-# npm install
-# npm run build
-
-export PATH=$HOME:$PATH
-curl -L -o $HOME/cf.tgz "https://cli.run.pivotal.io/stable?release=linux64-binary&version=6.15.0"
-tar xzvf $HOME/cf.tgz -C $HOME
-cf install-plugin autopilot -f -r CF-Community
