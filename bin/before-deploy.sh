aeab7a9ec2a166b78ee613dbf15677bfbf285f26
Alan deLevie <adelevie@gmail.com>
2016-11-30 15:26:03 -0500

    Removing npm install in before-deploy

--- bin/before-deploy.sh
+++ bin/before-deploy.sh
@@ -1,7 +1,7 @@
 echo "Running before-deploy script"
 
-npm install
-npm run build
+# npm install
+# npm run build
 
 export PATH=$HOME:$PATH
 curl -L -o $HOME/cf.tgz "https://cli.run.pivotal.io/stable?release=linux64-binary&version=6.15.0"
