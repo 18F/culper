3fb97cd97f06fe4518ac48a1a368542f4966742e
Bryan Allred <bryan.allred@gmail.com>
2017-01-03 12:39:45 -0500

    Telephone code coverage

---
+++ src/components/Form/Telephone/Telephone.test.jsx
@@ -0,0 +1,145 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import Telephone from './Telephone'
+
+describe('The URL component', () => {
+  it('renders appropriately with an error', () => {
+    const expected = {
+      name: 'input-error',
+      label: 'Telephone input error',
+      help: 'Helpful error message',
+      type: 'text',
+      error: true,
+      focus: false,
+      valid: false,
+      readonly: true
+    }
+    const component = mount(<Telephone name={expected.name} label={expected.label} help={expected.help} error={expected.error} focus={expected.focus} valid={expected.valid} readonly={expected.readonly} />)
+    expect(component.find('label.usa-input-error-label').text()).toEqual(expected.label)
+    expect(component.find('input#' + expected.name).length).toEqual(1)
+    expect(component.find('span.usa-input-error-message').text()).toEqual(expected.help)
+    expect(component.find('span.hidden').length).toEqual(0)
+  })
+
+  it('renders appropriately with focus', () => {
+    const expected = {
+      name: 'input-focus',
+      label: 'Telephone input focused',
+      help: 'Helpful error message',
+      type: 'text',
+      error: false,
+      focus: true,
+      valid: false
+    }
+    const component = mount(<Telephone name={expected.name} label={expected.label} help={expected.help} error={expected.error} focus={expected.focus} valid={expected.valid} />)
+    expect(component.find('label').text()).toEqual(expected.label)
+    expect(component.find('input#' + expected.name).length).toEqual(1)
+    expect(component.find('input#' + expected.name).hasClass('usa-input-focus')).toEqual(true)
+    expect(component.find('span.hidden').length).toEqual(1)
+  })
+
+  it('renders appropriately with validity checks', () => {
+    const expected = {
+      name: 'input-success',
+      label: 'Telephone input success',
+      help: 'Helpful error message',
+      type: 'text',
+      error: false,
+      focus: false,
+      valid: true
+    }
+    const component = mount(<Telephone name={expected.name} label={expected.label} help={expected.help} error={expected.error} focus={expected.focus} valid={expected.valid} />)
+    expect(component.find('label').text()).toEqual(expected.label)
+    expect(component.find('input#' + expected.name).length).toEqual(1)
+    expect(component.find('input#' + expected.name).hasClass('usa-input-success')).toEqual(true)
+    expect(component.find('span.hidden').length).toEqual(1)
+  })
+
+  it('renders sane defaults', () => {
+    const expected = {
+      name: 'input-type-text',
+      label: 'Telephone input label',
+      help: 'Helpful error message',
+      type: 'text',
+      error: false,
+      focus: false,
+      valid: false
+    }
+    const component = mount(<Telephone name={expected.name} label={expected.label} help={expected.help} error={expected.error} focus={expected.focus} valid={expected.valid} />)
+    expect(component.find('label').text()).toEqual(expected.label)
+    expect(component.find('input#' + expected.name).length).toEqual(1)
+    expect(component.find('span.hidden').length).toEqual(1)
+  })
+
+  it('bubbles up validate event', () => {
+    let validations = 0
+    const expected = {
+      name: 'input-error',
+      label: 'Text input error',
+      help: 'Helpful error message',
+      error: true,
+      focus: false,
+      valid: false,
+      handleValidation: function (event) {
+        validations++
+      }
+    }
+    const component = mount(<Telephone name={expected.name} onValidate={expected.handleValidation} />)
+    component.find('input').first().simulate('change')
+    expect(validations > 0).toEqual(true)
+  })
+
+  it('bubbles up change event', () => {
+    let changes = 0
+    const expected = {
+      name: 'input-error',
+      label: 'Text input error',
+      help: 'Helpful error message',
+      error: true,
+      focus: false,
+      valid: false,
+      handleChange: function (event) {
+        changes++
+      }
+    }
+    const component = mount(<Telephone name={expected.name} onChange={expected.handleChange} />)
+    component.find('input').first().simulate('change')
+    expect(changes).toEqual(1)
+  })
+
+  it('bubbles up focus event', () => {
+    let foci = 0
+    const expected = {
+      name: 'input-error',
+      label: 'Text input error',
+      help: 'Helpful error message',
+      error: true,
+      focus: false,
+      valid: false,
+      handleFocus: function (event) {
+        foci++
+      }
+    }
+    const component = mount(<Telephone name={expected.name} onFocus={expected.handleFocus} />)
+    component.find('input').first().simulate('focus')
+    expect(foci).toEqual(1)
+  })
+
+  it('bubbles up blur event', () => {
+    let blurs = 0
+    const expected = {
+      name: 'input-error',
+      label: 'Text input error',
+      help: 'Helpful error message',
+      error: true,
+      focus: false,
+      valid: false,
+      handleBlur: function (event) {
+        blurs++
+      }
+    }
+    const component = mount(<Telephone name={expected.name} onBlur={expected.handleBlur} />)
+    component.find('input').first().simulate('blur')
+    expect(blurs).toEqual(1)
+  })
+})
