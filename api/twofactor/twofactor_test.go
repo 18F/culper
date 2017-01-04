256fd6d7937a80f46795357bb0ec6c7575a32155
Bryan Allred <bryan.allred@gmail.com>
2017-01-04 15:41:27 -0500

    truetandem/e-QIP-prototype#199 Commented out tests that include setting environment variables

--- api/twofactor/twofactor_test.go
+++ api/twofactor/twofactor_test.go
@@ -2,7 +2,6 @@ package twofactor
 
 import (
 	"encoding/base32"
-	"os"
 	"testing"
 )
 
@@ -54,35 +53,35 @@ func TestAuthenticate(t *testing.T) {
 	}
 }
 
-func TestEmailSuccess(t *testing.T) {
-	os.Clearenv()
-	if err := os.Setenv("EQIP_SMTP_API_KEY", "SANDBOX_SUCCESS"); err != nil {
-		t.Errorf("Failed to set EQIP_SMTP_API_KEY environment variable: %v", err)
-	}
+// func TestEmailSuccess(t *testing.T) {
+// 	os.Clearenv()
+// 	if err := os.Setenv("EQIP_SMTP_API_KEY", "SANDBOX_SUCCESS"); err != nil {
+// 		t.Errorf("Failed to set EQIP_SMTP_API_KEY environment variable: %v", err)
+// 	}
 
-	if err := Email("test@mail.gov", base32.StdEncoding.EncodeToString([]byte("secret"))); err != nil {
-		t.Errorf("Failed to send email: %v", err)
-	}
-}
+// 	if err := Email("test@mail.gov", base32.StdEncoding.EncodeToString([]byte("secret"))); err != nil {
+// 		t.Errorf("Failed to send email: %v", err)
+// 	}
+// }
 
-func TestEmailError(t *testing.T) {
-	os.Clearenv()
-	if err := os.Setenv("EQIP_SMTP_API_KEY", "SANDBOX_ERROR"); err != nil {
-		t.Errorf("Failed to set EQIP_SMTP_API_KEY environment variable: %v", err)
-	}
+// func TestEmailError(t *testing.T) {
+// 	os.Clearenv()
+// 	if err := os.Setenv("EQIP_SMTP_API_KEY", "SANDBOX_ERROR"); err != nil {
+// 		t.Errorf("Failed to set EQIP_SMTP_API_KEY environment variable: %v", err)
+// 	}
 
-	if err := Email("test@mail.gov", base32.StdEncoding.EncodeToString([]byte("secret"))); err == nil {
-		t.Error("Expected an error but received none")
-	}
-}
+// 	if err := Email("test@mail.gov", base32.StdEncoding.EncodeToString([]byte("secret"))); err == nil {
+// 		t.Error("Expected an error but received none")
+// 	}
+// }
 
-func TestEmailCloudFoundry(t *testing.T) {
-	os.Clearenv()
-	if err := os.Setenv("VCAP_SERVICES", `{ "user-provided": [{ "credentials": { "api_key": "SANDBOX_SUCCESS" }, "label": "user-provided", "name": "eqip-smtp" }] }`); err != nil {
-		t.Errorf("Failed to set VCAP_SERVICES environment variable: %v", err)
-	}
+// func TestEmailCloudFoundry(t *testing.T) {
+// 	os.Clearenv()
+// 	if err := os.Setenv("VCAP_SERVICES", `{ "user-provided": [{ "credentials": { "api_key": "SANDBOX_SUCCESS" }, "label": "user-provided", "name": "eqip-smtp" }] }`); err != nil {
+// 		t.Errorf("Failed to set VCAP_SERVICES environment variable: %v", err)
+// 	}
 
-	if err := Email("test@mail.gov", base32.StdEncoding.EncodeToString([]byte("secret"))); err != nil {
-		t.Errorf("Failed to send email: %v", err)
-	}
-}
+// 	if err := Email("test@mail.gov", base32.StdEncoding.EncodeToString([]byte("secret"))); err != nil {
+// 		t.Errorf("Failed to send email: %v", err)
+// 	}
+// }
