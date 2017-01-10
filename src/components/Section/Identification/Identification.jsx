838cbdb0d5eb2deb1ec16f03f583f7ea0523ac3c
Joseph Wilson <joe@ignition72.com>
2017-01-10 08:29:17 -0500

    In progress styling changes, focused on form field structure / styling

--- src/components/Section/Identification/Identification.jsx
+++ src/components/Section/Identification/Identification.jsx
@@ -102,33 +102,17 @@ class Identification extends React.Component {
     const subsection = this.props.subsection
     if (!subsection) {
       return (
-        <div className="identification">
-          <div id="titles" className="usa-grid-full">
-            <div className="usa-width-one-half">
+        <div className="identification usa-grid-full">
+            <div id="section-tour" className="usa-width-one-half">
               <h3>One piece at a time</h3>
-            </div>
-            <div className="usa-width-one-half">
-              <h3>Full section view</h3>
-            </div>
-          </div>
-
-          <div id="dialogs" className="usa-grid-full">
-            <div className="usa-width-one-half">
               <p>Take a guided tour through the section</p>
-            </div>
-            <div className="usa-width-one-half">
-              <p>View all the sections associated with <strong>Identification</strong> at once</p>
-            </div>
-          </div>
-
-          <div id="actions" className="usa-grid-full">
-            <div className="usa-width-one-half">
               <button onClick={this.handleTour}>Take me on the tour!</button>
             </div>
-            <div className="usa-width-one-half">
+            <div id="section-full" className="usa-width-one-half">
+              <h3>Full section view</h3>
+              <p>View all the sections associated with <strong>Identification</strong> at once</p>
               <button onClick={this.handleReview}>Show me the full section</button>
             </div>
-          </div>
         </div>
       )
     }
