b4b2bc8d767d40e6f4c7c95a456aefd71c19dd71
John Flores <johnsflores@gmail.com>
2017-01-17 13:47:44 -0500

    Updated passport test

--- src/components/Section/Foreign/Passport/Passport.test.jsx
+++ src/components/Section/Foreign/Passport/Passport.test.jsx
@@ -21,7 +21,6 @@ describe('The passport component', () => {
     }
     const component = mount(<Passport name={expected.name} HasPassport="Yes" />)
     expect(component.find('input[name="has_passport"]').length).toEqual(2)
-    expect(component.find('input#first').length).toEqual(1)
     expect(component.find('input#number').length).toEqual(1)
     expect(component.find('input#month').length).toEqual(2)
     expect(component.find('div.hidden').length).toBeGreaterThan(0)
