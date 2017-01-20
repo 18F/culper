cf66d69bb572d1a76561fac50b42488ee5d6f0d6
John Flores <johnsflores@gmail.com>
2017-01-20 13:39:24 -0500

    truetandem/e-QIP-prototype#294 Updated login and 2factor page to reflect design comps. Added OPM seal

--- src/boot.jsx
+++ src/boot.jsx
@@ -15,10 +15,10 @@ ReactDOM.render(
     <Router history={hashHistory}>
       <Route path="/" component={App} onEnter={onEnter}>
         <IndexRoute component={Home} />
-        <Route path="/login" component={Login} />
         <Route path="/help" component={Help} />
         <Route path="/form(/:section(/:subsection))" component={Form} />
       </Route>
+      <Route path="/login" component={Login} />
     </Router>
   </Provider>
     , app)
