6a0e7a2edf6e49477d1606acd7ebe814013be8b4
John Flores <johnsflores@gmail.com>
2017-02-23 15:08:42 -0500

    truetandem/e-QIP-prototype#441 Added contact information validator

--- src/validators/index.js
+++ src/validators/index.js
@@ -2,9 +2,11 @@ import PhysicalValidator from './physical'
 import NameValidator from './name'
 import OtherNamesValidator from './othernames'
 import BirthPlaceValidator from './birthplace'
+import ContactInformationValidator from './contactinformation'
 
 export {
   BirthPlaceValidator,
+  ContactInformationValidator,
   NameValidator,
   OtherNamesValidator,
   PhysicalValidator
