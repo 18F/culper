e24f4d13868e6c428991c55dcf70d88cf959a91c
Bryan Allred <bryan.allred@gmail.com>
2017-04-05 09:36:43 -0400

    truetandem/e-QIP-prototype#801 Card abuse details

---
+++ src/components/Section/Financial/Card/Card.jsx
@@ -0,0 +1,202 @@
+import React from 'react'
+import { i18n } from '../../../../config'
+import { CardAbuseValidator } from '../../../../validators'
+import { ValidationElement, Branch, Show, Accordion, DateControl, Number, Help, HelpIcon,
+         Address, Checkbox, Text, Textarea } from '../../../Form'
+
+export default class Card extends ValidationElement {
+  constructor (props) {
+    super(props)
+    this.state = {
+      HasCardAbuse: props.HasCardAbuse,
+      List: props.List,
+      errorCodes: []
+    }
+
+    this.updateBranch = this.updateBranch.bind(this)
+    this.updateList = this.updateList.bind(this)
+    this.summary = this.summary.bind(this)
+  }
+
+  /**
+   * Handle the validation event.
+   */
+  handleValidation (event, status, error) {
+    if (!event) {
+      return
+    }
+
+    let codes = super.mergeError(this.state.errorCodes, super.flattenObject(error))
+    let complexStatus = null
+    if (codes.length > 0) {
+      complexStatus = false
+    } else if (this.isValid()) {
+      complexStatus = true
+    }
+
+    this.setState({error: complexStatus === false, valid: complexStatus === true, errorCodes: codes}, () => {
+      const errorObject = { [this.props.name]: codes }
+      const statusObject = { [this.props.name]: { status: complexStatus } }
+      super.handleValidation(event, statusObject, errorObject)
+    })
+  }
+
+  /**
+   * Determine if all items in the collection are considered to be in
+   * a valid state.
+   */
+  isValid () {
+    return new CardAbuseValidator(this.state, null).isValid()
+  }
+
+  /**
+   * Updates triggered by the branching component.
+   */
+  updateBranch (val, event) {
+    this.setState({ HasCardAbuse: val }, () => {
+      this.updateList(val === 'No' ? [] : this.state.List)
+      this.handleValidation(event, null, null)
+    })
+  }
+
+  /**
+   * Dispatch callback initiated from the collection to notify of any new
+   * updates to the items.
+   */
+  updateList (collection) {
+    this.setState({ List: collection }, () => {
+      if (this.props.onUpdate) {
+        this.props.onUpdate({
+          List: this.state.List,
+          HasCardAbuse: this.state.HasCardAbuse
+        })
+      }
+    })
+  }
+
+  /**
+   * Assists in rendering the summary section.
+   */
+  summary (item, index) {
+    const obj = (item || {})
+    const agency = (obj.Agency || {}).value || i18n.t('financial.card.collection.summary.unknown')
+    const year = (obj.Year || {}).value || ''
+
+    return (
+      <span>
+        <span className="index">{i18n.t('financial.card.collection.summary.item')} {index + 1}:</span>
+        <span><strong>{agency}</strong></span>
+        <span className="dates">{year}</span>
+      </span>
+    )
+  }
+
+  render () {
+    return (
+      <div className="card-abuse">
+        <Branch name="has_cardabuse"
+                className="card-branch eapp-field-wrap no-label"
+                value={this.state.HasCardAbuse}
+                help="financial.card.help.branch"
+                onUpdate={this.updateBranch}>
+        </Branch>
+        <Show when={this.state.HasCardAbuse === 'Yes'}>
+          <Accordion minimum="1"
+                     items={this.state.List}
+                     onUpdate={this.updateList}
+                     onValidate={this.handleValidation}
+                     summary={this.summary}
+                     description={i18n.t('financial.card.collection.summary.title')}
+                     appendTitle={i18n.t('financial.card.collection.appendTitle')}
+                     appendMessage={i18n.m('financial.card.collection.appendMessage')}
+                     appendLabel={i18n.t('financial.card.collection.append')}>
+
+            <h3>{i18n.t('financial.card.heading.agency')}</h3>
+            <div className="eapp-field-wrap no-label">
+              <Help id="financial.card.help.agency">
+                <Text name="Agency"
+                      className="card-agency"
+                      bind={true}
+                      />
+                <HelpIcon />
+              </Help>
+            </div>
+
+            <h3>{i18n.t('financial.card.heading.address')}</h3>
+            <div className="eapp-field-wrap no-label">
+              <Help id="financial.card.help.address">
+                <Address name="Address"
+                         className="card-address"
+                         bind={true}
+                         />
+                <HelpIcon />
+              </Help>
+            </div>
+
+            <h3>{i18n.t('financial.card.heading.date')}</h3>
+            <div className="eapp-field-wrap">
+              <Help id="financial.card.help.date">
+                <DateControl name="Date"
+                             className="card-date"
+                             hideDay={true}
+                             bind={true}
+                             />
+                <HelpIcon />
+              </Help>
+            </div>
+
+            <h3>{i18n.t('financial.card.heading.reason')}</h3>
+            <div className="eapp-field-wrap no-label">
+              <Help id="financial.card.help.reason">
+                <Textarea name="Reason"
+                          className="card-reason"
+                          bind={true}
+                          />
+                <HelpIcon />
+              </Help>
+            </div>
+
+            <h3>{i18n.t('financial.card.heading.amount')}</h3>
+            <div className="eapp-field-wrap no-label">
+              <Help id="financial.card.help.amount">
+                <i className="fa fa-dollar"></i>
+                <Number name="Amount"
+                        className="card-amount"
+                        placeholder={i18n.t('financial.card.placeholder.amount')}
+                        min="1"
+                        bind={true}
+                        />
+                <HelpIcon />
+                <div className="card-amount coupled-flags">
+                  <Checkbox name="AmountEstimated"
+                            ref="estimated"
+                            label={i18n.t('financial.card.label.estimated')}
+                            toggle="false"
+                            bind={true}
+                            />
+                </div>
+              </Help>
+            </div>
+
+            <h3>{i18n.t('financial.card.heading.description')}</h3>
+            <div className="eapp-field-wrap no-label">
+              <Help id="financial.card.help.description">
+                <Textarea name="Description"
+                          className="card-description"
+                          bind={true}
+                          />
+                <HelpIcon />
+              </Help>
+            </div>
+
+          </Accordion>
+        </Show>
+      </div>
+    )
+  }
+}
+
+Card.defaultProps = {
+  HasCardAbuse: '',
+  List: []
+}
