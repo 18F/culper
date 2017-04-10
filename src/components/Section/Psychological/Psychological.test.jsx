9f1920d91a6240a395c7036f07f61740e5e2ce30
John Flores <johnsflores@gmail.com>
2017-04-10 18:50:22 -0400

    Updated psychological section tests

--- src/components/Section/Psychological/Psychological.test.jsx
+++ src/components/Section/Psychological/Psychological.test.jsx
@@ -40,4 +40,41 @@ describe('The legal section', () => {
     component.find('.competence .yes input').simulate('change')
     expect(component.find('.competence .accordion').length).toBe(1)
   })
+
+  it('can complete section', () => {
+    const store = mockStore({ authentication: { authenticated: true, twofactor: true } })
+    const component = mount(<Provider store={store}><Psychological subsection="review" /></Provider>)
+    expect(component.find('div').length).toBeGreaterThan(0)
+    component.find('.competence .no input').simulate('change')
+    component.find('.competence .no input').simulate('blur')
+    expect(component.find('.competence .accordion').length).toBe(0)
+
+    component.find('.consultation .no input').simulate('change')
+    component.find('.consultation .no input').simulate('blur')
+    expect(component.find('.consultation .accordion').length).toBe(0)
+
+    component.find('.hospitalizations .no input').simulate('change')
+    component.find('.hospitalizations .no input').simulate('blur')
+    expect(component.find('.hospitalizations .accordion').length).toBe(0)
+
+    component.find('.diagnosed .no input').simulate('change')
+    component.find('.diagnosed .no input').simulate('blur')
+    expect(component.find('.diagnoses .accordion').length).toBe(0)
+
+    component.find('.didnotconsult .no input').simulate('change')
+    component.find('.didnotconsult .no input').simulate('blur')
+    component.find('.intreatment .no input').simulate('change')
+    component.find('.intreatment .no input').simulate('blur')
+    expect(component.find('.diagnoses .accordion').length).toBe(0)
+
+    component.find('.hascondition .no input').simulate('change')
+    component.find('.hascondition .no input').simulate('blur')
+    component.find({name: 'treatment', value: 'Decline'}).simulate('change')
+    component.find({name: 'treatment', value: 'Decline'}).simulate('blur')
+    expect(component.find('.existingconditions .accordion').length).toBe(0)
+
+    component.find('.didnotfollow .no input').simulate('change')
+    component.find('.didnotfollow .no input').simulate('blur')
+    expect(component.find('textarea#DidNotFollowExplanation').length).toBe(0)
+  })
 })
