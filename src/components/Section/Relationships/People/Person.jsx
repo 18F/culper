8a3ea02d40d2a26cd289d11f4c1908c5e78c7ead
John Flores <johnsflores@gmail.com>
2017-04-19 16:20:12 -0400

    Updated Person component and unit test

--- src/components/Section/Relationships/People/Person.jsx
+++ src/components/Section/Relationships/People/Person.jsx
@@ -1,24 +1,36 @@
 import React from 'react'
 import { i18n } from '../../../../config'
-import { ValidationElement, Branch, Name, DateRange, Field, Textarea } from '../../../Form'
+import { Name, DateRange, Field, NotApplicable, Telephone, Email, Text, Address } from '../../../Form'
 
-export default class Person extends ValidationElement {
+export default class Person extends React.Component {
   constructor (props) {
     super(props)
 
     this.state = {
       Name: props.Name,
       KnownDates: props.KnownDates,
-      Rank: props.Rank
+      Rank: props.Rank,
+      RankNotApplicable: props.RankNotApplicable,
+      MobileTelephone: props.MobileTelephone,
+      OtherTelephone: props.OtherTelephone,
+      Email: props.Email,
+      EmailNotApplicable: props.EmailNotApplicable,
+      Address: props.Address
     }
 
-    this.onUpdate = this.onUpdate.bind(this)
+    this.update = this.update.bind(this)
     this.updateName = this.updateName.bind(this)
     this.updateKnownDates = this.updateKnownDates.bind(this)
-    this.updateRank = this.updateKnownDates.bind(this)
+    this.updateRank = this.updateRank.bind(this)
+    this.updateRankNotApplicable = this.updateRankNotApplicable.bind(this)
+    this.updateMobileTelephone = this.updateMobileTelephone.bind(this)
+    this.updateOtherTelephone = this.updateOtherTelephone.bind(this)
+    this.updateEmail = this.updateEmail.bind(this)
+    this.updateEmailNotApplicable = this.updateEmailNotApplicable.bind(this)
+    this.updateAddress = this.updateAddress.bind(this)
   }
 
-  onUpdate (name, values, fn) {
+  update (name, values, fn) {
     this.setState({ [name]: values }, () => {
       if (this.props.onUpdate) {
         this.props.onUpdate({
@@ -30,7 +42,7 @@ export default class Person extends ValidationElement {
   }
 
   updateName (values) {
-    this.onUpdate('Name', values)
+    this.update('Name', values)
   }
 
   updateKnownDates (values) {
@@ -41,10 +53,34 @@ export default class Person extends ValidationElement {
     this.update('Rank', values)
   }
 
+  updateRankNotApplicable (values) {
+    this.update('RankNotApplicable', values)
+  }
+
+  updateMobileTelephone (values) {
+    this.update('MobileTelephone', values)
+  }
+
+  updateOtherTelephone (values) {
+    this.update('OtherTelephone', values)
+  }
+
+  updateEmail (values) {
+    this.update('Email', values)
+  }
+
+  updateEmailNotApplicable (values) {
+    this.update('EmailNotApplicable', values)
+  }
+
+  updateAddress (values) {
+    this.update('Address', values)
+  }
+
   render () {
     return (
       <div className="person">
-        <Field title={i18n.t('relationships.person.heading.alias.title')}>
+        <Field title={i18n.t('relationships.person.heading.knownDates')}>
           {i18n.m('relationships.relatives.para.alias')}
           <DateRange name="KnownDates"
             className="known-dates"
@@ -54,21 +90,68 @@ export default class Person extends ValidationElement {
           />
         </Field>
 
-        <Field title={i18n.t('relationships.person.heading.alias.title')}>
-          {i18n.m('relationships.relatives.para.alias')}
+        <Field title={i18n.t('relationships.person.heading.name')}>
           <Name name="Name"
-            className="alias-name"
+            className="name"
             {...this.state.Name}
             onUpdate={this.updateName}
             onValidate={this.props.onValidate}
           />
         </Field>
 
-        <Field title={i18n.t('relationships.person.heading.rank.title')}>
-          <Text name="Rank"
-            className="rank"
-            {...this.state.Rank}
-            onUpdate={this.updateRank}
+        <Field title={i18n.t('relationships.person.heading.rank')}>
+          <NotApplicable name="RankNotApplicable"
+            className="rank-notapplicable"
+            {...this.state.RankNotApplicable}
+            label={i18n.t('relationships.person.label.rankNotApplicable')}
+            or={i18n.m('relationships.person.label.or')}
+            onUpdate={this.updateRankNotApplicable}>
+            <Text name="Rank"
+              className="rank"
+              {...this.state.Rank}
+              onUpdate={this.updateRank}
+              onValidate={this.props.onValidate}
+            />
+          </NotApplicable>
+        </Field>
+
+        <Field title={i18n.t('relationships.person.heading.mobileTelephone')} className="mobile-telephone">
+          <Telephone name="MobileTelephone"
+            {...this.state.MobileTelephone}
+            onUpdate={this.updateMobileTelephone}
+            onValidate={this.props.onValidate}
+          />
+        </Field>
+
+        <Field title={i18n.t('relationships.person.heading.otherTelephone')} className="other-telephone">
+          <Telephone name="OtherTelephone"
+            {...this.state.OtherTelephone}
+            onUpdate={this.updateOtherTelephone}
+            onValidate={this.props.onValidate}
+          />
+        </Field>
+
+        <Field title={i18n.t('relationships.person.heading.email')}>
+          <NotApplicable name="EmailNotApplicable"
+            className="email-notapplicable"
+            {...this.state.EmailNotApplicable}
+            label={i18n.t('relationships.person.label.emailNotApplicable')}
+            or={i18n.m('relationships.person.label.or')}
+            onUpdate={this.updateEmailNotApplicable}>
+            <Email name="Email"
+              className="email"
+              {...this.state.Email}
+              onUpdate={this.updateEmail}
+              onValidate={this.props.onValidate}
+            />
+          </NotApplicable>
+        </Field>
+
+        <Field title={i18n.t('relationships.person.heading.address')}>
+          <Address name="Address"
+            className="address"
+            {...this.state.Address}
+            onUpdate={this.updateAddress}
             onValidate={this.props.onValidate}
           />
         </Field>
--- src/components/Section/Relationships/People/Person.test.jsx
+++ src/components/Section/Relationships/People/Person.test.jsx
@@ -9,6 +9,29 @@ describe('The relative alias component', () => {
     }
 
     const component = mount(<Person {...expected} />)
-    expect(component.find('.alias-name').length).toEqual(1)
+    expect(component.find('.person').length).toEqual(1)
+  })
+
+  it('updates values', () => {
+    let updates = 0
+    const expected = {
+      name: 'person',
+      onUpdate: () => {
+        updates++
+      }
+    }
+
+    const component = mount(<Person {...expected} />)
+    expect(component.find('.person').length).toEqual(1)
+    component.find('.known-dates .from input#month').simulate('change', { target: { value: '10' } })
+    component.find('.name input[name="first"]').simulate('change')
+    component.find('.rank input').simulate('change')
+    component.find('.rank-notapplicable .button input').simulate('change')
+    component.find('.mobile-telephone .home input').simulate('change')
+    component.find('.other-telephone .home input').simulate('change')
+    component.find('.email input[name="Email"]').simulate('change')
+    component.find('.email-notapplicable .button input').simulate('change')
+    component.find('.address input[name="city"]').simulate('change')
+    expect(updates).toBe(9)
   })
 })
