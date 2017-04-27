3aff65408f5b1e3e31066a6a421943711e05e857
John Flores <johnsflores@gmail.com>
2017-04-27 13:38:23 -0400

    truetandem/e-QIP-prototype#1038 Updated checkbox hit area

--- src/components/Form/Checkbox/Checkbox.jsx
+++ src/components/Form/Checkbox/Checkbox.jsx
@@ -60,7 +60,7 @@ export default class Checkbox extends ValidationElement {
    * Style classes applied to the wrapper.
    */
   divClass () {
-    let klass = `${this.props.className || ''} block`
+    let klass = `${this.props.className || ''} checkbox block`
 
     if (this.props.children) {
       klass += ' extended'
