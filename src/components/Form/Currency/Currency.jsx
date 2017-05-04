c76ecf0381da5c8d288419e01665094f4ab2d4b0
John Flores <johnsflores@gmail.com>
2017-05-04 10:01:57 -0400

    Added some comments to currency

--- src/components/Form/Currency/Currency.jsx
+++ src/components/Form/Currency/Currency.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Number from '../Number'
 
+/**
+ * Wraps a Number component and adds the dollar icon. This is meant mostly for UI purposes. This can be a drop-in
+ * replacement for any current field being used for currency purposes. This simply just passes down the props.
+ */
 function CurrencyWrapper (WrappedComponent) {
   return class extends React.Component {
     render () {
