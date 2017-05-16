8cd2d878df8d19662441c9a5799c397900dbabf0
Bryan Allred <bryan.allred@gmail.com>
2017-05-16 12:32:10 -0400

    Change "Date" to "Date of outcome"

--- src/config/locales/en.js
+++ src/config/locales/en.js
@@ -6018,7 +6018,7 @@ const en = {
         courtaddress: 'Provide the location of the court',
         chargedetails: 'Provide all the charges brought against you for this offense, and the outcome of each charged offense',
         chargeType: 'Type of charge',
-        courtdate: 'Date',
+        courtdate: 'Date of outcome',
         sentenced: 'Were you sentenced as a result of this offense?',
         needmore: 'Since you answered yes we need more information',
         sentenceDescription: 'Provide a description of the sentence',
