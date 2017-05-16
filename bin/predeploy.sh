f710b8509e7d11be5c983f20f33d5830d11d94ee
Alan deLevie <adelevie@gmail.com>
2017-05-16 14:01:24 -0400

    Tidying up dev server environment

--- bin/predeploy.sh
+++ bin/predeploy.sh
@@ -2,16 +2,28 @@
 
 set -e
 
+SPACE=$1
+
 #
 # Determine what environment to configure based on the
 # originating repository.
 #
 # Reference: https://docs.travis-ci.com/user/environment-variables
 #
-if [ "$TRAVIS_REPO_SLUG" = "18F/e-QIP-prototype" ]; then
+# if [ "$TRAVIS_REPO_SLUG" = "18F/e-QIP-prototype" ]; then
+#   ENV_FILE=".env.production"
+# elif [ "$TRAVIS_REPO_SLUG" = "truetandem/e-QIP-prototype" ]; then
+#   ENV_FILE=".env.staging"
+# else
+#   ENV_FILE=".env.test"
+# fi
+
+if [ "$SPACE" = "production" ]; then
   ENV_FILE=".env.production"
-elif [ "$TRAVIS_REPO_SLUG" = "truetandem/e-QIP-prototype" ]; then
+elif [ "$SPACE" = "staging" ]; then
   ENV_FILE=".env.staging"
+elif [ "$SPACE" = "dev" ]; then
+  ENV_FILE=".env.dev"
 else
   ENV_FILE=".env.test"
 fi
