48bee84e0d2d8fb76aa96993bf5a8800a820402a
Jay Neighbours <jay@ignition72.com>
2017-05-19 11:25:02 -0400

    Update Taxes.jsx
    removing help in taxes

--- src/components/Section/Financial/Taxes/Taxes.jsx
+++ src/components/Section/Financial/Taxes/Taxes.jsx
@@ -103,7 +103,6 @@ export default class Taxes extends ValidationElement {
         <Branch name="has_taxes"
                 className="taxes-branch"
                 value={this.state.HasTaxes}
-                help="financial.taxes.help.branch"
                 onUpdate={this.updateBranch}>
         </Branch>
         <Show when={this.state.HasTaxes === 'Yes'}>
@@ -118,7 +117,6 @@ export default class Taxes extends ValidationElement {
                      appendLabel={i18n.t('financial.taxes.collection.append')}>
 
             <Field title={i18n.t('financial.taxes.heading.failure')}
-                   help="financial.taxes.help.failure"
                    adjustFor="buttons"
                    shrink={true}>
               <FailureType name="Failure"
@@ -127,8 +125,7 @@ export default class Taxes extends ValidationElement {
                            />
             </Field>
 
-            <Field title={i18n.t('financial.taxes.heading.year')}
-                   help="financial.taxes.help.year">
+            <Field title={i18n.t('financial.taxes.heading.year')}>
               <Number name="Year"
                       className="taxes-year"
                       placeholder={i18n.t('financial.taxes.placeholder.year')}
@@ -145,32 +142,28 @@ export default class Taxes extends ValidationElement {
               </div>
             </Field>
 
-            <Field title={i18n.t('financial.taxes.heading.reason')}
-                   help="financial.taxes.help.reason">
+            <Field title={i18n.t('financial.taxes.heading.reason')}>
               <Textarea name="Reason"
                         className="taxes-reason"
                         bind={true}
                         />
             </Field>
 
-            <Field title={i18n.t('financial.taxes.heading.agency')}
-                   help="financial.taxes.help.agency">
+            <Field title={i18n.t('financial.taxes.heading.agency')}>
               <Text name="Agency"
                     className="taxes-agency"
                     bind={true}
                     />
             </Field>
 
-            <Field title={i18n.t('financial.taxes.heading.taxtype')}
-                   help="financial.taxes.help.taxtype">
+            <Field title={i18n.t('financial.taxes.heading.taxtype')}>
               <Text name="TaxType"
                     className="taxes-taxtype"
                     bind={true}
                     />
             </Field>
 
-            <Field title={i18n.t('financial.taxes.heading.amount')}
-                   help="financial.taxes.help.amount">
+            <Field title={i18n.t('financial.taxes.heading.amount')}>
               <div>
                 <Currency name="Amount"
                           className="taxes-amount"
@@ -190,7 +183,6 @@ export default class Taxes extends ValidationElement {
             </Field>
 
             <Field title={i18n.t('financial.taxes.heading.date')}
-                   help="financial.taxes.help.date"
                    adjustFor="label"
                    shrink={true}>
               <NotApplicable name="DateNotApplicable"
@@ -205,8 +197,7 @@ export default class Taxes extends ValidationElement {
               </NotApplicable>
             </Field>
 
-            <Field title={i18n.t('financial.taxes.heading.description')}
-                   help="financial.taxes.help.description">
+            <Field title={i18n.t('financial.taxes.heading.description')}>
               <Textarea name="Description"
                         className="taxes-description"
                         bind={true}
