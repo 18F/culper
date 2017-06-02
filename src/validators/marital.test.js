7a79dd487dade3ed2361615a5f4a4769b426fd07
Bryan Allred <bryan.allred@gmail.com>
2017-06-02 15:43:53 -0400

    Coverage bump for MaritalValidator
    truetandem/e-QIP-prototype#1363

--- src/validators/marital.test.js
+++ src/validators/marital.test.js
@@ -126,6 +126,99 @@ describe('Marital validation', function () {
 
         },
         expected: false
+      },
+      {
+        state: {
+          Status: 'Annulled',
+          DivorcedList: [{Divorce: {}}],
+          DivorcedListBranch: ''
+        },
+        expected: false
+      },
+      {
+        state: {
+          Status: 'InCivilUnion',
+          DivorcedList: [{Divorce: {}}],
+          DivorcedListBranch: '',
+          CivilUnion: {
+            Name: {
+              first: 'Foo',
+              firstInitialOnly: false,
+              middle: 'J',
+              middleInitialOnly: true,
+              noMiddleName: false,
+              last: 'Bar',
+              lastInitialOnly: false,
+              suffix: 'Jr'
+            },
+            Birthdate: {
+              day: '1',
+              month: '1',
+              year: '2016',
+              date: new Date('1/1/2016')
+            },
+            BirthPlace: {
+              domestic: 'Yes',
+              country: 'United States',
+              city: 'Arlington',
+              county: 'Arlington',
+              state: 'VA'
+            },
+            Address: {
+              addressType: 'United States',
+              address: '1234 Some Rd',
+              city: 'Arlington',
+              state: 'Virginia',
+              zipcode: '22202'
+            },
+            Telephone: {
+              noNumber: '',
+              number: '7031112222',
+              numberType: 'Home',
+              timeOfDay: 'Both',
+              extension: ''
+            },
+            SSN: {
+              first: '111',
+              middle: '11',
+              last: '1111',
+              applicable: true
+            },
+            Divorced: 'Yes',
+            Separated: 'No',
+            OtherNameNotApplicable: false,
+            OtherName: {
+              first: 'Foo',
+              firstInitialOnly: false,
+              middle: 'J',
+              middleInitialOnly: true,
+              noMiddleName: false,
+              last: 'Bar',
+              lastInitialOnly: false,
+              suffix: 'Jr'
+            },
+            DatesUsed: {
+              from: {
+                date: new Date('1/1/2010')
+              },
+              to: {
+                date: new Date('1/1/2016')
+              },
+              present: false
+            },
+            Citizenship: {
+              value: ['Germany', 'United States']
+            },
+            ForeignBornDocument: {
+              DocumentType: 'FS240',
+              DocumentExpirationNotApplicable: true,
+              DocumentNumber: {
+                value: 'A1234'
+              }
+            }
+          }
+        },
+        expected: false
       }
     ]
     tests.forEach(test => {
