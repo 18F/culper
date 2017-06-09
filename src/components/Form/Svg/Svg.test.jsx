64e626bacf5e4f317e2887c25d626cbe3d66f587
Bryan Allred <bryan.allred@gmail.com>
2017-06-09 08:17:09 -0400

    Fix Svg unit tests

--- src/components/Form/Svg/Svg.test.jsx
+++ src/components/Form/Svg/Svg.test.jsx
@@ -5,7 +5,7 @@ import Svg from './Svg'
 describe('The Svg component', () => {
   it('can render pixie dust', () => {
     const expected = {
-      src: 'img/eye.svg'
+      src: '/img/eye.svg'
     }
     const component = mount(<Svg src={expected.src} />)
     expect(component.find('svg').length).toEqual(1)
@@ -14,7 +14,7 @@ describe('The Svg component', () => {
 
   it('can render img', () => {
     const expected = {
-      src: 'img/eye-multicolor.svg'
+      src: '/img/eye-multicolor.svg'
     }
     const component = mount(<Svg src={expected.src} />)
     expect(component.find('svg').length).toEqual(0)
