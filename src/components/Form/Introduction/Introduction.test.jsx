005c98a22939bb43a9348ea70ecb916741df707c
Bryan Allred <bryan.allred@gmail.com>
2017-06-16 15:11:38 -0400

    Form introduction and term acceptance
    truetandem/e-QIP-prototype#652

---
+++ src/components/Form/Introduction/Introduction.test.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import MockAdapter from 'axios-mock-adapter'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { mount } from 'enzyme'
+import AuthenticatedIntroduction, { Introduction } from './Introduction'
+
+describe('The Introduction component', () => {
+  it('logout if "No" is selected', () => {
+    let dispatched = 0
+    const props = {
+      dispatch: () => { dispatched++ }
+    }
+    const component = mount(<Introduction {...props} />)
+    expect(component.find('.branch .no').length).toBe(1)
+    component.find('.branch .no input').simulate('change')
+    expect(dispatched).toBe(2)
+  })
+
+  it('dispatch fired on "Yes"', () => {
+    let dispatched = 0
+    const props = {
+      dispatch: () => { dispatched++ }
+    }
+    const component = mount(<Introduction {...props} />)
+    expect(component.find('.branch .yes').length).toBe(1)
+    component.find('.branch .yes input').simulate('change')
+    expect(dispatched).toBe(1)
+  })
+
+  it('display on authentication', () => {
+    const middlewares = [thunk]
+    const mockStore = configureMockStore(middlewares)
+    const store = mockStore({
+      authentication: {
+        authenticated: true,
+        twofactor: true
+      },
+      application: {
+        Settings: {
+          acceptedTerms: ''
+        }
+      }
+    })
+    const component = mount(<Provider store={store}><AuthenticatedIntroduction /></Provider>)
+    expect(component.find('.introduction-modal').length).toBe(1)
+  })
+
+  it('hidden if previously accepted', () => {
+    const middlewares = [thunk]
+    const mockStore = configureMockStore(middlewares)
+    const store = mockStore({
+      authentication: {
+        authenticated: true,
+        twofactor: true
+      },
+      application: {
+        Settings: {
+          acceptedTerms: 'Yes'
+        }
+      }
+    })
+    const component = mount(<Provider store={store}><AuthenticatedIntroduction /></Provider>)
+    expect(component.find('.introduction-modal').length).toBe(0)
+  })
+
+  it('hidden if not authenticated', () => {
+    const middlewares = [thunk]
+    const mockStore = configureMockStore(middlewares)
+    const store = mockStore({
+      authentication: {},
+      application: {
+        Settings: {
+          acceptedTerms: ''
+        }
+      }
+    })
+    const component = mount(<Provider store={store}><AuthenticatedIntroduction /></Provider>)
+    expect(component.find('.introduction-modal').length).toBe(0)
+  })
+})
--- src/views/Form/Form.jsx
+++ src/views/Form/Form.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { push } from '../../middleware/history'
 import AuthenticatedView from '../AuthenticatedView'
 import { Section, SavedIndicator } from '../../components'
+import { Introduction } from '../../components/Form'
 
 // The concept is that we have three different inputs:
 //  1. The index which just brings up the first entry of the form.
@@ -35,6 +36,7 @@ class Form extends React.Component {
     return (
       <div id="eapp-form" className="eapp-form">
         <div id="info">
+          <Introduction dispatch={this.props.dispatch} />
           <Section section={this.props.params.section} subsection={subsection} />
           <SavedIndicator interval="3000" />
         </div>
