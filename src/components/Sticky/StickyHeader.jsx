c6b6b4598c5a38a8731d1e8a3e6e08ae7519f066
John Flores <johnsflores@gmail.com>
2017-07-11 18:02:42 -0400

    Updated sticky header

--- src/components/Sticky/StickyHeader.jsx
+++ src/components/Sticky/StickyHeader.jsx
@@ -7,78 +7,47 @@ export default class StickyHeader extends React.Component {
     this.state = {
       stick: false
     }
-    this.originalHeight = 0
     this.onScroll = this.onScroll.bind(this)
   }
 
-  componentDidUpdate () {
-    if (!this.state.stick) {
-      this.initialTop = this.refs.content.offsetTop
-    }
-  }
-
-  componentReceiveProps () {
-    if (!this.state.stick) {
-      this.initialTop = this.refs.content.offsetTop
-    }
-  }
-
   componentDidMount () {
-    this.originalHeight = this.refs.content.offsetHeight
-    //this.initialTop = this.refs.content.getBoundingClientRect().top
-    this.initialTop = this.refs.content.offsetTop
-    //this.initialOffsetTop = this.refs.content.offsetTop
     this.props.events.forEach(e => window.addEventListener(e, this.onScroll))
+    this.onScroll()
   }
 
   componentWillUnmount () {
     this.props.events.forEach(e => window.removeEventListener(e, this.onScroll))
   }
 
-  onScroll (event) {
-    if (this.props.disabled) {
-      this.setState({
-        stick: false
-      })
-      return
-    }
-    const offset = this.props.offset
-    const rect = this.refs.content.getBoundingClientRect()
+  documentScrollTop () {
     const doc = document.documentElement
-    let st = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0) - offset
-    //let ot = rect.top
-    let ot = this.refs.content.offsetTop
+    let st = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0)
+    return st
+  }
 
+  elementOffset () {
+    const rect = this.refs.content.getBoundingClientRect()
+    const bodyRect = document.body.getBoundingClientRect()
+    const elOffset = rect.top - bodyRect.top
+    return elOffset + this.props.offset
+  }
+
+  onScroll (event) {
+    // Obtain document scroll top which is a representation of the position
+    // of the top of the page
+    let documentScrollTop = this.documentScrollTop()
+
+    // Obtain the position of the element
+    let elementOffset = this.elementOffset()
+
+    // Ensure that the top of the screen is within the bounds of the contents
+    // of the accordion. The top and bottom of the accordion content region are
+    // represented by elementOffset and elementBottomOffset respectively.
     let stick = this.state.stick
-    //console.log('=====')
-    //console.log(this.props.index)
-    //console.log('st:', st)
-    //console.log('ot:', ot)
-    //console.log('rect:', rect)
-    //console.log('=====')
-    //console.log('initialTop:', this.initialTop)
-    //console.log('initialOffsetTop:', this.initialOffsetTop)
-
-    if (this.state.stick) {
-      if (this.initialTop < st) {
-        console.log('STICK ME')
-        stick = true
-      } else {
-        if (this.initialTop >= st) {
-          console.log('UNSTICK ME')
-          stick = false
-        }
-      }
+    if (documentScrollTop >= elementOffset) {
+      stick = true
     } else {
-      if (ot < st) {
-        console.log('STICK ME')
-        stick = true
-      } else {
-        if (ot >= st) {
-          console.log('UNSTICK ME')
-          stick = false
-        }
-      }
+      stick = false
     }
 
     // Don't update state unless previous stick values have changed. This is to
@@ -88,84 +57,26 @@ export default class StickyHeader extends React.Component {
         stick: stick
       })
     }
-    if (this.props.disableFunc) {
-      this.props.disableFunc(this.refs.childrenRefs)
-    }
   }
 
   render () {
-    const stickClass = this.state.stick ? this.props.className : ''
-    let style = {}
-    if (this.state.stick) {
-      //style.position = 'fixed'
-    } else {
-      //style.position = 'relative'
-      style.top = ''
-    }
-
-    const cloned = React.cloneElement(
-      this.props.children,
-      {
-        ref: 'childrenRefs'
-      }
-    )
+    const stickyClass = this.state.stick ? this.props.stickyClass : ''
+    const classes = [stickyClass, this.props.className].join(' ')
     return (
-      <div>
-        { this.state.stick && <div style={{height: this.originalHeight}}></div> }
-        <div ref="sticky" style={style} className={stickClass} ref="content">
-          { cloned }
-        </div>
+      <div className={classes} ref="content">
+        { this.props.children }
       </div>
     )
   }
 }
 
 StickyHeader.defaultProps = {
-  className: 'stickify',
+  preventStick: false,
   offset: 0,
-
-  position: 'relative',
-  top: 0,
-  scrollY: 0,
-  settings: {
-    step: 3,
-    ratio: 100,
-    unit: 'vh',
-    breakpoint: {
-      upper: 200,
-      lower: 10
-    }
-  },
   events: [
     'scroll',
     'resize',
     'pageshow',
     'load'
-  ],
-  addEvent: (w, name, fn) => {
-    w.addEventListener(name, fn)
-  },
-  removeEvent: (w, name, fn) => {
-    w.removeEventListener(name, fn)
-  },
-  window: () => { return window },
-  getBox: (ref) => {
-    if (ref) {
-      return ref.getBoundingClientRect()
-    }
-
-    return { top: 0, bottom: 0 }
-  },
-  getDelta: (component, event, w, state, fn) => {
-    let delta = 0
-    if (!event.deltaY) {
-      delta = w.pageYOffset - state.scrollY
-    } else {
-      delta = event.deltaY
-    }
-
-    component.setState({ scrollY: w.pageYOffset }, () => {
-      fn(delta)
-    })
-  }
+  ]
 }
