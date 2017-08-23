05e78fb34f03188403e5cd9ec1fc61e0e3a44c37
Jay Neighbours <jay@ignition72.com>
2017-08-23 13:34:47 -0400

    Update Debarred.jsx
    removing unused help

--- src/components/Section/Legal/Investigations/Debarred.jsx
+++ src/components/Section/Legal/Investigations/Debarred.jsx
@@ -81,7 +81,6 @@ export default class Debarred extends SubsectionElement {
                      required={this.props.required}
                      scrollIntoView={this.props.scrollIntoView}>
             <Field title={i18n.t('legal.investigations.debarred.heading.agency')}
-                   help="legal.investigations.debarred.help.agency"
                    adjustFor="text"
                    scrollIntoView={this.props.scrollIntoView}>
               <Text name="Agency"
