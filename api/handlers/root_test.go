e54f1e9f8def5623599417f8a4ff4d6930a13568
Bryan Allred <bryan@revolvingcow.com>
2017-08-29 12:28:59 -0400

    Update root_test.go

--- api/handlers/root_test.go
+++ api/handlers/root_test.go
@@ -26,7 +26,7 @@ func TestRoot(t *testing.T) {
 	//	t.Errorf("handler returned unexpected body: got %v want %v", rec.Body.String(), expected)
 	//}
 
-	expected = fmt.Sprintf("%s.%s", APIName, APIVersion)
+	expected := fmt.Sprintf("%s.%s", APIName, APIVersion)
 	if rec.Header().Get("X-Eqip-Media-Type") != expected {
 		t.Errorf("handler returned unexpected header: got %v want %v", rec.Header().Get("X-Eqip-Media-Type"), expected)
 	}
