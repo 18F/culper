14a2ddf138f0c89bc945ebdf4fe1c85e8105fb8d
Bryan Allred <bryan.allred@gmail.com>
2017-11-07 08:54:52 -0500

    Quick patch so printing does not invalidate sections

--- src/validators/identificationbirthdate.js
+++ src/validators/identificationbirthdate.js
@@ -3,13 +3,13 @@ import { now } from '../components/Section/History/dateranges'
 
 export default class IdentificationBirthDateValidator {
   constructor (data = {}) {
-    this.date = data || {}
+    this.date = data.value || data || {}
   }
 
   inRange () {
-    const m = now.getMonth() - this.date.value.getMonth()
-    let age = now.getFullYear() - this.date.value.getFullYear()
-    if (m < 0 || (m === 0 && now.getDate() < this.date.value.getDate())) {
+    const m = now.getMonth() - this.date.date.getMonth()
+    let age = now.getFullYear() - this.date.date.getFullYear()
+    if (m < 0 || (m === 0 && now.getDate() < this.date.date.getDate())) {
       age--
     }
     return age > 16 && age < 130
