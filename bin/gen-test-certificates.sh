4e1e46e71121efe6f8da64a203fb7b354d3f02fc
Bryan Allred <bryan.allred@gmail.com>
2018-01-03 15:47:52 -0500

    Apply permissions on newly created certificates

--- bin/gen-test-certificates.sh
+++ bin/gen-test-certificates.sh
@@ -8,9 +8,14 @@ WS_CER=./api/webservice/testdata/test.cer
 openssl genrsa -out "$WS_KEY" 2048
 openssl pkcs8 -topk8 -inform PEM -in "$WS_KEY" -outform DER -nocrypt -out "$WS_PKCS"
 openssl req -new -x509 -subj "$SUBJ" -days 365 -key "$WS_KEY" -out "$WS_CER" -sha256
+chmod 0600 "$WS_KEY"
+chmod 0640 "$WS_PKCS"
+chmod 0640 "$WS_CER"
 
 SAML_KEY=./api/eapp.key
 SAML_CER=./api/eapp.crt
 
 openssl genrsa -out "$SAML_KEY" 2048
 openssl req -new -x509 -subj "$SUBJ" -days 365 -key "$SAML_KEY" -out "$SAML_CER" -sha256
+chmod 0600 "$SAML_KEY"
+chmod 0640 "$SAML_CER"
