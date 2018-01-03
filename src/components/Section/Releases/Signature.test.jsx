14a6323339b3816b696abd300dccd0240375dcad
Bryan Allred <bryan.allred@gmail.com>
2018-01-03 08:27:33 -0500

    Fix date summaries used in signatures
    Resolves truetandem/e-QIP-prototype#657
    
    It appears the commit 766f264 changed how the `DateSummary` performed
    by not looking for the `date` or `value` property. This is fine but
    all usages of the function were not fully tested to check for this change.

--- src/components/Section/Releases/Signature.test.jsx
+++ src/components/Section/Releases/Signature.test.jsx
@@ -13,4 +13,28 @@ describe('The Signature Release component', () => {
     component.find('button').simulate('click')
     expect(updates).toBe(1)
   })
+
+  it('displays name and date', () => {
+    const now = new Date()
+    const name = {
+      first: 'Bob',
+      middle: 'Joe',
+      last: 'Smith'
+    }
+    const expected = {
+      Name: name,
+      LegalName: {
+        Name: name
+      },
+      Date: {
+        date: now,
+        month: `${now.getMonth()+1}`,
+        day: `${now.getDate()}`,
+        year: `${now.getFullYear()}`
+      }
+    }
+    const component = mount(<Signature {...expected} />)
+    expect(component.find('.name.wet').text()).toBe(`${name.first} ${name.middle} ${name.last}`)
+    expect(component.find('.date.wet').text()).toBe(`${now.getMonth()+1}/${now.getDate()}/${now.getFullYear()}`)
+  })
 })
