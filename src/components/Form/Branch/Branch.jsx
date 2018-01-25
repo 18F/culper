0b7de986a913b21ee44171de722459d3e5afb0fb
Bryan Allred <bryan.allred@gmail.com>
2018-01-25 16:31:31 -0500

    Only allow domestic and intl addresses for relative employment
    Resolves truetandem/e-QIP-prototype#2332

--- src/components/Form/Branch/Branch.jsx
+++ src/components/Form/Branch/Branch.jsx
@@ -45,11 +45,11 @@ export default class Branch extends React.Component {
         <div className="content">
           {this.props.children}
         </div>
-        <RadioGroup
-          className="option-list branch"
-          required={this.props.required}
-          onError={this.props.onError}
-          selectedValue={this.props.value}>
+        <RadioGroup className="option-list branch"
+                    disabled={this.props.disabled}
+                    required={this.props.required}
+                    onError={this.props.onError}
+                    selectedValue={this.props.value}>
           <Radio name={this.props.name}
                  label={this.props.yesLabel}
                  value={this.props.yesValue}
