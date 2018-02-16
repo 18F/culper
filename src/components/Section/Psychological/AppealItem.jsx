17cb66d9958adaa2488421d1e5215fc456c154a8
Bryan Allred <bryan.allred@gmail.com>
2018-02-16 14:17:01 -0500

    Psych validation fixes

---
+++ src/components/Section/Psychological/AppealItem.jsx
@@ -0,0 +1,106 @@
+import React from 'react'
+import { i18n } from '../../../config'
+import { Location, ValidationElement, Field, Text, Svg } from '../../Form'
+
+export class AppealItem extends ValidationElement {
+  constructor (props) {
+    super(props)
+    this.update = this.update.bind(this)
+    this.updateCourtName = this.updateCourtName.bind(this)
+    this.updateCourtAddress = this.updateCourtAddress.bind(this)
+    this.updateDisposition = this.updateDisposition.bind(this)
+  }
+
+  update (queue) {
+    this.props.onUpdate({
+      CourtName: this.props.CourtName,
+      CourtAddress: this.props.CourtAddress,
+      Disposition: this.props.Disposition,
+      ...queue
+    })
+  }
+
+  updateCourtName (values) {
+    this.update({
+      CourtName: values
+    })
+  }
+
+  updateCourtAddress (values) {
+    this.update({
+      CourtAddress: values
+    })
+  }
+
+  updateDisposition (values) {
+    this.update({
+      Disposition: values
+    })
+  }
+
+  render () {
+    const prefix = this.props.prefix
+    return (
+      <div className="appeal">
+        <Field title={i18n.t(`psychological.${prefix}.heading.needMore`)}
+               optional={true}
+               className="more title"
+               scrollIntoView={this.props.scrollIntoView}>
+          <Svg src="/img/date-down-arrow.svg" className="more arrow" />
+        </Field>
+
+        <Field title={i18n.t(`psychological.${prefix}.heading.appealCourtName`)}
+               scrollIntoView={this.props.scrollIntoView}>
+          <Text {...this.props.CourtName}
+                name="CourtName"
+                className="appealcourtname"
+                onUpdate={this.updateCourtName}
+                onError={this.props.onError}
+                required={this.props.required}
+                />
+        </Field>
+
+        <Field title={i18n.t(`psychological.${prefix}.heading.appealCourtAddress`)}
+               optional={true}
+               adjustFor="address"
+               scrollIntoView={this.props.scrollIntoView}>
+          <Location {...this.props.CourtAddress}
+                    name="CourtAddress"
+                    className="appealcourtaddress"
+                    label={i18n.t(`psychological.${prefix}.label.courtAddress`)}
+                    layout={Location.ADDRESS}
+                    geocode={true}
+                    addressBooks={this.props.addressBooks}
+                    addressBook="Court"
+                    onUpdate={this.updateCourtAddress}
+                    onError={this.props.onError}
+                    required={this.props.required}
+                    />
+        </Field>
+
+        <Field title={i18n.t(`psychological.${prefix}.heading.disposition`)}
+               help={`psychological.${prefix}.help.disposition`}
+               scrollIntoView={this.props.scrollIntoView}>
+          <Text {...this.props.Disposition}
+                name="Disposition"
+                className="disposition"
+                onUpdate={this.updateDisposition}
+                onError={this.props.onError}
+                required={this.props.required}
+                />
+        </Field>
+      </div>
+    )
+  }
+}
+
+AppealItem.defaultProps = {
+  prefix: 'order',
+  addressBooks: {},
+  CourtName: {},
+  CourtAddress: {},
+  Disposition: {},
+  dispatch: (action) => {},
+  onUpdate: (queue) => {},
+  onError: (value, arr) => { return arr }
+}
