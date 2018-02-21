c91f173fa0fe9d4101f8e55331b0dd3578182846
Bryan Allred <bryan.allred@gmail.com>
2018-02-21 07:52:09 -0500

    Saving point with description separated from options

--- src/components/Section/History/Employment/ReasonLeft.jsx
+++ src/components/Section/History/Employment/ReasonLeft.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { i18n } from '../../../../config'
-import { ValidationElement, Field, Textarea, BranchCollection } from '../../../Form'
+import { today, daysAgo } from '../dateranges'
+import { buildDate } from '../../../../validators/helpers'
+import { ValidationElement, Show, Field, Textarea, BranchCollection } from '../../../Form'
 import ReasonOptions from './ReasonOptions'
 
 export default class ReasonLeft extends ValidationElement {
@@ -15,11 +17,13 @@ export default class ReasonLeft extends ValidationElement {
    * Handle any updates and bubble them up.
    */
   update (queue) {
+    const allowDescription = this.showDescription()
+    const allowOptions = this.showOptions()
     this.props.onUpdate({
       name: this.props.name,
-      Comments: this.props.Comments,
-      Reasons: this.props.Reasons,
-      ReasonDescription: this.props.ReasonDescription,
+      Reasons: allowOptions ? this.props.Reasons : { items: [], branch: {} },
+      ReasonDescription: allowDescription ? this.props.ReasonDescription : {},
+      Comments: allowDescription ? this.props.Comments : {},
       ...queue
     })
   }
@@ -36,38 +40,76 @@ export default class ReasonLeft extends ValidationElement {
     this.update({ Reasons: values })
   }
 
+  /**
+   * Only show the reasons for leaving if
+   *  - not currently employed there
+   */
+  showDescription () {
+    const now = new Date()
+    const dates = this.props.Dates || {}
+    const from = buildDate(dates.from)
+    const to = dates.present === true ? now : buildDate(dates.to)
+
+    // Check user is not currently employed.
+    return !(to &&
+             to.getFullYear() === now.getFullYear() &&
+             to.getMonth() === now.getMonth() &&
+             to.getDate() === now.getDate())
+  }
+
+  /**
+   * Only show the reasons options if
+   *  - employed there within the last 7 years
+   */
+  showOptions () {
+    const sevenYearsAgo = daysAgo(today, 365 * 7)
+    const now = new Date()
+    const dates = this.props.Dates || {}
+    const from = buildDate(dates.from)
+    const to = dates.present === true ? now : buildDate(dates.to)
+
+    // Check user is within seven years and part of approved employers.
+    return (from && from >= sevenYearsAgo) || (to && to >= sevenYearsAgo)
+  }
+
+
   render () {
     return (
       <div className="reason-leaving">
-        <Field title={i18n.t('history.employment.default.left.title')}
-               titleSize="h3"
-               comments={true}
-               commentsName="comments"
-               commentsValue={this.props.Comments}
-               scrollIntoView={this.props.scrollIntoView}>
-          <Textarea name="reason_description"
-                    {...this.props.ReasonDescription}
-                    className="reason-description"
-                    onUpdate={this.updateReasonDescription}
-                    onError={this.props.onError}
-                    required={this.props.required}
-                    />
-        </Field>
+        <Show when={this.showDescription()}>
+          <Field title={i18n.t('history.employment.default.left.title')}
+                 titleSize="h3"
+                 comments={true}
+                 commentsName="comments"
+                 commentsValue={this.props.Comments}
+                 scrollIntoView={this.props.scrollIntoView}>
+            <Textarea name="reason_description"
+                      {...this.props.ReasonDescription}
+                      className="reason-description"
+                      onUpdate={this.updateReasonDescription}
+                      onError={this.props.onError}
+                      required={this.props.required}
+                      />
+          </Field>
+        </Show>
 
-        <BranchCollection label={i18n.t('history.employment.default.left.branch')}
-                          appendLabel={i18n.t('history.employment.default.left.append')}
-                          content={i18n.m('history.employment.default.left.list')}
-                          {...(this.props.Reasons || {})}
-                          onUpdate={this.updateReasons}
-                          onError={this.props.onError}
-                          required={this.props.required}
-                          scrollIntoView={this.props.scrollIntoView}
-                          >
-          <ReasonOptions name="Item"
-                         bind={true}
-                         required={this.props.required}
-                         scrollIntoView={this.props.scrollIntoView} />
-        </BranchCollection>
+        <Show when={this.showOptions()}>
+          <BranchCollection label={i18n.t('history.employment.default.left.branch')}
+                            appendLabel={i18n.t('history.employment.default.left.append')}
+                            content={i18n.m('history.employment.default.left.list')}
+                            {...(this.props.Reasons || {})}
+                            className="reason-options"
+                            onUpdate={this.updateReasons}
+                            onError={this.props.onError}
+                            required={this.props.required}
+                            scrollIntoView={this.props.scrollIntoView}
+                            >
+            <ReasonOptions name="Item"
+                           bind={true}
+                           required={this.props.required}
+                           scrollIntoView={this.props.scrollIntoView} />
+          </BranchCollection>
+        </Show>
       </div>
     )
   }
@@ -77,6 +119,7 @@ ReasonLeft.defaultProps = {
   Comments: {},
   ReasonDescription: {},
   Reasons: {},
+  Dates: {},
   onUpdate: (queue) => {},
   onError: (value, arr) => { return arr }
 }
