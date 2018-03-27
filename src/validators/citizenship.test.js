bd716e209058478bb497599cde66def488b1bfee
Bryan Allred <bryan.allred@gmail.com>
2018-03-27 12:55:46 -0400

    Fix validation logic for citizenship section
    Resolves truetandem/e-QIP-prototype#2780

--- src/validators/citizenship.test.js
+++ src/validators/citizenship.test.js
@@ -264,226 +264,226 @@ describe('citizenship component validation', function () {
 
   it('can validate', () => {
     const tests = [
-      //{
-        //data: {
-          //CitizenshipStatus: { value: 'Citizen' }
-        //},
-        //expected: true
-      //},
-      //{
-        //data: {
-          //CitizenshipStatus: { value: 'ForeignBorn' },
-          //AbroadDocumentation: { value: 'Other' },
-          //Explanation: {
-            //value: 'Explanation'
-          //},
-          //DocumentNumber: {
-            //value: 'document number'
-          //},
-          //DocumentIssued: {
-            //day: '1',
-            //month: '1',
-            //year: '2016',
-            //date: new Date('1/1/2016')
-          //},
-          //PlaceIssued: {
-            //layout: Location.CITY_STATE_COUNTRY,
-            //country: { value: 'United States' },
-            //city: 'Arlington',
-            //state: 'VA'
-          //},
-          //DocumentName: {
-            //first: 'Foo',
-            //firstInitialOnly: false,
-            //middle: 'J',
-            //middleInitialOnly: true,
-            //noMiddleName: false,
-            //last: 'Bar',
-            //lastInitialOnly: false,
-            //suffix: 'Jr'
-          //},
-          //CertificateNumber: {
-            //value: 'certificate number'
-          //},
-          //CertificateIssued: {
-            //day: '1',
-            //month: '1',
-            //year: '2016',
-            //date: new Date('1/1/2016')
-          //},
-          //CertificateName: {
-            //first: 'Foo',
-            //firstInitialOnly: false,
-            //middle: 'J',
-            //middleInitialOnly: true,
-            //noMiddleName: false,
-            //last: 'Bar',
-            //lastInitialOnly: false,
-            //suffix: 'Jr'
-          //},
-          //BornOnMilitaryInstallation: { value: 'Yes' },
-          //MilitaryBase: {
-            //value: 'Camp Pendleton'
-          //}
-        //},
-        //expected: true
-      //},
-      //{
-        //data: {
-          //CitizenshipStatus: { value: 'Naturalized' },
-          //EntryDate: {
-            //day: '1',
-            //month: '1',
-            //year: '2016',
-            //date: new Date('1/1/2016')
-          //},
-          //EntryLocation: {
-            //country: { value: 'United States' },
-            //city: 'Arlington',
-            //state: 'VA',
-            //layout: Location.CITY_STATE
-          //},
-          //PriorCitizenship: {
-            //value: ['Germany']
-          //},
-          //HasAlienRegistration: { value: 'Yes' },
-          //AlienRegistrationNumber: {
-            //value: 'number'
-          //},
-          //CertificateNumber: {
-            //value: 'certificate number'
-          //},
-          //CertificateCourtName: {
-            //value: 'court name'
-          //},
-          //CertificateCourtAddress: {
-            //country: 'United States',
-            //street: '1234 Some Rd',
-            //city: 'Arlington',
-            //state: 'VA',
-            //zipcode: '22202',
-            //layout: Location.US_ADDRESS
-          //},
-          //CertificateIssued: {
-            //day: '1',
-            //month: '1',
-            //year: '2016',
-            //date: new Date('1/1/2016')
-          //},
-          //CertificateName: {
-            //first: 'Foo',
-            //firstInitialOnly: false,
-            //middle: 'J',
-            //middleInitialOnly: true,
-            //noMiddleName: false,
-            //last: 'Bar',
-            //lastInitialOnly: false,
-            //suffix: 'Jr'
-          //},
-          //Basis: { value: 'Other' },
-          //Explanation: {
-            //value: 'Explanation'
-          //}
-        //},
-        //expected: true
-      //},
-      //{
-        //data: {
-          //CitizenshipStatus: { value: 'Derived' },
-          //AlienRegistrationNumber: {
-            //value: 'number'
-          //},
-          //PermanentResidentCardNumber: {
-            //value: 'number'
-          //},
-          //CertificateNumber: {
-            //value: 'certificate number'
-          //},
-          //CertificateName: {
-            //first: 'Foo',
-            //firstInitialOnly: false,
-            //middle: 'J',
-            //middleInitialOnly: true,
-            //noMiddleName: false,
-            //last: 'Bar',
-            //lastInitialOnly: false,
-            //suffix: 'Jr'
-          //},
-          //CertificateIssued: {
-            //day: '1',
-            //month: '1',
-            //year: '2016',
-            //date: new Date('1/1/2016')
-          //},
-          //Basis: { value: 'Other' },
-          //Explanation: {
-            //value: 'Explanation'
-          //}
-        //},
-        //expected: true
-      //},
-      //{
-        //data: {
-          //CitizenshipStatus: { value: 'NotCitizen' },
-          //ResidenceStatus: {
-            //value: 'status'
-          //},
-          //EntryDate: {
-            //day: '1',
-            //month: '1',
-            //year: '2016',
-            //date: new Date('1/1/2016')
-          //},
-          //EntryLocation: {
-            //city: 'Arlington',
-            //state: 'VA',
-            //country: { value: 'United States' },
-            //layout: Location.CITY_STATE
-          //},
-          //PriorCitizenship: {
-            //value: ['Germany']
-          //},
-          //AlienRegistrationNumber: {
-            //value: 'number'
-          //},
-          //AlienRegistrationExpiration: {
-            //day: '1',
-            //month: '1',
-            //year: '2016',
-            //date: new Date('1/1/2016')
-          //},
-          //DocumentType: { value: 'Other' },
-          //Explanation: {
-            //value: 'Explanation'
-          //},
-          //DocumentNumber: {
-            //value: 'document number'
-          //},
-          //DocumentName: {
-            //first: 'Foo',
-            //firstInitialOnly: false,
-            //middle: 'J',
-            //middleInitialOnly: true,
-            //noMiddleName: false,
-            //last: 'Bar',
-            //lastInitialOnly: false,
-            //suffix: 'Jr'
-          //},
-          //DocumentIssued: {
-            //day: '1',
-            //month: '1',
-            //year: '2016',
-            //date: new Date('1/1/2016')
-          //},
-          //DocumentExpiration: {
-            //day: '1',
-            //month: '1',
-            //year: '2016',
-            //date: new Date('1/1/2016')
-          //}
-        //},
-        //expected: true
-      //}
+      {
+        data: {
+          CitizenshipStatus: { value: 'Citizen' }
+        },
+        expected: true
+      },
+      {
+        data: {
+          CitizenshipStatus: { value: 'ForeignBorn' },
+          AbroadDocumentation: { value: 'Other' },
+          Explanation: {
+            value: 'Explanation'
+          },
+          DocumentNumber: {
+            value: 'document number'
+          },
+          DocumentIssued: {
+            day: '1',
+            month: '1',
+            year: '2016',
+            date: new Date('1/1/2016')
+          },
+          PlaceIssued: {
+            layout: Location.CITY_STATE_COUNTRY,
+            country: { value: 'United States' },
+            city: 'Arlington',
+            state: 'VA'
+          },
+          DocumentName: {
+            first: 'Foo',
+            firstInitialOnly: false,
+            middle: 'J',
+            middleInitialOnly: true,
+            noMiddleName: false,
+            last: 'Bar',
+            lastInitialOnly: false,
+            suffix: 'Jr'
+          },
+          CertificateNumber: {
+            value: 'certificate number'
+          },
+          CertificateIssued: {
+            day: '1',
+            month: '1',
+            year: '2016',
+            date: new Date('1/1/2016')
+          },
+          CertificateName: {
+            first: 'Foo',
+            firstInitialOnly: false,
+            middle: 'J',
+            middleInitialOnly: true,
+            noMiddleName: false,
+            last: 'Bar',
+            lastInitialOnly: false,
+            suffix: 'Jr'
+          },
+          BornOnMilitaryInstallation: { value: 'Yes' },
+          MilitaryBase: {
+            value: 'Camp Pendleton'
+          }
+        },
+        expected: true
+      },
+      {
+        data: {
+          CitizenshipStatus: { value: 'Naturalized' },
+          EntryDate: {
+            day: '1',
+            month: '1',
+            year: '2016',
+            date: new Date('1/1/2016')
+          },
+          EntryLocation: {
+            country: { value: 'United States' },
+            city: 'Arlington',
+            state: 'VA',
+            layout: Location.CITY_STATE
+          },
+          PriorCitizenship: {
+            value: ['Germany']
+          },
+          HasAlienRegistration: { value: 'Yes' },
+          AlienRegistrationNumber: {
+            value: 'number'
+          },
+          CertificateNumber: {
+            value: 'certificate number'
+          },
+          CertificateCourtName: {
+            value: 'court name'
+          },
+          CertificateCourtAddress: {
+            country: 'United States',
+            street: '1234 Some Rd',
+            city: 'Arlington',
+            state: 'VA',
+            zipcode: '22202',
+            layout: Location.US_ADDRESS
+          },
+          CertificateIssued: {
+            day: '1',
+            month: '1',
+            year: '2016',
+            date: new Date('1/1/2016')
+          },
+          CertificateName: {
+            first: 'Foo',
+            firstInitialOnly: false,
+            middle: 'J',
+            middleInitialOnly: true,
+            noMiddleName: false,
+            last: 'Bar',
+            lastInitialOnly: false,
+            suffix: 'Jr'
+          },
+          Basis: { value: 'Other' },
+          Explanation: {
+            value: 'Explanation'
+          }
+        },
+        expected: true
+      },
+      {
+        data: {
+          CitizenshipStatus: { value: 'Derived' },
+          AlienRegistrationNumber: {
+            value: 'number'
+          },
+          PermanentResidentCardNumber: {
+            value: 'number'
+          },
+          CertificateNumber: {
+            value: 'certificate number'
+          },
+          CertificateName: {
+            first: 'Foo',
+            firstInitialOnly: false,
+            middle: 'J',
+            middleInitialOnly: true,
+            noMiddleName: false,
+            last: 'Bar',
+            lastInitialOnly: false,
+            suffix: 'Jr'
+          },
+          CertificateIssued: {
+            day: '1',
+            month: '1',
+            year: '2016',
+            date: new Date('1/1/2016')
+          },
+          Basis: { value: 'Other' },
+          Explanation: {
+            value: 'Explanation'
+          }
+        },
+        expected: true
+      },
+      {
+        data: {
+          CitizenshipStatus: { value: 'NotCitizen' },
+          ResidenceStatus: {
+            value: 'status'
+          },
+          EntryDate: {
+            day: '1',
+            month: '1',
+            year: '2016',
+            date: new Date('1/1/2016')
+          },
+          EntryLocation: {
+            city: 'Arlington',
+            state: 'VA',
+            country: { value: 'United States' },
+            layout: Location.CITY_STATE
+          },
+          PriorCitizenship: {
+            value: ['Germany']
+          },
+          AlienRegistrationNumber: {
+            value: 'number'
+          },
+          AlienRegistrationExpiration: {
+            day: '1',
+            month: '1',
+            year: '2016',
+            date: new Date('1/1/2016')
+          },
+          DocumentType: { value: 'Other' },
+          Explanation: {
+            value: 'Explanation'
+          },
+          DocumentNumber: {
+            value: 'document number'
+          },
+          DocumentName: {
+            first: 'Foo',
+            firstInitialOnly: false,
+            middle: 'J',
+            middleInitialOnly: true,
+            noMiddleName: false,
+            last: 'Bar',
+            lastInitialOnly: false,
+            suffix: 'Jr'
+          },
+          DocumentIssued: {
+            day: '1',
+            month: '1',
+            year: '2016',
+            date: new Date('1/1/2016')
+          },
+          DocumentExpiration: {
+            day: '1',
+            month: '1',
+            year: '2016',
+            date: new Date('1/1/2016')
+          }
+        },
+        expected: true
+      }
     ]
 
     tests.forEach(test => {
