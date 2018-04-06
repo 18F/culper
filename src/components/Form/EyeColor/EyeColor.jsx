095e5c7ad74317af6cedc76cafc6abb29135834b
Bryan Allred <bryan.allred@gmail.com>
2018-04-06 13:27:43 -0400

    Update order of hair and eye colors and image for bald option
    Resolves truetandem/e-QIP-prototype#2842
    Resolves truetandem/e-QIP-prototype#2843

--- src/components/Form/EyeColor/EyeColor.jsx
+++ src/components/Form/EyeColor/EyeColor.jsx
@@ -29,34 +29,6 @@ export default class EyeColor extends ValidationElement {
                     selectedValue={this.props.value}
                     required={this.props.required}
                     onError={this.props.onError}>
-          <Radio name={`${this.props.name}-black`}
-                 label={i18n.t('identification.traits.eye.black')}
-                 value="Black"
-                 className="black"
-                 disabled={this.props.disabled}
-                 onUpdate={this.handleUpdate}
-                 onError={this.props.onError}
-                 onBlur={this.props.onBlur}
-                 onFocus={this.props.onFocus}
-                 >
-            <div className="eye-icon black">
-              <Svg src="/img/eye.svg" />
-            </div>
-          </Radio>
-          <Radio name={`${this.props.name}-blue`}
-                 label={i18n.t('identification.traits.eye.blue')}
-                 value="Blue"
-                 className="blue"
-                 disabled={this.props.disabled}
-                 onUpdate={this.handleUpdate}
-                 onError={this.props.onError}
-                 onBlur={this.props.onBlur}
-                 onFocus={this.props.onFocus}
-                 >
-            <div className="eye-icon blue">
-              <Svg src="/img/eye.svg" />
-            </div>
-          </Radio>
           <Radio name={`${this.props.name}-brown`}
                  label={i18n.t('identification.traits.eye.brown')}
                  value="Brown"
@@ -68,34 +40,6 @@ export default class EyeColor extends ValidationElement {
                  onFocus={this.props.onFocus}
                  >
             <div className="eye-icon brown">
-              <Svg src="/img/eye.svg" />
-            </div>
-          </Radio>
-          <Radio name={`${this.props.name}-gray`}
-                 label={i18n.t('identification.traits.eye.gray')}
-                 value="Gray"
-                 className="gray"
-                 disabled={this.props.disabled}
-                 onUpdate={this.handleUpdate}
-                 onError={this.props.onError}
-                 onBlur={this.props.onBlur}
-                 onFocus={this.props.onFocus}
-                 >
-            <div className="eye-icon gray">
-              <Svg src="/img/eye.svg" />
-            </div>
-          </Radio>
-          <Radio name={`${this.props.name}-green`}
-                 label={i18n.t('identification.traits.eye.green')}
-                 value="Green"
-                 className="green"
-                 disabled={this.props.disabled}
-                 onUpdate={this.handleUpdate}
-                 onError={this.props.onError}
-                 onBlur={this.props.onBlur}
-                 onFocus={this.props.onFocus}
-                 >
-            <div className="eye-icon green">
               <Svg src="/img/eye.svg" />
             </div>
           </Radio>
@@ -113,6 +57,48 @@ export default class EyeColor extends ValidationElement {
               <Svg src="/img/eye.svg" />
             </div>
           </Radio>
+          <Radio name={`${this.props.name}-blue`}
+                 label={i18n.t('identification.traits.eye.blue')}
+                 value="Blue"
+                 className="blue"
+                 disabled={this.props.disabled}
+                 onUpdate={this.handleUpdate}
+                 onError={this.props.onError}
+                 onBlur={this.props.onBlur}
+                 onFocus={this.props.onFocus}
+                 >
+            <div className="eye-icon blue">
+              <Svg src="/img/eye.svg" />
+            </div>
+          </Radio>
+          <Radio name={`${this.props.name}-green`}
+                 label={i18n.t('identification.traits.eye.green')}
+                 value="Green"
+                 className="green"
+                 disabled={this.props.disabled}
+                 onUpdate={this.handleUpdate}
+                 onError={this.props.onError}
+                 onBlur={this.props.onBlur}
+                 onFocus={this.props.onFocus}
+                 >
+            <div className="eye-icon green">
+              <Svg src="/img/eye.svg" />
+            </div>
+          </Radio>
+          <Radio name={`${this.props.name}-gray`}
+                 label={i18n.t('identification.traits.eye.gray')}
+                 value="Gray"
+                 className="gray"
+                 disabled={this.props.disabled}
+                 onUpdate={this.handleUpdate}
+                 onError={this.props.onError}
+                 onBlur={this.props.onBlur}
+                 onFocus={this.props.onFocus}
+                 >
+            <div className="eye-icon gray">
+              <Svg src="/img/eye.svg" />
+            </div>
+          </Radio>
           <Radio name={`${this.props.name}-maroon`}
                  label={i18n.t('identification.traits.eye.maroon')}
                  value="Maroon"
@@ -124,6 +110,20 @@ export default class EyeColor extends ValidationElement {
                  onFocus={this.props.onFocus}
                  >
             <div className="eye-icon maroon">
+              <Svg src="/img/eye.svg" />
+            </div>
+          </Radio>
+          <Radio name={`${this.props.name}-black`}
+                 label={i18n.t('identification.traits.eye.black')}
+                 value="Black"
+                 className="black"
+                 disabled={this.props.disabled}
+                 onUpdate={this.handleUpdate}
+                 onError={this.props.onError}
+                 onBlur={this.props.onBlur}
+                 onFocus={this.props.onFocus}
+                 >
+            <div className="eye-icon black">
               <Svg src="/img/eye.svg" />
             </div>
           </Radio>
