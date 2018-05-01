10578d70289c5db191df0301871d93f5b321be87
Bryan Allred <bryan.allred@gmail.com>
2018-05-01 09:33:41 -0400

    Added caching controls
    Resolves truetandem/e-QIP-prototype#2933

--- api/log.go
+++ api/log.go
@@ -108,4 +108,5 @@ const (
 	AttachmentSaved      = "Attachment saved"
 	AttachmentDeleted    = "Attachment deleted"
 	AttachmentDownloaded = "Attachment downloaded"
+	CacheHeaders         = "Applying headers for caching"
 )
