df353b587e8e17f97f0cd6ada0aa62d8bb9a0da7
Bryan Allred <bryan.allred@gmail.com>
2018-05-01 13:54:33 -0400

    API test coverage bump

--- src/services/api.test.js
+++ src/services/api.test.js
@@ -93,4 +93,101 @@ describe('The API', () => {
 
     window.location = previousLocation
   })
+
+  it('can issue refresh of token', () => {
+    const mock = new MockAdapter(api.proxy)
+    mock.onPost('/refresh').reply(200, true)
+    api.refresh().then(response => {
+      expect(response.data).toEqual(true)
+    })
+  })
+
+  it('can issue save', () => {
+    const mock = new MockAdapter(api.proxy)
+    mock.onPost('/me/save').reply(200, true)
+    api.save({}).then(response => {
+      expect(response.data).toEqual(true)
+    })
+  })
+
+  it('can request section', () => {
+    const mock = new MockAdapter(api.proxy)
+    mock.onGet('/me/form/section?type=blah').reply(200, true)
+    api.section('blah').then(response => {
+      expect(response.data).toEqual(true)
+    })
+  })
+
+  it('can status', () => {
+    const mock = new MockAdapter(api.proxy)
+    mock.onGet('/me/status').reply(200, true)
+    api.status().then(response => {
+      expect(response.data).toEqual(true)
+    })
+  })
+
+  it('can submit', () => {
+    const mock = new MockAdapter(api.proxy)
+    mock.onPost('/me/form/submit').reply(200, true)
+    api.submit({}).then(response => {
+      expect(response.data).toEqual(true)
+    })
+  })
+
+  it('can get form data', () => {
+    const mock = new MockAdapter(api.proxy)
+    mock.onGet('/me/form').reply(200, true)
+    api.form().then(response => {
+      expect(response.data).toEqual(true)
+    })
+  })
+
+  it('can get form hash', () => {
+    const mock = new MockAdapter(api.proxy)
+    mock.onGet('/me/form/hash').reply(200, true)
+    api.hash().then(response => {
+      expect(response.data).toEqual(true)
+    })
+  })
+
+  it('can get SAML request', () => {
+    const mock = new MockAdapter(api.proxy)
+    mock.onGet('/auth/saml').reply(200, true)
+    api.saml().then(response => {
+      expect(response.data).toEqual(true)
+    })
+  })
+
+  it('can list attachments', () => {
+    const mock = new MockAdapter(api.proxy)
+    mock.onGet('/me/attachment').reply(200, true)
+    api.listAttachments().then(response => {
+      expect(response.data).toEqual(true)
+    })
+  })
+
+  it('can save attachment', () => {
+    const mock = new MockAdapter(api.proxy)
+    mock.onPost('/me/attachment').reply(200, true)
+    api.saveAttachment({}).then(response => {
+      expect(response.data).toEqual(true)
+    })
+  })
+
+  it('can update attachment', () => {
+    const mock = new MockAdapter(api.proxy)
+    mock.onPost('/me/attachment/1').reply(200, true)
+    mock.onPost('/me/attachment/1/delete').reply(200, true)
+    api.updateAttachment(1, 'blah').then(response => {
+      expect(response.data).toEqual(true)
+    })
+  })
+
+  it('can delete attachment', () => {
+    const mock = new MockAdapter(api.proxy)
+    mock.onPost('/me/attachment/1/delete').reply(200, true)
+    api.deleteAttachment(1).then(response => {
+      expect(response.data).toEqual(true)
+    })
+  })
 })
