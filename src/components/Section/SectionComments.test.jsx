03399e1fda89a01595636c4d4136cd8c6dc69309
John Flores <johnsflores@gmail.com>
2018-05-01 13:46:05 -0400

    Added basic default tests

--- src/components/Section/SectionComments.test.jsx
+++ src/components/Section/SectionComments.test.jsx
@@ -1,14 +1,8 @@
 import React from 'react'
-import configureMockStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
-import { Provider } from 'react-redux'
 import { mount } from 'enzyme'
 import SectionComments from './SectionComments'
 
 describe('Section comments', () => {
-  const middlewares = [ thunk ]
-  const mockStore = configureMockStore(middlewares)
-
   it('has optional text', () => {
     const props = {
       title: 'Test section comments'
@@ -29,5 +23,12 @@ describe('Section comments', () => {
     const component = mount(<SectionComments {...props} />)
     component.find('.section-comment input').simulate('change', { target: { value: 'no comment' } })
     expect(comments).toBe('no comment')
+  })
+
+  it('defaults behave as expected', () => {
+    expect(SectionComments.defaultProps.onError(0, [])).toEqual([])
+    expect(SectionComments.defaultProps.validator()).toBe(true)
+    expect(SectionComments.defaultProps.onUpdate()).toBe(undefined)
+    expect(SectionComments.defaultProps.dispatch()).toBe(undefined)
   })
 })
