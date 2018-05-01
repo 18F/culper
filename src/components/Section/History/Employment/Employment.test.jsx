e67d20728a064f4a3ab5e973fd9db8eaef59008f
Bryan Allred <bryan.allred@gmail.com>
2018-05-01 16:49:14 -0400

    History bump

---
+++ src/components/Section/History/Employment/Employment.test.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import Employment from './Employment'
+
+describe('The employment section', () => {
+  it('can trigger updates', () => {
+    let updates = 0
+    const expected = {
+      List: {
+        branch: {},
+        items: [{
+          Item: {
+            Dates: {
+              from: {
+                day: '1',
+                month: '1',
+                year: '2014',
+                date: new Date('1/1/2014')
+              },
+              to: {
+                day: '1',
+                month: '1',
+                year: '2018',
+                date: new Date('1/1/2018')
+              }
+            }
+          }
+        }]
+      },
+      onUpdate: () => { updates++ }
+    }
+    const component = mount(<Employment {...expected} />)
+    expect(updates).toBe(1)
+  })
+})
