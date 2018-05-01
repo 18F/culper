0deafff84c73305a73c487ee185ada342a2f9d11
John Flores <johnsflores@gmail.com>
2018-05-01 16:18:00 -0400

    Additional tests for ErrorList

--- src/components/ErrorList/ErrorList.test.jsx
+++ src/components/ErrorList/ErrorList.test.jsx
@@ -4,7 +4,7 @@ import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import { mount } from 'enzyme'
-import ErrorList from './ErrorList'
+import ErrorList, { groupBy, inAccordion } from './ErrorList'
 
 describe('The error list component', () => {
   // Setup
@@ -28,5 +28,21 @@ describe('The error list component', () => {
     expect(component.find('.error-list').length).toBe(1)
     expect(component.find('.error-list .field .title').length).toBe(1)
     expect(component.find('.error-list .field .error-messages').length).toBe(1)
+  })
+
+  it('groups stuff', () => {
+    const getter = function () { return 'foo' }
+    expect(groupBy([], getter)).toEqual({})
+    expect(groupBy([{name: 'Doe'}], getter)).toEqual({
+      foo: [
+        {
+          name: 'Doe'
+        }
+      ]
+    })
+  })
+
+  it('checks if element is within accordion', () => {
+    expect(inAccordion(null)).toEqual(false)
   })
 })
