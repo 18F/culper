db8794eba2e14eb170ca2c6edfe0be886db0e073
Ryan Hofschneider <ryan.hofschneider@gsa.gov>
2018-06-10 23:43:16 -0700

    Initial commit for e-signature PDFs
    Parses applicant data and populates template PDFs for ESIGN Act.
    Original OmniGraffle files are provided for when templates need to be
    regenerated – the templates currently do not have any additional,
    to-be-determined eApp/eAgency record meta-data that would be helpful to
    have in the archived PDFs. #372

---
+++ api/pdf/pdf.go
@@ -0,0 +1,243 @@
+package pdf
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"path"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/18F/e-QIP-prototype/api"
+	"github.com/Jeffail/gabs"
+)
+
+// An ArchivalPdf represents a PDF that needs to be retained and submitted to e-QIP.
+type ArchivalPdf struct {
+	template string // filename of PDF template that will be populated
+	section  string // eApp sub-section where signature information is stored
+	eqipType string // document type defined by e-QIP (e.g., CER, REL, etc.)
+}
+
+var (
+	Certification  = ArchivalPdf{"certification-SF86-November2016.template.pdf", "AdditionalComments", "CER"}
+	CreditRelease  = ArchivalPdf{"credit-SF86-November2016.template.pdf", "Credit", "FCR"}
+	MedicalRelease = ArchivalPdf{"medical-SF86-November2016.template.pdf", "Medical", "MEL"}
+	GeneralRelease = ArchivalPdf{"general-SF86-November2016.template.pdf", "General", "REL"}
+)
+
+// A PdfField represents the fixed-width placeholder in a ArchivalPdf template.
+type field struct {
+	name     string // field identifier as it appears in the template
+	length   int    // the fixed-width length of the field
+	getValue func(*gabs.Container) string
+}
+
+type Service struct {
+	Env api.Settings
+	Log api.LogService
+}
+
+// CreatePdf populates a template PDF with values from the application, using basic text subsitution.
+// Field names (e.g., SSN) are replaced with application values, space padded to a fixed length to
+// ensure that PDF object/xref byte offsets do not need to be updated. Assumes values are ASCII.
+func (service Service) CreatePdf(application map[string]interface{}, pdfType ArchivalPdf) ([]byte, error) {
+	// Get application data into easily queryable form
+	json, _ := gabs.Consume(application)
+
+	signedOn := func(json *gabs.Container) string {
+		return getSignedOn(json, pdfType.section)
+	}
+
+	// Field widths are based on monospaced font and fixed point size
+	// used in PDF templates and their layout
+	fields := []field{
+		{"SSN", 11, getSsn},
+		{"FIRST_MIDDLE_LAST", 64, getFullName},
+		{"SIGNED_ON", 10, signedOn},
+		{"OTHER_NAMES", 77, getOtherNames},
+		{"CITY_COUNTRY", 26, getCityCountry},
+		{"STATE", 6, getState},
+		{"ZIP_CODE", 10, getZipCode},
+		{"DOB", 10, getDob},
+		{"TELEPHONE", 20, getTelephone},
+		{"STREET_ADDRESS", 40, getStreetAddress},
+	}
+
+	dat, err := ioutil.ReadFile(path.Join("templates", pdfType.template))
+	if err != nil {
+		return nil, err
+	}
+	str := string(dat)
+
+	for _, f := range fields {
+		// PDF template field names are followed by one or more spaces
+		re := f.name + "  *"
+
+		// Pad field values with spaces to preserve PDF object/xref byte offsets
+		format := "%-" + strconv.Itoa(f.length) + "s"
+
+		str = regexp.MustCompile(re).ReplaceAllString(str, fmt.Sprintf(format, f.getValue(json)))
+	}
+
+	return []byte(str), nil
+}
+
+func getPath(json *gabs.Container, path string) string {
+	value, _ := json.Path(path).Data().(string)
+	return value
+}
+
+func getPaths(json *gabs.Container, prefix string, keys []string) []string {
+	if prefix[len(prefix)-1:] != "." {
+		prefix = prefix + "."
+	}
+
+	values := []string{}
+	for _, k := range keys {
+		values = append(values, getPath(json, prefix+k))
+	}
+	return values
+}
+
+// Location fields are one of few structures that are not persisted
+// in JSON structure or database if they are empty; see location.go.
+func getFullAddress(json *gabs.Container) (*gabs.Container, error) {
+	path := "History.Residence.props.List.props.items"
+	children, _ := json.Path(path).Children()
+	for _, child := range children {
+		present, _ := child.Path("Item.Dates.props.present").Data().(bool)
+		if present == true {
+			return child, nil
+		}
+	}
+	return nil, errors.New("No address indicated as `present`")
+}
+
+func getStreetAddress(json *gabs.Container) string {
+	full, err := getFullAddress(json)
+	if err != nil {
+		return ""
+	}
+
+	street := getPath(full, "Item.Address.props.street")
+	street2 := getPath(full, "Item.Address.props.street2")
+
+	if street2 != "" {
+		street = street + ", " + street2
+	}
+
+	return street
+}
+
+func getTelephone(json *gabs.Container) string {
+	path := "Identification.Contacts.props.PhoneNumbers.props.items"
+	children, _ := json.Path(path).Children()
+	for _, child := range children {
+		// Return the first telephone phone number in the list
+
+		prefix := "Item.Telephone.props."
+		numberType := getPath(child, prefix+"type")
+		number := getPath(child, prefix+"number")
+		extension := getPath(child, prefix+"extension")
+		if extension != "" {
+			extension = " x" + extension
+		}
+
+		if numberType != "Domestic" {
+			return number + extension
+		}
+
+		return fmt.Sprintf("(%s) %s-%s%s",
+			number[0:3], number[3:6], number[6:], extension)
+	}
+
+	return ""
+}
+
+func getDob(json *gabs.Container) string {
+	prefix := "Identification.ApplicantBirthDate.props.Date.props"
+	keys := []string{"month", "day", "year"}
+	values := getPaths(json, prefix, keys)
+
+	return strings.Join(values, "/")
+}
+
+func getZipCode(json *gabs.Container) string {
+	full, err := getFullAddress(json)
+	if err != nil {
+		return ""
+	}
+
+	return getPath(full, "Item.Address.props.zipcode")
+}
+
+func getState(json *gabs.Container) string {
+	full, err := getFullAddress(json)
+	if err != nil {
+		return ""
+	}
+	return getPath(full, "Item.Address.props.state")
+}
+
+func getCityCountry(json *gabs.Container) string {
+	full, err := getFullAddress(json)
+	if err != nil {
+		return ""
+	}
+
+	city := getPath(full, "Item.Address.props.city")
+	country := getPath(full, "Item.Address.props.country")
+
+	if country == "" || country == "United States" {
+		return city
+	}
+	return city + ", " + country
+}
+
+func getOtherNames(json *gabs.Container) string {
+	names := []string{}
+
+	path := "Identification.OtherNames.props.List.props.items"
+	children, _ := json.Path(path).Children()
+	for _, child := range children {
+		keys := []string{"first", "middle", "last"}
+		values := getPaths(child, "Item.Name.props", keys)
+		n := strings.Join(values, " ")
+		names = append(names, n)
+	}
+	return strings.Join(names, ", ")
+}
+
+func getSignedOn(json *gabs.Container, docSubsection string) string {
+	prefix := "Submission.Releases.props." + docSubsection + ".props.Signature.props.Date.props"
+	keys := []string{"month", "day", "year"}
+	values := getPaths(json, prefix, keys)
+
+	return strings.Join(values, "/")
+}
+
+func getSsn(json *gabs.Container) string {
+	prefix := "Identification.ApplicantSSN.props.ssn.props"
+	keys := []string{"first", "middle", "last"}
+	values := getPaths(json, prefix, keys)
+
+	return strings.Join(values, "-")
+}
+
+func getFullName(json *gabs.Container) string {
+	prefix := "Identification.ApplicantName.props.Name.props"
+	keys := []string{"first", "middle", "last"}
+	values := getPaths(json, prefix, keys)
+
+	suffix := getPath(json, prefix+"suffix")
+	if suffix == "Other" {
+		suffix = getPath(json, prefix+"suffixOther")
+	}
+	if suffix != "" {
+		values = append(values, suffix)
+	}
+
+	return strings.Join(values, " ")
+}
---
+++ api/pdf/pdf_test.go
@@ -0,0 +1,66 @@
+package pdf
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"testing"
+
+	"github.com/18F/e-QIP-prototype/api/mock"
+)
+
+func TestPackage(t *testing.T) {
+	application := applicationData(t)
+	logger := &mock.LogService{}
+	service := Service{Log: logger}
+
+	templates := []ArchivalPdf{
+		Certification,
+		MedicalRelease,
+		GeneralRelease,
+		CreditRelease,
+	}
+
+	for _, pdf := range templates {
+		dat, err := service.CreatePdf(application, pdf)
+		if err != nil {
+			t.Fatalf(fmt.Sprintf("Error creating PDF from %s: %s", pdf.template, err.Error()))
+		}
+		_ = dat
+	}
+}
+
+func applicationData(t *testing.T) map[string]interface{} {
+	return map[string]interface{}{
+		"Identification": map[string]interface{}{
+			"ApplicantName":       readSectionData("testdata/identification-name.json", t),
+			"Contacts":            readSectionData("testdata/identification-contacts.json", t),
+			"OtherNames":          readSectionData("testdata/identification-othernames.json", t),
+			"ApplicantBirthDate":  readSectionData("testdata/identification-birthdate.json", t),
+			"ApplicantBirthPlace": readSectionData("testdata/identification-birthplace.json", t),
+			"ApplicantSSN":        readSectionData("testdata/identification-ssn.json", t),
+			"Physical":            readSectionData("testdata/identification-physical.json", t),
+		},
+		"Submission": map[string]interface{}{
+			"Releases": readSectionData("testdata/submission.json", t),
+		},
+		"History": map[string]interface{}{
+			"Residence": readSectionData("testdata/history-residence.json", t),
+		},
+	}
+}
+
+func readSectionData(file string, t *testing.T) map[string]interface{} {
+	b, err := ioutil.ReadFile(file)
+	if err != nil {
+		t.Fatalf("Error message %s", err)
+		return map[string]interface{}{}
+	}
+
+	var js map[string]interface{}
+	if err := json.Unmarshal(b, &js); err != nil {
+		t.Fatalf("Error message %s", err)
+		return map[string]interface{}{}
+	}
+	return js
+}
