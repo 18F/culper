ec4c8e7e8fa0bdfe7d57d93fc59f607316cf5750
Aidan Feldman <aidan.feldman@gsa.gov>
2018-06-18 18:42:04 -0400

    use the Redux DevTools Extension for troubleshooting
    Having the log message output to the console is noisy, and not helpful
    if not dealing with Redux. Switched to using the browser extension,
    which has more features.

--- src/store.js
+++ src/store.js
@@ -1,17 +1,23 @@
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 import { createStore, applyMiddleware } from 'redux'
-import logger from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { historyMiddleware, sectionMiddleware, saveMiddleware, settingsMiddleware, clearErrorsMiddleware } from './middleware/history'
 
-let middleware = []
-if (process.env.NODE_ENV !== 'production') {
-  middleware.push(logger)
-}
-middleware = [thunk, ...middleware, historyMiddleware, sectionMiddleware, saveMiddleware, settingsMiddleware, clearErrorsMiddleware]
+const middleware = [thunk, historyMiddleware, sectionMiddleware, saveMiddleware, settingsMiddleware, clearErrorsMiddleware]
 
 // Creates a redux store that defines the state tree for the application.
 // See rootReducer for all sub-states.
-const store = process.env.NODE_ENV === 'test' ? createStore(rootReducer) : createStore(rootReducer, applyMiddleware(...middleware))
+let store
+switch (process.env.NODE_ENV) {
+case 'test':
+  store = createStore(rootReducer)
+  break
+case 'production':
+  store = createStore(rootReducer, applyMiddleware(...middleware))
+  break
+default:
+  store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware)))
+}
 
 export default store
