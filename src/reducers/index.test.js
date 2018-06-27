a5bd488996015bbf098b8807f33fd44907c696ca
Aidan Feldman <aidan.feldman@gsa.gov>
2018-06-27 14:19:32 -0400

    clear state on logout

--- src/reducers/index.test.js
+++ src/reducers/index.test.js
@@ -36,4 +36,23 @@ describe('Root Reducer', function () {
     const action = { type: 'unknown' }
     expect(rootReducer(startState, action)).toEqual(defaultState)
   })
+
+  it('should handle logout', function () {
+    const startState = {
+      application: {
+        AddressBooks: { foo: 'bar' }
+      },
+      authentication: {
+        authenticated: true,
+        token: 'dummytoken'
+      },
+      section: {
+        section: "identification",
+        subsection: ""
+      }
+    }
+
+    const action = { type: AuthConstants.LOGOUT }
+    expect(rootReducer(startState, action)).toEqual(defaultState)
+  })
 })
