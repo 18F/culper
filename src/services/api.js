d326e106bf2cd1ccc0fd2227cdf29cb13e529a13
Aidan Feldman <aidan.feldman@gsa.gov>
2018-06-29 17:25:51 -0400

    use COOKIE_DOMAIN in browser if set

--- src/services/api.js
+++ src/services/api.js
@@ -29,7 +29,9 @@ const getSplitValue = (key, raw, delim1, delim2) => {
 }
 
 export const deleteCookie = (name) => {
-  document.cookie = `${name}=; domain=${window.location.hostname}; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;`
+  const domain = process.env.COOKIE_DOMAIN || window.location.hostname
+  // TODO complain if cookie not present
+  document.cookie = `${name}=; domain=${domain}; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;`
 }
 
 class Api {
