fd0fdb09dea9905c99a16ac5ca26ef8a9ac17c31
Aidan Feldman <aidan.feldman@gsa.gov>
2018-07-16 17:19:40 -0400

    match errors even when they are from a different subsection
    Previously, it would only match on the same subsection (and the same
    question itself), which wouldn't clear errors across subsection
    boundaries (Review -> something else, specifically).

--- src/reducers/error.test.js
+++ src/reducers/error.test.js
@@ -2,12 +2,13 @@ import errorReducer from './error'
 import { clearErrors, updateApplication } from '../actions/ApplicationActions'
 
 describe('Error reducer', () => {
+  const sectionName = 'errorTest'
+
   it('should return default state', () => {
     expect(errorReducer('')(undefined, {})).toEqual({})
   })
 
   it('can add new error', () => {
-    const sectionName = 'errorTest'
     const defaultState = {}
     const action = updateApplication(sectionName, 'identification', [
       {
@@ -29,7 +30,6 @@ describe('Error reducer', () => {
   })
 
   it('can update error', () => {
-    const sectionName = 'errorTest'
     const defaultState = {
       identification: [
         {
@@ -78,5 +78,38 @@ describe('Error reducer', () => {
     const action = clearErrors('identification', 'othernames')
     expect(errorReducer('Errors')(defaultState, action)[action.property].length).toBe(0)
     expect(errorReducer('Errors')(defaultState, action)['foreign'].length).toBe(1)
+  })
+
+  it('matches errors from a different subsection', () => {
+    const defaultState = {
+      identification: [
+        {
+          section: 'identification',
+          subsection: 'othernames',
+          uid: '1',
+          code: 'something.looks.off',
+          valid: false
+        }
+      ]
+    }
+    const action = {
+      section: sectionName,
+      property: 'identification',
+      subsection: 'review',
+      values: [
+        {
+          section: 'identification',
+          subsection: 'review',
+          uid: '2',
+          code: 'something.looks.off',
+          valid: true
+        }
+      ]
+    }
+
+    const newState = errorReducer(sectionName)(defaultState, action)
+    const errors = newState[action.property]
+    expect(errors.length).toBe(1)
+    expect(errors[0].valid).toBe(true)
   })
 })
