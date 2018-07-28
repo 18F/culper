e148c8fb1384d71e4e3823419205baf6e57224d3
Aidan Feldman <aidan.feldman@gsa.gov>
2018-07-28 11:55:35 -0400

    add snapshot tests for Identification section

--- src/components/Section/Identification/Identification.test.jsx
+++ src/components/Section/Identification/Identification.test.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import renderer from 'react-test-renderer'
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
-import Identification from './Identification'
+import Identification, { IdentificationSections } from './Identification'
 import { mount } from 'enzyme'
 
 const applicationState = {
@@ -48,5 +49,25 @@ describe('The identification section', () => {
       const component = mount(<Provider store={store}><Identification subsection={section} /></Provider>)
       expect(component.find('div').length).toBeGreaterThan(0)
     })
+  })
+
+  it('renders the Identification component', () => {
+    const store = mockStore({
+      authentication: { authenticated: true },
+      application: applicationState
+    })
+    const component = renderer.create(
+      <Provider store={store}>
+        <Identification />
+      </Provider>
+    )
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders the IdentificationSections component', () => {
+    const component = renderer.create(<IdentificationSections />)
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
   })
 })
