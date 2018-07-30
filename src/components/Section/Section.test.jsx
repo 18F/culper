62d9149d7c2bb44329f7932998f534593e0a9b9c
Aidan Feldman <aidan.feldman@gsa.gov>
2018-07-30 14:42:21 -0400

    add a snapshot test for the Section component

--- src/components/Section/Section.test.jsx
+++ src/components/Section/Section.test.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import renderer from 'react-test-renderer'
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
@@ -36,5 +37,21 @@ describe('The section component', () => {
       </Provider>
     )
     expect(component.find('div').length > 0).toBe(true)
+  })
+
+  it('renders the Section component', () => {
+    window.token = 'fake-token'
+    const store = mockStore({
+      authentication: { authenticated: true, token: 'fake-token' }
+    })
+    const component = renderer.create(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Section section="identification" />
+        </MemoryRouter>
+      </Provider>
+    )
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
   })
 })
