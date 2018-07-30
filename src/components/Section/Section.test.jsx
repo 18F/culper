b1ba5f6fa34823a77c648bfd296b27fd0c27ddac
Aidan Feldman <aidan.feldman@gsa.gov>
2018-07-30 14:53:08 -0400

    remove need for AuthenticatedView on Section component

--- src/components/Section/Section.test.jsx
+++ src/components/Section/Section.test.jsx
@@ -1,40 +1,19 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import configureMockStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import { MemoryRouter } from 'react-router'
 import Section from './Section'
 import { mount } from 'enzyme'
 
 describe('The section component', () => {
-  // Setup
-  const middlewares = [thunk]
-  const mockStore = configureMockStore(middlewares)
+  const mockStore = configureMockStore()
 
-  it('hidden when not authenticated', () => {
-    const store = mockStore({ authentication: [] })
+  it('is visible', () => {
     const component = mount(
-      <Provider store={store}>
-        <MemoryRouter>
-          <Section />
-        </MemoryRouter>
-      </Provider>
-    )
-    expect(component.find('div').length).toEqual(0)
-  })
-
-  it('visible when authenticated', () => {
-    window.token = 'fake-token'
-    const store = mockStore({
-      authentication: { authenticated: true, token: 'fake-token' }
-    })
-    const component = mount(
-      <Provider store={store}>
-        <MemoryRouter>
-          <Section />
-        </MemoryRouter>
-      </Provider>
+      <MemoryRouter>
+        <Section />
+      </MemoryRouter>
     )
     expect(component.find('div').length > 0).toBe(true)
   })
