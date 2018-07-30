1fd26a82f5fc86f951ddd8a25b403659d46c603c
Aidan Feldman <aidan.feldman@gsa.gov>
2018-07-30 14:32:07 -0400

    format the Section tests

--- src/components/Section/Section.test.jsx
+++ src/components/Section/Section.test.jsx
@@ -8,19 +8,33 @@ import { mount } from 'enzyme'
 
 describe('The section component', () => {
   // Setup
-  const middlewares = [ thunk ]
+  const middlewares = [thunk]
   const mockStore = configureMockStore(middlewares)
 
   it('hidden when not authenticated', () => {
     const store = mockStore({ authentication: [] })
-    const component = mount(<Provider store={store}><MemoryRouter><Section /></MemoryRouter></Provider>)
+    const component = mount(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Section />
+        </MemoryRouter>
+      </Provider>
+    )
     expect(component.find('div').length).toEqual(0)
   })
 
   it('visible when authenticated', () => {
     window.token = 'fake-token'
-    const store = mockStore({ authentication: { authenticated: true, token: 'fake-token' } })
-    const component = mount(<Provider store={store}><MemoryRouter><Section /></MemoryRouter></Provider>)
+    const store = mockStore({
+      authentication: { authenticated: true, token: 'fake-token' }
+    })
+    const component = mount(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Section />
+        </MemoryRouter>
+      </Provider>
+    )
     expect(component.find('div').length > 0).toBe(true)
   })
 })
