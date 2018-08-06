3fdcdea82d3f0d156100e05a97edee1dbcc35fc8
Aidan Feldman <aidan.feldman@gsa.gov>
2018-08-06 16:01:35 -0500

    move store to component mapping to its own file

--- src/components/Section/Identification/Identification.jsx
+++ src/components/Section/Identification/Identification.jsx
@@ -13,23 +13,7 @@ import {
   createPrintSubsectionViews
 } from '../generators'
 import navigation from './navigation'
-import ApplicantName from './ApplicantName'
-import ApplicantSSN from './ApplicantSSN'
-import ApplicantBirthPlace from './ApplicantBirthPlace'
-import ApplicantBirthDate from './ApplicantBirthDate'
-import OtherNames from './OtherNames'
-import Physical from './Physical'
-import ContactInformation from './ContactInformation'
-
-const storeToComponentMap = {
-  ApplicantBirthDate,
-  ApplicantBirthPlace,
-  ApplicantName,
-  ApplicantSSN,
-  Contacts: ContactInformation,
-  OtherNames,
-  Physical
-}
+import storeToComponentMap from './subsections'
 
 class Identification extends SectionElement {
   getSubsectionProps(subsection) {
