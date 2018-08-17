dfba06f7684e0fabd9c49181ae37ddf38faa19bd
Aidan Feldman <aidan.feldman@gsa.gov>
2018-08-17 01:44:13 -0400

    check that rendered components have a minimum number of elements

--- src/components/test-helpers.js
+++ src/components/test-helpers.js
@@ -1,6 +1,11 @@
 import renderer from 'react-test-renderer'
+import { mount } from 'enzyme'
 
-export const testSnapshot = jsx => {
+export const testSnapshot = (jsx, minElements = 3) => {
+  // Sanity check to make sure the component is being rendered with the full subtree. Using Enzyme as a workaround for not having the find*() methods available in React 16
+  const enzymeComponent = mount(jsx)
+  expect(enzymeComponent.find('div').length).toBeGreaterThan(minElements)
+
   const component = renderer.create(jsx)
   let tree = component.toJSON()
   expect(tree).toMatchSnapshot()
