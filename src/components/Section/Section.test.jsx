10b7b9dc65239b66a8105aec19f21442fbeef194
Aidan Feldman <aidan.feldman@gsa.gov>
2018-08-17 08:51:46 -0400

    generate snapshots for every section+subsection (tests failing)
    ...rather than having snapshot tests for every section independently.

--- src/components/Section/Section.test.jsx
+++ src/components/Section/Section.test.jsx
@@ -5,6 +5,7 @@ import { MemoryRouter } from 'react-router'
 import Section from './Section'
 import { mount } from 'enzyme'
 import { testSnapshot } from '../test-helpers'
+import navigation from '../../config/navigation'
 
 // give a fake GUID so the field IDs don't differ between snapshots
 // https://github.com/facebook/jest/issues/936#issuecomment-404246102
@@ -26,17 +27,24 @@ describe('The section component', () => {
     expect(component.find('div').length > 0).toBe(true)
   })
 
-  it('renders the Section component at a particular subsection', () => {
-    window.token = 'fake-token'
-    const store = mockStore({
-      authentication: { authenticated: true, token: 'fake-token' }
+  navigation.forEach(section => {
+    if (section.exclude) {
+      return
+    }
+    section.subsections.forEach(subsection => {
+      window.token = 'fake-token'
+      it(`renders ${section.url}.${subsection.url}`, () => {
+        const store = mockStore({
+          authentication: { authenticated: true, token: 'fake-token' }
+        })
+        testSnapshot(
+          <Provider store={store}>
+            <MemoryRouter>
+              <Section section={section.url} subsection={subsection.url} />
+            </MemoryRouter>
+          </Provider>
+        )
+      })
     })
-    testSnapshot(
-      <Provider store={store}>
-        <MemoryRouter>
-          <Section section="identification" subsection="contacts" />
-        </MemoryRouter>
-      </Provider>
-    )
   })
 })
