ce2198738c46aab371b3b11d34b8ee43fe41b44f
Ryan Hofschneider <ryan.hofschneider@gsa.gov>
2018-08-30 00:38:00 -0700

    Fix encoding of police related Answer elements

--- api/xml/xml.go
+++ api/xml/xml.go
@@ -25,6 +25,7 @@ func (service Service) DefaultTemplate(templateName string, data map[string]inte
 	fmap := template.FuncMap{
 		"addressIn":              addressIn,
 		"branch":                 branch,
+		"branchToAnswer":         branchToAnswer,
 		"branchToBool":           branchToBool,
 		"branchcollectionHas":    branchcollectionHas,
 		"branchAny":              branchAny,
@@ -613,6 +614,17 @@ func branchToBool(data map[string]interface{}) string {
 		}
 	}
 	return "False"
+}
+
+func branchToAnswer(data map[string]interface{}) string {
+	props, ok := data["props"]
+	if ok {
+		val, ok := (props.(map[string]interface{}))["value"]
+		if ok && val == "Yes" {
+			return "Yes"
+		}
+	}
+	return "No"
 }
 
 func countryComments(data map[string]interface{}) string {
