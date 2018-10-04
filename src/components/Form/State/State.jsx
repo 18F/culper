dbd784153dee29d4d35c3324095d46b61a94bef5
Christopher Hui <christopher.kam.hui@gmail.com>
2018-10-03 19:45:49 -0700

    move states out of render and concatenate internal states and children states

--- src/components/Form/State/State.jsx
+++ src/components/Form/State/State.jsx
@@ -5,7 +5,92 @@ import Dropdown from '../Dropdown'
 export default class State extends ValidationElement {
   constructor(props) {
     super(props)
+    this.getStates = this.getStates.bind(this)
     this.handleError = this.handleError.bind(this)
+
+    this.states = [
+      { name: 'Alabama', abbreviation: 'AL' },
+      { name: 'Alaska', abbreviation: 'AK' },
+      { name: 'Arizona', abbreviation: 'AZ' },
+      { name: 'Arkansas', abbreviation: 'AR' },
+      { name: 'California', abbreviation: 'CA' },
+      { name: 'Colorado', abbreviation: 'CO' },
+      { name: 'Connecticut', abbreviation: 'CT' },
+      { name: 'Delaware', abbreviation: 'DE' },
+      { name: 'Washington D.C.', abbreviation: 'DC' },
+      { name: 'Florida', abbreviation: 'FL' },
+      { name: 'Georgia', abbreviation: 'GA' },
+      { name: 'Hawaii', abbreviation: 'HI' },
+      { name: 'Idaho', abbreviation: 'ID' },
+      { name: 'Illinois', abbreviation: 'IL' },
+      { name: 'Indiana', abbreviation: 'IN' },
+      { name: 'Iowa', abbreviation: 'IA' },
+      { name: 'Kansas', abbreviation: 'KS' },
+      { name: 'Kentucky', abbreviation: 'KY' },
+      { name: 'Louisiana', abbreviation: 'LA' },
+      { name: 'Maine', abbreviation: 'ME' },
+      { name: 'Maryland', abbreviation: 'MD' },
+      { name: 'Massachusetts', abbreviation: 'MA' },
+      { name: 'Michigan', abbreviation: 'MI' },
+      { name: 'Minnesota', abbreviation: 'MN' },
+      { name: 'Mississippi', abbreviation: 'MS' },
+      { name: 'Missouri', abbreviation: 'MO' },
+      { name: 'Montana', abbreviation: 'MT' },
+      { name: 'Nebraska', abbreviation: 'NE' },
+      { name: 'Nevada', abbreviation: 'NV' },
+      { name: 'New Hampsire', abbreviation: 'NH' },
+      { name: 'New Jersey', abbreviation: 'NJ' },
+      { name: 'New Mexico', abbreviation: 'NM' },
+      { name: 'New York', abbreviation: 'NY' },
+      { name: 'North Carolina', abbreviation: 'NC' },
+      { name: 'North Dakota', abbreviation: 'ND' },
+      { name: 'Ohio', abbreviation: 'OH' },
+      { name: 'Oklahoma', abbreviation: 'OK' },
+      { name: 'Oregon', abbreviation: 'OR' },
+      { name: 'Pennsylvania', abbreviation: 'PA' },
+      { name: 'Rhode Island', abbreviation: 'RI' },
+      { name: 'South Carolina', abbreviation: 'SC' },
+      { name: 'South Dakota', abbreviation: 'SD' },
+      { name: 'Tennessee', abbreviation: 'TN' },
+      { name: 'Texas', abbreviation: 'TX' },
+      { name: 'Utah', abbreviation: 'UT' },
+      { name: 'Vermont', abbreviation: 'VT' },
+      { name: 'Virginia', abbreviation: 'VA' },
+      { name: 'Washington', abbreviation: 'WA' },
+      { name: 'West Virginia', abbreviation: 'WV' },
+      { name: 'Wisconsin', abbreviation: 'WI' },
+      { name: 'Wyoming', abbreviation: 'WY' },
+      { name: 'American Samoa', abbreviation: 'AS' },
+      { name: 'FQ', abbreviation: 'FQ' },
+      { name: 'Guan', abbreviation: 'GU' },
+      { name: 'HQ', abbreviation: 'HQ' },
+      { name: 'DQ', abbreviation: 'DQ' },
+      { name: 'JQ', abbreviation: 'JQ' },
+      { name: 'KQ', abbreviation: 'KQ' },
+      { name: 'Marshall Islands', abbreviation: 'MH' },
+      { name: 'Micronesia', abbreviation: 'FM' },
+      { name: 'MQ', abbreviation: 'MQ' },
+      { name: 'BQ', abbreviation: 'BQ' },
+      { name: 'Northern Mariana Islands', abbreviation: 'MP' },
+      { name: 'Palau', abbreviation: 'PW' },
+      { name: 'LQ', abbreviation: 'LQ' },
+      { name: 'Puerto Rico', abbreviation: 'PR' },
+      { name: 'Virgin Islands', abbreviation: 'VI' },
+      { name: 'WQ', abbreviation: 'WQ' }
+    ]
+  }
+
+  // Gets all internally stored states, and children states to spread in the render
+  getStates() {
+    console.log(this.states)
+    console.log(this.props.children)
+    const states = this.states.map(state => (
+      <option key={state.name} value={state.abbreviation}>
+        {state.name}
+      </option>
+    ))
+
+    return [...states, ...this.props.children]
   }
 
   handleError(value, arr) {
@@ -38,211 +123,7 @@ export default class State extends ValidationElement {
         tabBack={this.props.tabBack}
         tabNext={this.props.tabNext}
         receiveProps={true}>
-        <option key="Alabama" value="AL">
-          Alabama
-        </option>
-        <option key="Alaska" value="AK">
-          Alaska
-        </option>
-        <option key="Arizona" value="AZ">
-          Arizona
-        </option>
-        <option key="Arkansas" value="AR">
-          Arkansas
-        </option>
-        <option key="California" value="CA">
-          California
-        </option>
-        <option key="Colorado" value="CO">
-          Colorado
-        </option>
-        <option key="Connecticut" value="CT">
-          Connecticut
-        </option>
-        <option key="Delaware" value="DE">
-          Delaware
-        </option>
-        <option key="Washington D.C." value="DC">
-          Washington D.C.
-        </option>
-        <option key="Florida" value="FL">
-          Florida
-        </option>
-        <option key="Georgia" value="GA">
-          Georgia
-        </option>
-        <option key="Hawaii" value="HI">
-          Hawaii
-        </option>
-        <option key="Idaho" value="ID">
-          Idaho
-        </option>
-        <option key="Illinois" value="IL">
-          Illinois
-        </option>
-        <option key="Indiana" value="IN">
-          Indiana
-        </option>
-        <option key="Iowa" value="IA">
-          Iowa
-        </option>
-        <option key="Kansas" value="KS">
-          Kansas
-        </option>
-        <option key="Kentucky" value="KY">
-          Kentucky
-        </option>
-        <option key="Louisiana" value="LA">
-          Louisiana
-        </option>
-        <option key="Maine" value="ME">
-          Maine
-        </option>
-        <option key="Maryland" value="MD">
-          Maryland
-        </option>
-        <option key="Massachusetts" value="MA">
-          Massachusetts
-        </option>
-        <option key="Michigan" value="MI">
-          Michigan
-        </option>
-        <option key="Minnesota" value="MN">
-          Minnesota
-        </option>
-        <option key="Mississippi" value="MS">
-          Mississippi
-        </option>
-        <option key="Missouri" value="MO">
-          Missouri
-        </option>
-        <option key="Montana" value="MT">
-          Montana
-        </option>
-        <option key="Nebraska" value="NE">
-          Nebraska
-        </option>
-        <option key="Nevada" value="NV">
-          Nevada
-        </option>
-        <option key="New Hampshire" value="NH">
-          New Hampshire
-        </option>
-        <option key="New Jersey" value="NJ">
-          New Jersey
-        </option>
-        <option key="New Mexico" value="NM">
-          New Mexico
-        </option>
-        <option key="New York" value="NY">
-          New York
-        </option>
-        <option key="North Carolina" value="NC">
-          North Carolina
-        </option>
-        <option key="North Dakota" value="ND">
-          North Dakota
-        </option>
-        <option key="Ohio" value="OH">
-          Ohio
-        </option>
-        <option key="Oklahoma" value="OK">
-          Oklahoma
-        </option>
-        <option key="Oregon" value="OR">
-          Oregon
-        </option>
-        <option key="Pennsylvania" value="PA">
-          Pennsylvania
-        </option>
-        <option key="Rhode Island" value="RI">
-          Rhode Island
-        </option>
-        <option key="South Carolina" value="SC">
-          South Carolina
-        </option>
-        <option key="South Dakota" value="SD">
-          South Dakota
-        </option>
-        <option key="Tennessee" value="TN">
-          Tennessee
-        </option>
-        <option key="Texas" value="TX">
-          Texas
-        </option>
-        <option key="Utah" value="UT">
-          Utah
-        </option>
-        <option key="Vermont" value="VT">
-          Vermont
-        </option>
-        <option key="Virginia" value="VA">
-          Virginia
-        </option>
-        <option key="Washington" value="WA">
-          Washington
-        </option>
-        <option key="West Virginia" value="WV">
-          West Virginia
-        </option>
-        <option key="Wisconsin" value="WI">
-          Wisconsin
-        </option>
-        <option key="Wyoming" value="WY">
-          Wyoming
-        </option>
-        <option key="American Samoa" value="AS">
-          American Samoa
-        </option>
-        <option key="FQ" value="FQ">
-          FQ
-        </option>
-        <option key="Guam" value="GU">
-          Guam
-        </option>
-        <option key="HQ" value="HQ">
-          HQ
-        </option>
-        <option key="DQ" value="DQ">
-          DQ
-        </option>
-        <option key="JQ" value="JQ">
-          JQ
-        </option>
-        <option key="KQ" value="KQ">
-          KQ
-        </option>
-        <option key="Marshall Islands" value="MH">
-          Marshall Islands
-        </option>
-        <option key="Micronesia" value="FM">
-          Micronesia
-        </option>
-        <option key="MQ" value="MQ">
-          MQ
-        </option>
-        <option key="BQ" value="BQ">
-          BQ
-        </option>
-        <option key="Northern Mariana Islands" value="MP">
-          Northern Mariana Islands
-        </option>
-        <option key="Palau" value="PW">
-          Palau
-        </option>
-        <option key="LQ" value="LQ">
-          LQ
-        </option>
-        <option key="Puerto Rico" value="PR">
-          Puerto Rico
-        </option>
-        <option key="Virgin Islands" value="VI">
-          Virgin Islands
-        </option>
-        <option key="WQ" value="WQ">
-          WQ
-        </option>
-        {this.props.children}
+        {this.getStates()}
       </Dropdown>
     )
   }
