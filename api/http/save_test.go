96b252f337cc3872c01a6912719ba32569adc353
MacRae Linton <55759+macrael@users.noreply.github.com>
2018-10-19 16:58:14 -0700

    future proof errors

--- api/http/save_test.go
+++ api/http/save_test.go
@@ -120,7 +120,7 @@ func TestSaveHandlerBadEntity(t *testing.T) {
     }
 
     // Check the response body is what we expect.
-    expected := `{"Errors":"Error converting payload into valid entity"}` + "\n"
+    expected := `{"errors":[{"message":"Error converting payload into valid entity"}]}` + "\n"
     if rr.Body.String() != expected {
         t.Errorf("handler returned unexpected body: got %v want %v",
             rr.Body.String(), expected)
