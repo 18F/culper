5e4a6c514549dc8b3b7d6686ff5455bdf8f6bd5a
Scott Weber <stephen.weber@gsa.gov>
2018-10-24 09:32:01 -0400

    change foreign travel days to use the radiogroup control

--- src/components/Section/Foreign/Travel/TravelDays.jsx
+++ src/components/Section/Foreign/Travel/TravelDays.jsx
@@ -1,93 +1,90 @@
 import React from 'react'
 import { i18n } from '../../../../config'
-import { ValidationElement, CheckboxGroup, Checkbox } from '../../../Form'
+import { ValidationElement, RadioGroup, Radio } from '../../../Form'
 
 export default class TravelDays extends ValidationElement {
   constructor(props) {
     super(props)
     this.update = this.update.bind(this)
+    this.updateTravelDays = this.updateTravelDays.bind(this)
   }
 
-  update(response) {
-    if (!this.props.onUpdate) {
-      return
+  update(updateValues) {
+    if (this.props.onUpdate) {
+      this.props.onUpdate({
+        value: this.props.value,
+        ...updateValues
+      })
     }
+  }
 
-    let selected = response.value
-    let list = [...(this.props.values || [])]
-
-    if (list.includes(selected)) {
-      list.splice(list.indexOf(selected), 1)
-    } else {
-      list.push(selected)
-    }
-
-    this.props.onUpdate({ values: list })
+  updateTravelDays(values) {
+    this.update({ value: values.value })
   }
 
   render() {
+    const selected = this.props.value
     return (
-      <CheckboxGroup
+      <RadioGroup
         className={`travel-days ${this.props.className || ''}`.trim()}
         onError={this.props.onError}
         required={this.props.required}
-        selectedValues={this.props.values || []}>
-        <Checkbox
+        selectedValue={selected}>
+        <Radio
           name="days-1-5"
           label={i18n.m('foreign.travel.label.one')}
           value="1-5"
           className="days-1-5"
-          onUpdate={this.update}
+          onUpdate={this.updateTravelDays}
           onError={this.props.onError}
         />
-        <Checkbox
+        <Radio
           name="days-6-10"
           label={i18n.m('foreign.travel.label.six')}
           value="6-10"
           className="days-6-10"
-          onUpdate={this.update}
+          onUpdate={this.updateTravelDays}
           onError={this.props.onError}
         />
-        <Checkbox
+        <Radio
           name="days-11-20"
           label={i18n.m('foreign.travel.label.eleven')}
           value="11-20"
           className="days-11-20"
-          onUpdate={this.update}
+          onUpdate={this.updateTravelDays}
           onError={this.props.onError}
         />
-        <Checkbox
+        <Radio
           name="days-21-30"
           label={i18n.m('foreign.travel.label.twentyone')}
           value="21-30"
           className="days-21-30"
-          onUpdate={this.update}
+          onUpdate={this.updateTravelDays}
           onError={this.props.onError}
         />
-        <Checkbox
+        <Radio
           name="more-than-30"
           label={i18n.m('foreign.travel.label.more')}
           value="More than 30"
           className="more-than-30"
-          onUpdate={this.update}
+          onUpdate={this.updateTravelDays}
           onError={this.props.onError}
         />
-        <Checkbox
+        <Radio
           name="many-short-trips"
           label={i18n.m('foreign.travel.label.many')}
           value="Many short trips"
           className="many-short-trips"
-          onUpdate={this.update}
+          onUpdate={this.updateTravelDays}
           onError={this.props.onError}
         />
-      </CheckboxGroup>
+      </RadioGroup>
     )
   }
 }
 
 TravelDays.defaultProps = {
-  values: [],
-  onUpdate: queue => {},
+  value: '',
   onError: (value, arr) => {
     return arr
   }
