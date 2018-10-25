9b0452a1da5299f70852f878ee97f427d57d08c8
Scott Weber <stephen.weber@gsa.gov>
2018-10-25 14:38:04 -0400

    Remove extra update function

--- src/components/Section/Foreign/Travel/TravelDays.jsx
+++ src/components/Section/Foreign/Travel/TravelDays.jsx
@@ -6,20 +6,11 @@ export default class TravelDays extends ValidationElement {
   constructor(props) {
     super(props)
     this.update = this.update.bind(this)
-    this.updateTravelDays = this.updateTravelDays.bind(this)
   }
 
-  update(updateValues) {
-    if (this.props.onUpdate) {
-      this.props.onUpdate({
-        value: this.props.value,
-        ...updateValues
-      })
-    }
-  }
-
-  updateTravelDays(values) {
-    this.update({ value: values.value })
+  update(values) {
+    const { onUpdate } = this.props;
+    onUpdate && onUpdate({ value: values.value })
   }
 
   render() {
@@ -35,7 +26,7 @@ export default class TravelDays extends ValidationElement {
           label={i18n.m('foreign.travel.label.one')}
           value="1-5"
           className="days-1-5"
-          onUpdate={this.updateTravelDays}
+          onUpdate={this.update}
           onError={this.props.onError}
         />
         <Radio
@@ -43,7 +34,7 @@ export default class TravelDays extends ValidationElement {
           label={i18n.m('foreign.travel.label.six')}
           value="6-10"
           className="days-6-10"
-          onUpdate={this.updateTravelDays}
+          onUpdate={this.update}
           onError={this.props.onError}
         />
         <Radio
@@ -51,7 +42,7 @@ export default class TravelDays extends ValidationElement {
           label={i18n.m('foreign.travel.label.eleven')}
           value="11-20"
           className="days-11-20"
-          onUpdate={this.updateTravelDays}
+          onUpdate={this.update}
           onError={this.props.onError}
         />
         <Radio
@@ -59,7 +50,7 @@ export default class TravelDays extends ValidationElement {
           label={i18n.m('foreign.travel.label.twentyone')}
           value="21-30"
           className="days-21-30"
-          onUpdate={this.updateTravelDays}
+          onUpdate={this.update}
           onError={this.props.onError}
         />
         <Radio
@@ -67,7 +58,7 @@ export default class TravelDays extends ValidationElement {
           label={i18n.m('foreign.travel.label.more')}
           value="More than 30"
           className="more-than-30"
-          onUpdate={this.updateTravelDays}
+          onUpdate={this.update}
           onError={this.props.onError}
         />
         <Radio
@@ -75,7 +66,7 @@ export default class TravelDays extends ValidationElement {
           label={i18n.m('foreign.travel.label.many')}
           value="Many short trips"
           className="many-short-trips"
-          onUpdate={this.updateTravelDays}
+          onUpdate={this.update}
           onError={this.props.onError}
         />
       </RadioGroup>
