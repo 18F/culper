a7e89f79c3b7df201d215276fefe31e71f38b0c7
MacRae Linton <55759+macrael@users.noreply.github.com>
2018-11-05 16:48:25 -0800

    fix space/tabs

--- api/bin/live_reload.sh
+++ api/bin/live_reload.sh
@@ -19,18 +19,18 @@ trap sigint_handler SIGINT
 
 while true; do
 
-	if make build; then
-		./api 2>&1 &
-	  PID=$!
-	else
-		PID=-1
-	fi
+  if make build; then
+    ./api 2>&1 &
+    PID=$!
+  else
+    PID=-1
+  fi
   # inotifiywait runs until it hears a file event in this directory or any recursive subdirectories.
   # when it does, we kill the running server and start the loop over again.
   # if this script is looping on end, that likely means that we are touching some file other than
   # ./api on `make build.` you can add other files that should be ignored to the --exclude regex.
   inotifywait --exclude "(api$|\\.sh$)" -e modify -e move -e create -e delete -e attrib -r .
   if [ "$PID" -ne -1 ]; then
-	  kill "$PID"
-	fi
+    kill "$PID"
+  fi
 done
