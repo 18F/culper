22c2cefa3ceb849a5ca41165f0967a73ad94e8b6
Adam Biagianti <adam.biagianti@gsa.gov>
2018-11-08 10:56:27 -0800

    Remove prop enumeration
    * Since the data is formatted and filtered before submission to the
    server, there isnt a need to enumerate the props we want to update
    
    * This also means we don't have to remember to add props to the
    enumerated object

--- src/components/Section/History/Employment/EmploymentItem.jsx
+++ src/components/Section/History/Employment/EmploymentItem.jsx
@@ -45,22 +45,7 @@ export default class EmploymentItem extends ValidationElement {
 
   update(queue) {
     this.props.onUpdate({
-      EmploymentActivity: this.props.EmploymentActivity,
-      Employment: this.props.Employment,
-      Dates: this.props.Dates,
-      Title: this.props.Title,
-      DutyStation: this.props.DutyStation,
-      Status: this.props.Status,
-      Address: this.props.Address,
-      Telephone: this.props.Telephone,
-      Supervisor: this.props.Supervisor,
-      ReferenceName: this.props.ReferenceName,
-      ReferencePhone: this.props.ReferencePhone,
-      ReferenceAddress: this.props.ReferenceAddress,
-      PhysicalAddress: this.props.PhysicalAddress,
-      Additional: this.props.Additional,
-      ReasonLeft: this.props.ReasonLeft,
-      Reprimand: this.props.Reprimand,
+      ...this.props,
       ...queue
     })
   }
