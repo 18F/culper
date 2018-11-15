9b793e1f3a9dba20e51a1581b1d571837dc98d95
Scott Weber <stephen.weber@gsa.gov>
2018-11-15 15:56:48 -0500

    Test tax dates

--- src/components/Section/Financial/Taxes/Taxes.test.jsx
+++ src/components/Section/Financial/Taxes/Taxes.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import Taxes from './Taxes'
+import { i18n } from '../../../../config'
 
 describe('The taxes component', () => {
   it('no error on empty', () => {
@@ -75,4 +76,74 @@ describe('The taxes component', () => {
       .simulate('change', { target: { value: 'Description for not filing' } })
     expect(updates).toBeGreaterThan(7)
   })
+
+  describe('handles tax dates', () => {
+    it('with good data', () => {
+      const props = {
+        HasTaxes: {
+          name: "has_taxes",
+          value: "Yes"
+        },
+        List: {
+          items: [{
+            Item: {
+              Year: {
+                estimated: false,
+                name: "Year",
+                year: "2000",
+                date: new Date("2000", "1")
+              },
+              Date: {
+                estimated: false,
+                month: "1",
+                name: "Date",
+                year: "2010",
+                date: new Date("2010", "1")
+              },
+          },
+          open: true
+        }]
+      },
+        valid: true
+      }
+
+      const component = mount(<Taxes {...props} />)
+      expect(component.find('.error-messages [data-i18n="error.taxesSatisfied.min"]').children().length).toEqual(0)
+    })
+    it('with bad data', () => {
+      const props = {
+        HasTaxes: {
+          name: "has_taxes",
+          value: "Yes"
+        },
+        List: {
+          items: [{
+            Item: {
+              Year: {
+                estimated: false,
+                name: "Year",
+                year: "2000",
+                date: new Date("2000", "1")
+              },
+              Date: {
+                estimated: false,
+                month: "1",
+                name: "Date",
+                year: "1970",
+                date: new Date("1970", "1")
+              },
+          },
+          open: true
+        }]
+      },
+        valid: false
+      }
+
+      const component = mount(<Taxes {...props} />)
+      expect(component.find('.error-messages [data-i18n="error.taxesSatisfied.min"]').text()).toEqual(
+        `${i18n.t('error.taxesSatisfied.min.title')}${i18n.t('error.taxesSatisfied.min.message')}`
+      )
+    })
+  })
+
 })
--- src/components/Section/Financial/Taxes/TaxesItem.test.jsx
+++ src/components/Section/Financial/Taxes/TaxesItem.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { i18n } from '../../../../config'
 import { mount } from 'enzyme'
+import Taxes from './Taxes'
 import TaxesItem from './TaxesItem'
 
 describe('The taxes item component', () => {
@@ -39,85 +39,4 @@ describe('The taxes item component', () => {
       .simulate('change')
     expect(updates).toBe(8)
   })
-
-  describe.only('handles tax dates', () => {
-
-    function handleError(code, value, arr) {
-      arr = arr.map(err => {
-        return {
-          code: `taxSatisfied.min`,
-          valid: err.valid,
-          uid: err.uid
-        }
-      })
-
-      // Take the original and concatenate our new error values to it
-      return this.props.onError(value, arr)
-    }
-
-    it('with good data', () => {
-      const params = {
-        Year: {
-          day: "1",
-          estimated: false,
-          month: "1",
-          name: "Year",
-          year: "2000"
-        },
-        Date: {
-          day: "1",
-          estimated: false,
-          month: "1",
-          name: "Date",
-          year: "2010"
-        },
-        valid: true
-      }
-
-      const component = mount(<TaxesItem {...params} />)
-      const year = component.find('.taxes-year .year input')
-      year.simulate('change', {target: { value: "2000" }})
-      component.find('.taxes-date-notapplicable .month input')
-      component.find('.taxes-date-notapplicable .year input')
-      expect(component.find('.taxes-date-notapplicable .error-messages').children().length).toEqual(0)
-    })
-    it('with bad data', () => {
-      const params = {
-        Year: {
-          day: "1",
-          estimated: false,
-          month: "1",
-          name: "Year",
-          year: "2000"
-        },
-        Date: {
-          day: "1",
-          estimated: false,
-          month: "1",
-          name: "Date",
-          year: "1998"
-        },
-        onError: (code, value, arr) => handleError()(code, value, arr),
-        valid: false
-      }
-
-      const component = mount(<TaxesItem {...params} />)
-      // component.find('.taxes-year .year input').simulate('change')
-      // component.find('.taxes-date-notapplicable .month input').simulate('change')
-      // component.find('.taxes-date-notapplicable .year input').simulate('change')
-      //
-      // console.log(component.find('.taxes-year .year input').html())
-      // console.log(component.find('.taxes-date-notapplicable .year input').html())
-      //
-      // console.log(component.find('.taxes-date-notapplicable .year input').html())
-      // console.log(component.find('.messages.error-messages').map(x => x.html()))
-      expect(component.find('.error-messages [data-i18n="error.taxesSatisfied.min"]').text()).toEqual(
-        i18n.t('error.taxesSatisfied.min.title')
-      )
-      expect(component.find('.taxes-date-notapplicable .error-messages .usa-alert-body > p').text()).toEqual(
-        i18n.t('error.taxesSatisfied.min.message')
-      )
-    })
-  })
-
 })
