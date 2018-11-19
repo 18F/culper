c6de1c7641ba6c6780edf9c73a2118da118828ed
Scott Weber <stephen.weber@gsa.gov>
2018-11-19 10:36:36 -0500

    Update marial, divorce and cohabitant date tests

--- src/components/Section/Relationships/RelationshipStatus/Cohabitants.test.jsx
+++ src/components/Section/Relationships/RelationshipStatus/Cohabitants.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import Cohabitants from './Cohabitants'
+import { i18n } from '../../../../config'
 
 describe('The cohabitants component', () => {
   it('no error on empty', () => {
@@ -51,4 +52,97 @@ describe('The cohabitants component', () => {
     component.find('.has-cohabitant .yes input').simulate('change')
     expect(updates).toBe(1)
   })
+
+  describe('handles cohanbitant dates', () => {
+    it('with good data', () => {
+      const props = {
+        name: "marital",
+        HasCohabitant: {
+            value: 'Yes'
+          },
+        applicantBirthdate: {
+          estimated: false,
+          day: "1",
+          month: "1",
+          name: "birthdate",
+          year: "1970",
+          date: new Date("1970", "1", "1")
+        },
+        CohabitantList: {
+          items: [{
+            Item: {
+              Birthdate: {
+                estimated: false,
+                day: "1",
+                month: "1",
+                name: "birthdate",
+                year: "1980",
+                date: new Date("1980", "1", "1")
+              },
+              CohabitationBegan: {
+                estimated: false,
+                day: "1",
+                month: "1",
+                name: "cohabitationBegan",
+                year: "1990",
+                date: new Date("1990", "1", "1")
+              },
+          },
+          open: true
+        }]
+      },
+        valid: true
+      }
+
+      const component =  mount(<Cohabitants {...props} />)
+      expect(component.find('.error-messages [data-i18n="error.cohabitant.min"]').children().length).toEqual(0)
+    })
+    it('with bad data', () => {
+      const props = {
+        name: "marital",
+        HasCohabitant: {
+            value: 'Yes'
+          },
+        applicantBirthdate: {
+          estimated: false,
+          day: "1",
+          month: "1",
+          name: "birthdate",
+          year: "1970",
+          date: new Date("1970", "1", "1")
+        },
+        CohabitantList: {
+          items: [{
+            Item: {
+              Birthdate: {
+                estimated: false,
+                day: "1",
+                month: "1",
+                name: "birthdate",
+                year: "1980",
+                date: new Date("1980", "1", "1")
+              },
+              CohabitationBegan: {
+                estimated: false,
+                day: "1",
+                month: "1",
+                name: "cohabitationBegan",
+                year: "1960",
+                date: new Date("1960", "1", "1")
+              },
+          },
+          open: true
+        }]
+      },
+        valid: false
+      }
+
+      const component =  mount(<Cohabitants {...props} />)
+      expect(component.find('.error-messages [data-i18n="error.cohabitant.min"]').text()).toEqual(
+        `${i18n.t('error.cohabitant.min.title')}${i18n.t('error.cohabitant.min.message')}`
+      )
+    })
+  })
+
+
 })
--- src/components/Section/Relationships/RelationshipStatus/Marital.test.jsx
+++ src/components/Section/Relationships/RelationshipStatus/Marital.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import Marital from './Marital'
+import { i18n } from '../../../../config'
 
 describe('The relationship status component', () => {
   it('no error on empty', () => {
@@ -62,4 +63,197 @@ describe('The relationship status component', () => {
     expect(updates).toBe(2)
     expect(component.find('.accordion').length).toBe(1)
   })
+
+  describe('handles civil union dates', () => {
+    it('with good data', () => {
+      const props = {
+        name: "marital",
+        Status: {
+            checked: true,
+            value: 'Married'
+          },
+        applicantBirthdate: {
+          estimated: false,
+          day: "1",
+          month: "1",
+          name: "birthdate",
+          year: "1970",
+          date: new Date("1970", "1", "1")
+        },
+        CivilUnion: {
+              Birthdate: {
+                estimated: false,
+                day: "1",
+                month: "1",
+                name: "birthdate",
+                year: "1980",
+                date: new Date("1980", "1", "1")
+              },
+              EnteredCivilUnion: {
+                estimated: false,
+                day: "1",
+                month: "1",
+                name: "enteredCivilUnion",
+                year: "1990",
+                date: new Date("1990", "1", "1")
+              },
+            },
+        valid: true
+      }
+
+      const component =  mount(<Marital {...props} />)
+      expect(component.find('.error-messages [data-i18n="error.civilUnion.min"]').children().length).toEqual(0)
+    })
+    it('with bad data', () => {
+      const props = {
+        name: "marital",
+        Status: {
+            checked: true,
+            value: 'Married'
+          },
+        applicantBirthdate: {
+          estimated: false,
+          day: "1",
+          month: "1",
+          name: "birthdate",
+          year: "1970",
+          date: new Date("1970", "1", "1")
+        },
+        CivilUnion: {
+              Birthdate: {
+                estimated: false,
+                day: "1",
+                month: "1",
+                name: "birthdate",
+                year: "1980",
+                date: new Date("1980", "1", "1")
+              },
+              EnteredCivilUnion: {
+                estimated: false,
+                day: "1",
+                month: "1",
+                name: "enteredCivilUnion",
+                year: "1960",
+                date: new Date("1960", "1", "1")
+              },
+            },
+        valid: false
+      }
+
+      const component =  mount(<Marital {...props} />)
+      expect(component.find('.error-messages [data-i18n="error.civilUnion.min"]').text()).toEqual(
+        `${i18n.t('error.civilUnion.min.title')}${i18n.t('error.civilUnion.min.message')}`
+      )
+    })
+  })
+
+  describe('handles divorce dates', () => {
+    it('with good data', () => {
+      const props = {
+        name: "marital",
+        Status: {
+            checked: true,
+            value: 'Divorced'
+          },
+        applicantBirthdate: {
+          estimated: false,
+          day: "1",
+          month: "1",
+          name: "birthdate",
+          year: "1970",
+          date: new Date("1970", "1", "1")
+        },
+        DivorcedList: {
+          items: [{
+            Item: {
+              Birthdate: {
+                estimated: false,
+                day: "1",
+                month: "1",
+                name: "birthdate",
+                year: "1980",
+                date: new Date("1980", "1", "1")
+              },
+              Recognized: {
+                estimated: false,
+                day: "1",
+                month: "1",
+                name: "Recognized",
+                year: "1990",
+                date: new Date("1990", "1", "1")
+              },
+              DateDivorced: {
+                estimated: false,
+                day: "1",
+                month: "1",
+                name: "DateDivorced",
+                year: "2000",
+                date: new Date("2000", "1", "1")
+              },
+            },
+          open: true
+        }],
+      },
+        valid: true
+      }
+
+      const component =  mount(<Marital {...props} />)
+      expect(component.find('.error-messages [data-i18n="error.divorceDate.min"]').children().length).toEqual(0)
+    })
+    it('with bad data', () => {
+      const props = {
+        name: "marital",
+        Status: {
+            checked: true,
+            value: 'Divorced'
+          },
+        applicantBirthdate: {
+          estimated: false,
+          day: "1",
+          month: "1",
+          name: "birthdate",
+          year: "1970",
+          date: new Date("1970", "1", "1")
+        },
+        DivorcedList: {
+          items: [{
+            Item: {
+              Birthdate: {
+                estimated: false,
+                day: "1",
+                month: "1",
+                name: "birthdate",
+                year: "1980",
+                date: new Date("1980", "1", "1")
+              },
+              Recognized: {
+                estimated: false,
+                day: "1",
+                month: "1",
+                name: "Recognized",
+                year: "1990",
+                date: new Date("1990", "1", "1")
+              },
+              DateDivorced: {
+                estimated: false,
+                day: "1",
+                month: "1",
+                name: "DateDivorced",
+                year: "1950",
+                date: new Date("1950", "1", "1")
+              },
+            },
+          open: true
+        }],
+      },
+        valid: false
+      }
+
+      const component =  mount(<Marital {...props} />)
+      expect(component.find('.error-messages [data-i18n="error.divorceDate.min"]').text()).toEqual(
+        `${i18n.t('error.divorceDate.min.title')}${i18n.t('error.divorceDate.min.message')}`
+      )
+    })
+  })
+
 })
