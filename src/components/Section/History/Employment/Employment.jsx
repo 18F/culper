a0519a31bcbb41ff33e432dcb9262466a694e01e
Christopher Hui <christopher.kam.hui@gmail.com>
2018-11-26 13:14:01 -0800

    implement sort for employment items

--- src/components/Section/History/Employment/Employment.jsx
+++ src/components/Section/History/Employment/Employment.jsx
@@ -61,7 +61,21 @@ export default class Employment extends SubsectionElement {
   }
 
   sortEmploymentItems(employmentItems) {
-    console.log(employmentItems)
+    return employmentItems.sort((a, b)  => {
+      if (
+        a.Item &&
+        a.Item.Dates &&
+        a.Item.Dates.from
+      ) {
+        const aDateObj = a.Item.Dates.from
+        const bDateObj = b.Item.Dates.from
+        const aDate = new Date(aDateObj.year, aDateObj.month - 1, aDateObj.day)
+        const bDate = new Date(bDateObj.year, bDateObj.month - 1, bDateObj.day)
+
+        return bDate.getTime() - aDate.getTime()
+      }
+      return 0
+    })
   }
 
   update(queue) {
@@ -72,10 +86,12 @@ export default class Employment extends SubsectionElement {
       ...queue
     }
 
-    this.sortEmploymentItems(queue.List)
-    // if (queue.List) {
-    //   updatedValues.List = this.sortEmploymentItems(queue.List)
-    // }
+    if (queue.List) {
+      updatedValues.List = {
+        ...queue.List,
+        items: this.sortEmploymentItems(queue.List.items)
+      }
+    }
 
     this.props.onUpdate(updatedValues)
   }
