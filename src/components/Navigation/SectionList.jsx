4f692163d5b4b2dc8c82f9fcc3d2f87a770bd8d9
Brian Hurst <brian.hurst@gsa.gov>
2019-01-09 12:36:55 -0500

    Implement hidden logic in navigation

--- src/components/Navigation/SectionList.jsx
+++ src/components/Navigation/SectionList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import SectionLink from './SectionLink'
 import ToggleItem from './ToggleItem'
@@ -6,24 +7,36 @@ import ToggleItem from './ToggleItem'
 class SectionList extends React.Component {
   render() {
     const className = `usa-accordion ${this.props.className}`
-    const navItems = this.props.sections.map(section => {
-      if (section.subsections) {
+    console.log(this.props.sections)
+    const navItems = this.props.sections
+      .filter(section => {
+        if (
+          section.hidden ||
+          (section.hiddenFunc && section.hiddenFunc(this.props.application))
+        ) {
+          return false
+        }
+
+        return true
+      })
+      .map(section => {
+        if (section.subsections) {
+          return (
+            <ToggleItem
+              key={section.url}
+              baseUrl={this.props.baseUrl}
+              section={section}
+            />
+          )
+        }
         return (
-          <ToggleItem
+          <SectionLink
             key={section.url}
             baseUrl={this.props.baseUrl}
             section={section}
           />
         )
-      }
-      return (
-        <SectionLink
-          key={section.url}
-          baseUrl={this.props.baseUrl}
-          section={section}
-        />
-      )
-    })
+      })
 
     return <ol className={className}>{navItems}</ol>
   }
@@ -32,6 +45,9 @@ class SectionList extends React.Component {
 SectionList.propTypes = {
   baseUrl: PropTypes.string,
   className: PropTypes.string,
+  application: PropTypes.object.isRequired,
+  completed: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
   sections: PropTypes.array.isRequired
 }
 
@@ -40,4 +56,15 @@ SectionList.defaultProps = {
   className: 'usa-sidenav-list'
 }
 
-export default SectionList
+function mapStateToProps(state) {
+  const application = state.application || {}
+  const completed = application.Completed || {}
+  const errors = application.Errors || {}
+  return {
+    application,
+    completed,
+    errors
+  }
+}
+
+export default connect(mapStateToProps)(SectionList)
