6bca4d55f098f45806ce3df334944538cc4a5cda
Christopher Hui <christopher.kam.hui@gmail.com>
2019-01-12 11:26:39 -0800

    remove out of date navigation import reference

--- src/config/index.js
+++ src/config/index.js
@@ -1,13 +1,12 @@
 import env from './environment'
 import { i18n, markdown } from './locales'
-import navigation, { navigationWalker } from './navigation'
+import { navigationWalker } from './navigation'
 import { isZipcodeState, zipcodes } from './zipcodes'
 export {
   env,
   i18n,
   isZipcodeState,
   markdown,
-  navigation,
   navigationWalker,
   zipcodes
 }
