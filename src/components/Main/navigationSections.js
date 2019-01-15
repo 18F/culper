8cda37f810844dfd2c4ed61138183ae1bf88e7af
Christopher Hui <christopher.kam.hui@gmail.com>
2019-01-15 15:32:06 -0800

    remove use of alcohol from substance use because it is not a part of sf85

--- src/components/Main/navigationSections.js
+++ src/components/Main/navigationSections.js
@@ -7,7 +7,7 @@ import Citizenship from './../../components/Section/Citizenship/navigation'
 import Military from './../../components/Section/Military/navigation'
 import Foreign from './../../components/Section/Foreign/navigation'
 import Financial from './../../components/Section/Financial/navigation'
-import SubstanceUse from './../../components/Section/SubstanceUse/navigation'
+import substanceUse from './../../components/Section/SubstanceUse/navigation'
 import Legal from './../../components/Section/Legal/navigation'
 import psychologicalNavigation from './../../components/Section/Psychological/navigation'
 import { Review } from './../../config/navigation'
@@ -21,7 +21,7 @@ function navigationSections(formType) {
     Military,
     Foreign,
     Financial,
-    SubstanceUse,
+    substanceUse(formType),
     Legal,
     psychologicalNavigation(formType),
     Review
--- src/components/Section/SubstanceUse/navigation.js
+++ src/components/Section/SubstanceUse/navigation.js
@@ -1,102 +1,105 @@
+import compact from 'lodash/compact'
 import * as validators from '../../../validators/index'
 
-const navigation = {
-  name: 'Substance use',
-  title: 'Substance use',
-  url: 'substance',
-  store: 'Substance',
-  showNumber: true,
-  locked: validators.formIsLocked,
-  subsections: [
-    {
-      exclude: true,
-      name: 'Introduction',
-      url: 'intro'
-    },
-    {
-      name: 'Illegal use of drugs and drug activity',
-      url: 'drugs',
-      subsections: [
-        {
-          name: 'Usage',
-          url: 'usage',
-          store: 'DrugUses',
-          validator: validators.DrugUsesValidator
-        },
-        {
-          name: 'Purchase',
-          url: 'purchase',
-          store: 'DrugInvolvements',
-          validator: validators.DrugInvolvementsValidator
-        },
-        {
-          name: 'Security clearance position',
-          url: 'clearance',
-          store: 'DrugClearanceUses',
-          validator: validators.DrugClearanceUsesValidator
-        },
-        {
-          name: 'Public safety position',
-          url: 'publicsafety',
-          store: 'DrugPublicSafetyUses',
-          validator: validators.DrugPublicSafetyUsesValidator
-        },
-        {
-          name: 'Misuse',
-          url: 'misuse',
-          store: 'PrescriptionUses',
-          validator: validators.DrugPrescriptionUsesValidator
-        },
-        {
-          name: 'Mandatory counseling or treatment',
-          url: 'ordered',
-          store: 'OrderedTreatments',
-          validator: validators.DrugOrderedTreatmentsValidator
-        },
-        {
-          name: 'Voluntary counseling or treatment',
-          url: 'voluntary',
-          store: 'VoluntaryTreatments',
-          validator: validators.DrugVoluntaryTreatmentsValidator
-        }
-      ]
-    },
-    {
-      name: 'Use of alcohol',
-      url: 'alcohol',
-      subsections: [
-        {
-          name: 'Negative impact',
-          url: 'negative',
-          store: 'NegativeImpacts',
-          validator: validators.AlcoholNegativeImpactsValidator
-        },
-        {
-          name: 'Mandatory counseling or treatment',
-          url: 'ordered',
-          store: 'OrderedCounselings',
-          validator: validators.AlcoholOrderedCounselingsValidator
-        },
-        {
-          name: 'Voluntary counseling or treatment',
-          url: 'voluntary',
-          store: 'VoluntaryCounselings',
-          validator: validators.AlcoholVoluntaryCounselingsValidator
-        },
-        {
-          name: 'Additional instances',
-          url: 'additional',
-          store: 'ReceivedCounselings',
-          validator: validators.AlcoholReceivedCounselingsValidator
-        }
-      ]
-    },
-    {
-      exclude: true,
-      name: 'Review',
-      url: 'review'
-    }
-  ]
+function substanceUse(formType) {
+  return {
+    name: 'Substance use',
+    title: 'Substance use',
+    url: 'substance',
+    store: 'Substance',
+    showNumber: true,
+    locked: validators.formIsLocked,
+    subsections: compact([
+      {
+        exclude: true,
+        name: 'Introduction',
+        url: 'intro'
+      },
+      {
+        name: 'Illegal use of drugs and drug activity',
+        url: 'drugs',
+        subsections: [
+          {
+            name: 'Usage',
+            url: 'usage',
+            store: 'DrugUses',
+            validator: validators.DrugUsesValidator
+          },
+          {
+            name: 'Purchase',
+            url: 'purchase',
+            store: 'DrugInvolvements',
+            validator: validators.DrugInvolvementsValidator
+          },
+          {
+            name: 'Security clearance position',
+            url: 'clearance',
+            store: 'DrugClearanceUses',
+            validator: validators.DrugClearanceUsesValidator
+          },
+          {
+            name: 'Public safety position',
+            url: 'publicsafety',
+            store: 'DrugPublicSafetyUses',
+            validator: validators.DrugPublicSafetyUsesValidator
+          },
+          {
+            name: 'Misuse',
+            url: 'misuse',
+            store: 'PrescriptionUses',
+            validator: validators.DrugPrescriptionUsesValidator
+          },
+          {
+            name: 'Mandatory counseling or treatment',
+            url: 'ordered',
+            store: 'OrderedTreatments',
+            validator: validators.DrugOrderedTreatmentsValidator
+          },
+          {
+            name: 'Voluntary counseling or treatment',
+            url: 'voluntary',
+            store: 'VoluntaryTreatments',
+            validator: validators.DrugVoluntaryTreatmentsValidator
+          }
+        ]
+      },
+      ['86'].indexOf(formType) > -1 ? {
+        name: 'Use of alcohol',
+        url: 'alcohol',
+        subsections: [
+          {
+            name: 'Negative impact',
+            url: 'negative',
+            store: 'NegativeImpacts',
+            validator: validators.AlcoholNegativeImpactsValidator
+          },
+          {
+            name: 'Mandatory counseling or treatment',
+            url: 'ordered',
+            store: 'OrderedCounselings',
+            validator: validators.AlcoholOrderedCounselingsValidator
+          },
+          {
+            name: 'Voluntary counseling or treatment',
+            url: 'voluntary',
+            store: 'VoluntaryCounselings',
+            validator: validators.AlcoholVoluntaryCounselingsValidator
+          },
+          {
+            name: 'Additional instances',
+            url: 'additional',
+            store: 'ReceivedCounselings',
+            validator: validators.AlcoholReceivedCounselingsValidator
+          }
+        ]
+      } : null,
+      {
+        exclude: true,
+        name: 'Review',
+        url: 'review'
+      }
+    ])
+  }
 }
 
-export default navigation
+export default substanceUse
