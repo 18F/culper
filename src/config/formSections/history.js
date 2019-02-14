cefa65a9a995a0570077ea11cf29863dfa985b45
Christopher Hui <christopher@truss.works>
2019-02-14 13:08:35 -0800

    add label key to history config

--- src/config/formSections/history.js
+++ src/config/formSections/history.js
@@ -1,4 +1,5 @@
 import * as sections from '@constants/sections'
+import { i18n } from '@config'
 
 const HISTORY = {
   key: sections.HISTORY,
@@ -11,6 +12,7 @@ const HISTORY_INTRO = {
   key: sections.HISTORY_INTRO,
   name: 'intro',
   path: `${HISTORY.path}/intro`,
+  label: i18n.t('history.subsections.intro')
 }
 
 const HISTORY_RESIDENCE = {
@@ -18,6 +20,7 @@ const HISTORY_RESIDENCE = {
   name: 'residence',
   path: `${HISTORY.path}/residence`,
   storeKey: 'Residence',
+  label: i18n.t('history.subsections.residence')
 }
 
 const HISTORY_EMPLOYMENT = {
@@ -25,6 +28,7 @@ const HISTORY_EMPLOYMENT = {
   name: 'employment',
   path: `${HISTORY.path}/employment`,
   storeKey: 'Employment',
+  label: i18n.t('history.subsections.employment')
 }
 
 const HISTORY_EDUCATION = {
@@ -32,6 +36,7 @@ const HISTORY_EDUCATION = {
   name: 'education',
   path: `${HISTORY.path}/education`,
   storeKey: 'Education',
+  label: i18n.t('history.subsections.education')
 }
 
 const HISTORY_FEDERAL = {
@@ -39,12 +44,14 @@ const HISTORY_FEDERAL = {
   name: 'federal',
   path: `${HISTORY.path}/federal`,
   storeKey: 'Federal',
+  label: i18n.t('history.subsections.federal')
 }
 
 const HISTORY_REVIEW = {
   key: sections.HISTORY_REVIEW,
   name: 'review',
   path: `${HISTORY.path}/review`,
+  label: i18n.t('history.subsections.review')
 }
 
 export default {
