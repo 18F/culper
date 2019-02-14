42c5fcfff505a968a9660202119591cbc055e838
Christopher Hui <christopher@truss.works>
2019-02-14 13:04:06 -0800

    add label key to identification config

--- src/config/formSections/identification.js
+++ src/config/formSections/identification.js
@@ -1,4 +1,5 @@
 import * as sections from '@constants/sections'
+import { i18n } from '@config'
 
 const IDENTIFICATION = {
   key: sections.IDENTIFICATION,
@@ -11,6 +12,7 @@ const IDENTIFICATION_INTRO = {
   key: sections.IDENTIFICATION_INTRO,
   name: 'intro',
   path: `${IDENTIFICATION.path}/intro`,
+  label: i18n.t('identification.subsection.intro')
 }
 
 const IDENTIFICATION_NAME = {
@@ -18,6 +20,7 @@ const IDENTIFICATION_NAME = {
   name: 'name',
   path: `${IDENTIFICATION.path}/name`,
   storeKey: 'ApplicantName',
+  label: i18n.t('identification.subsection.name')
 }
 
 const IDENTIFICATION_BIRTH_DATE = {
@@ -25,6 +28,7 @@ const IDENTIFICATION_BIRTH_DATE = {
   name: 'birthdate',
   path: `${IDENTIFICATION.path}/birthdate`,
   storeKey: 'ApplicantBirthDate',
+  label: i18n.t('identification.subsection.birthdate')
 }
 
 const IDENTIFICATION_BIRTH_PLACE = {
@@ -32,6 +36,7 @@ const IDENTIFICATION_BIRTH_PLACE = {
   name: 'birthplace',
   path: `${IDENTIFICATION.path}/birthplace`,
   storeKey: 'ApplicantBirthPlace',
+  label: i18n.t('identification.subsection.birthplace')
 }
 
 const IDENTIFICATION_SSN = {
@@ -39,6 +44,7 @@ const IDENTIFICATION_SSN = {
   name: 'ssn',
   path: `${IDENTIFICATION.path}/ssn`,
   storeKey: 'ApplicantSSN',
+  label: i18n.t('identification.subsection.ssn')
 }
 
 const IDENTIFICATION_OTHER_NAMES = {
@@ -46,6 +52,7 @@ const IDENTIFICATION_OTHER_NAMES = {
   name: 'othernames',
   path: `${IDENTIFICATION.path}/othernames`,
   storeKey: 'OtherNames',
+  label: i18n.t('identification.subsection.othernames')
 }
 
 const IDENTIFICATION_CONTACTS = {
@@ -53,6 +60,7 @@ const IDENTIFICATION_CONTACTS = {
   name: 'contacts',
   path: `${IDENTIFICATION.path}/contacts`,
   storeKey: 'Contacts',
+  label: i18n.t('identification.subsection.contacts')
 }
 
 const IDENTIFICATION_PHYSICAL = {
@@ -60,12 +68,14 @@ const IDENTIFICATION_PHYSICAL = {
   name: 'physical',
   path: `${IDENTIFICATION.path}/physical`,
   storeKey: 'Physical',
+  label: i18n.t('identification.destination.physical')
 }
 
 const IDENTIFICATION_REVIEW = {
   key: sections.IDENTIFICATION_REVIEW,
   name: 'review',
   path: `${IDENTIFICATION.path}/review`,
+  label: i18n.t('identification.subsection.review')
 }
 
 export default {
