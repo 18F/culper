5971d8fdca8cfdd9d689662088fd9f735b37a66b
Suzanne Rozier <suz@truss.works>
2019-02-14 10:00:47 -0500

    Use single quotes for plain strings

--- src/config/formSections.js
+++ src/config/formSections.js
@@ -2,69 +2,69 @@ import * as sections from '../constants/sections'
 
 const IDENTIFICATION = {
   key: sections.IDENTIFICATION,
-  name: `identification`,
-  path: `/identification`,
-  store: `Identification`,
+  name: 'identification',
+  path: '/identification',
+  store: 'Identification',
 }
 
 const IDENTIFICATION_INTRO = {
   key: sections.IDENTIFICATION_INTRO,
-  name: `intro`,
+  name: 'intro',
   path: `${IDENTIFICATION.path}/intro`,
 }
 
 const IDENTIFICATION_NAME = {
   key: sections.IDENTIFICATION_NAME,
-  name: `name`,
+  name: 'name',
   path: `${IDENTIFICATION.path}/name`,
-  storeKey: `ApplicantName`,
+  storeKey: 'ApplicantName',
 }
 
 const IDENTIFICATION_BIRTH_DATE = {
   key: sections.IDENTIFICATION_BIRTH_DATE,
-  name: `birthdate`,
+  name: 'birthdate',
   path: `${IDENTIFICATION.path}/birthdate`,
-  storeKey: `ApplicantBirthDate`,
+  storeKey: 'ApplicantBirthDate',
 }
 
 const IDENTIFICATION_BIRTH_PLACE = {
   key: sections.IDENTIFICATION_BIRTH_PLACE,
-  name: `birthplace`,
+  name: 'birthplace',
   path: `${IDENTIFICATION.path}/birthplace`,
-  storeKey: `ApplicantBirthPlace`,
+  storeKey: 'ApplicantBirthPlace',
 }
 
 const IDENTIFICATION_SSN = {
   key: sections.IDENTIFICATION_SSN,
-  name: `ssn`,
+  name: 'ssn',
   path: `${IDENTIFICATION.path}/ssn`,
-  storeKey: `ApplicantSSN`,
+  storeKey: 'ApplicantSSN',
 }
 
 const IDENTIFICATION_OTHER_NAMES = {
   key: sections.IDENTIFICATION_OTHER_NAMES,
-  name: `othernames`,
+  name: 'othernames',
   path: `${IDENTIFICATION.path}/othernames`,
-  storeKey: `OtherNames`,
+  storeKey: 'OtherNames',
 }
 
 const IDENTIFICATION_CONTACTS = {
   key: sections.IDENTIFICATION_CONTACTS,
-  name: `contacts`,
+  name: 'contacts',
   path: `${IDENTIFICATION.path}/contacts`,
-  storeKey: `Contacts`,
+  storeKey: 'Contacts',
 }
 
 const IDENTIFICATION_PHYSICAL = {
   key: sections.IDENTIFICATION_PHYSICAL,
-  name: `physical`,
+  name: 'physical',
   path: `${IDENTIFICATION.path}/physical`,
-  storeKey: `Physical`,
+  storeKey: 'Physical',
 }
 
 const IDENTIFICATION_REVIEW = {
   key: sections.IDENTIFICATION_REVIEW,
-  name: `review`,
+  name: 'review',
   path: `${IDENTIFICATION.path}/review`,
 }
 
