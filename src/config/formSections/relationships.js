dd7bf21daa190831a3b507cd2ac17c3f9d2b0258
Christopher Hui <christopher@truss.works>
2019-02-23 14:52:32 -0800

    update relationships subsection data structure

--- src/config/formSections/relationships.js
+++ src/config/formSections/relationships.js
@@ -4,61 +4,83 @@ import { i18n } from '@config'
 const RELATIONSHIPS = {
   key: sections.RELATIONSHIPS,
   name: 'relationships',
-  path: '/relationships',
+  path: {
+    section: 'relationships',
+  },
   store: 'Relationships',
-  label: i18n.t('relationships.section.name')
+  label: i18n.t('relationships.section.name'),
 }
 
 const RELATIONSHIPS_INTRO = {
   key: sections.RELATIONSHIPS_INTRO,
   name: 'intro',
-  path: `${RELATIONSHIPS.path}/intro`,
-  label: i18n.t('relationships.subsection.intro')
+  path: {
+    section: RELATIONSHIPS.path.section,
+    subsection: 'intro',
+  },
+  label: i18n.t('relationships.subsection.intro'),
 }
 
 const RELATIONSHIPS_STATUS = {
   key: sections.RELATIONSHIPS_STATUS,
   name: 'status',
-  label: i18n.t('relationships.subsection.status')
+  label: i18n.t('relationships.subsection.status'),
 }
 
 const RELATIONSHIPS_STATUS_MARITAL = {
   key: sections.RELATIONSHIPS_STATUS_MARITAL,
   name: 'marital',
-  path: `${RELATIONSHIPS_STATUS.path}/marital`,
+  path: {
+    section: RELATIONSHIPS.path.section,
+    subsection: 'status/marital',
+  },
   storeKey: 'Marital',
-  label: i18n.t('relationships.subsection.marital')
+  label: i18n.t('relationships.subsection.marital'),
+  parentKey: RELATIONSHIPS_STATUS.key,
 }
 
 const RELATIONSHIPS_STATUS_COHABITANTS = {
   key: sections.RELATIONSHIPS_STATUS_COHABITANTS,
   name: 'cohabitants',
-  path: `${RELATIONSHIPS_STATUS.path}/cohabitants`,
+  path: {
+    section: RELATIONSHIPS.path.section,
+    subsection: 'status/cohabitants',
+  },
   storeKey: 'Cohabitants',
-  label: i18n.t('relationships.subsection.cohabitants')
+  label: i18n.t('relationships.subsection.cohabitants'),
+  parentKey: RELATIONSHIPS_STATUS.key,
 }
 
 const RELATIONSHIPS_PEOPLE = {
   key: sections.RELATIONSHIPS_PEOPLE,
   name: 'people',
-  path: `${RELATIONSHIPS.path}/people`,
+  path: {
+    section: RELATIONSHIPS.path.section,
+    subsection: 'people',
+  },
   storeKey: 'People',
-  label: i18n.t('relationships.subsection.people')
+  label: i18n.t('relationships.subsection.people'),
 }
 
 const RELATIONSHIPS_RELATIVES = {
   key: sections.RELATIONSHIPS_RELATIVES,
   name: 'relatives',
-  path: `${RELATIONSHIPS.path}/relatives`,
+  path: {
+    section: RELATIONSHIPS.path.section,
+    subsection: 'relatives',
+  },
   storeKey: 'Relatives',
-  label: i18n.t('relationships.subsection.relatives')
+  label: i18n.t('relationships.subsection.relatives'),
 }
 
 const RELATIONSHIPS_REVIEW = {
   key: sections.RELATIONSHIPS_REVIEW,
   name: 'review',
-  path: `${RELATIONSHIPS.path}/review`,
-  label: i18n.t('relationships.subsection.review')
+  path: {
+    section: RELATIONSHIPS.path.section,
+    subsection: 'review',
+  },
+  label: i18n.t('relationships.subsection.review'),
 }
 
 export default {
@@ -69,5 +91,5 @@ export default {
   RELATIONSHIPS_STATUS_COHABITANTS,
   RELATIONSHIPS_PEOPLE,
   RELATIONSHIPS_RELATIVES,
-  RELATIONSHIPS_REVIEW
+  RELATIONSHIPS_REVIEW,
 }
