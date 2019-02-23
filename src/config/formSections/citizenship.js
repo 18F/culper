05f1b86a17e8c3f205980857746cba14cf96772a
Christopher Hui <christopher@truss.works>
2019-02-23 14:58:13 -0800

    update citizenship subsection data structure

--- src/config/formSections/citizenship.js
+++ src/config/formSections/citizenship.js
@@ -4,47 +4,64 @@ import { i18n } from '@config'
 const CITIZENSHIP = {
   key: sections.CITIZENSHIP,
   name: 'citizenship',
-  path: '/citizenship',
+  path: {
+    section: 'citizenship',
+  },
   store: 'Citizenship',
-  label: i18n.t('citizenship.section.name')
+  label: i18n.t('citizenship.section.name'),
 }
 
 const CITIZENSHIP_INTRO = {
   key: sections.CITIZENSHIP_INTRO,
   name: 'intro',
-  path: `${CITIZENSHIP.path}/intro`,
-  label: i18n.t('citizenship.subsection.intro')
+  path: {
+    section: CITIZENSHIP.path.section,
+    subsection: '',
+  },
+  label: i18n.t('citizenship.subsection.intro'),
 }
 
 const CITIZENSHIP_STATUS = {
   key: sections.CITIZENSHIP_STATUS,
   name: 'status',
-  path: `${CITIZENSHIP.path}/status`,
+  path: {
+    section: CITIZENSHIP.path.section,
+    subsection: 'status',
+  },
   storeKey: 'Status',
-  label: i18n.t('citizenship.subsection.status')
+  label: i18n.t('citizenship.subsection.status'),
 }
 
 const CITIZENSHIP_MULTIPLE = {
   key: sections.CITIZENSHIP_MULTIPLE,
   name: 'multiple',
-  path: `${CITIZENSHIP.path}/multiple`,
+  path: {
+    section: CITIZENSHIP.path.section,
+    subsection: 'multiple',
+  },
   storeKey: 'Multiple',
-  label: i18n.t('citizenship.subsection.multiple')
+  label: i18n.t('citizenship.subsection.multiple'),
 }
 
 const CITIZENSHIP_PASSPORTS = {
   key: sections.CITIZENSHIP_PASSPORTS,
   name: 'passports',
-  path: `${CITIZENSHIP.path}/passports`,
+  path: {
+    section: CITIZENSHIP.path.section,
+    subsection: 'passports',
+  },
   storeKey: 'Passports',
-  label: i18n.t('citizenship.subsection.passports')
+  label: i18n.t('citizenship.subsection.passports'),
 }
 
 const CITIZENSHIP_REVIEW = {
   key: sections.CITIZENSHIP_REVIEW,
   name: 'review',
-  path: `${CITIZENSHIP.path}/review`,
-  label: i18n.t('citizenship.subsection.review')
+  path: {
+    section: CITIZENSHIP.path.section,
+    subsection: 'review',
+  },
+  label: i18n.t('citizenship.subsection.review'),
 }
 
 export default {
@@ -53,5 +70,5 @@ export default {
   CITIZENSHIP_STATUS,
   CITIZENSHIP_MULTIPLE,
   CITIZENSHIP_PASSPORTS,
-  CITIZENSHIP_REVIEW
+  CITIZENSHIP_REVIEW,
 }
