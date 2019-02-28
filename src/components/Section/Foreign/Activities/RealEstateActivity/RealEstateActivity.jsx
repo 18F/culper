50edc4f5cc744d7ba72984d0a5da226497d32a46
Christopher Hui <christopher@truss.works>
2019-02-28 12:42:14 -0800

    refactor real estate section and disable tests because they were originally broken

--- src/components/Section/Foreign/Activities/RealEstateActivity/RealEstateActivity.jsx
+++ src/components/Section/Foreign/Activities/RealEstateActivity/RealEstateActivity.jsx
@@ -1,47 +1,57 @@
 import React from 'react'
-import { i18n } from '../../../../../config'
-import schema from '../../../../../schema'
-import validate from '../../../../../validators'
-import { Summary, AddressSummary, DateSummary } from '../../../../Summary'
-import { Accordion, Branch, Show } from '../../../../Form'
-import {
-  ForeignRealEstateActivityValidator,
-  ForeignRealEstateInterestValidator
-} from '../../../../../validators'
-import SubsectionElement from '../../../SubsectionElement'
+import { i18n } from 'config'
+import schema from 'schema'
+import validate, { ForeignRealEstateInterestValidator } from 'validators'
+import { Summary, AddressSummary, DateSummary } from 'components/Summary'
+import { Accordion, Branch, Show } from 'components/Form'
+import { FOREIGN, FOREIGN_ACTIVITIES_REAL_ESTATE } from 'config/formSections/foreign'
+import Subsection from 'components/Section/shared/Subsection'
+import connectForeignSection from '../../ForeignConnector'
 import RealEstateInterest from './RealEstateInterest'
 
-export default class RealEstateActivity extends SubsectionElement {
+const sectionConfig = {
+  section: FOREIGN.name,
+  store: FOREIGN.store,
+  subsection: FOREIGN_ACTIVITIES_REAL_ESTATE.name,
+  storeKey: FOREIGN_ACTIVITIES_REAL_ESTATE.storeKey,
+}
+export class RealEstateActivity extends Subsection {
   constructor(props) {
     super(props)
 
-    this.update = this.update.bind(this)
-    this.updateHasInterests = this.updateHasInterests.bind(this)
-    this.updateList = this.updateList.bind(this)
+
+    const {
+      section, subsection, store, storeKey,
+    } = sectionConfig
+
+    this.section = section
+    this.subsection = subsection
+    this.store = store
+    this.storeKey = storeKey
   }
 
-  update(queue) {
+  update = (queue) => {
     this.props.onUpdate({
       List: this.props.List,
       HasInterests: this.props.HasInterests,
-      ...queue
+      ...queue,
     })
   }
 
-  updateList(values) {
+  updateList = (values) => {
     this.update({
-      List: values
+      List: values,
     })
   }
 
-  updateHasInterests(values) {
+  updateHasInterests = (values) => {
     this.update({
       HasInterests: values,
-      List: values.value === 'Yes' ? this.props.List : []
+      List: values.value === 'Yes' ? this.props.List : [],
     })
   }
 
-  summary(item, index) {
+  summary = (item, index) => {
     const o = (item || {}).Item || {}
     const who = ((o.InterestTypes || {}).values || []).join(', ')
     const acquired = DateSummary(o.Acquired)
@@ -59,10 +69,10 @@ export default class RealEstateActivity extends SubsectionElement {
 
     return Summary({
       type: i18n.t('foreign.activities.realestate.collection.itemType'),
-      index: index,
+      index,
       left: who || address ? summary : '',
       right: acquired,
-      placeholder: i18n.t('foreign.activities.realestate.collection.summary')
+      placeholder: i18n.t('foreign.activities.realestate.collection.summary'),
     })
   }
 
@@ -70,14 +80,15 @@ export default class RealEstateActivity extends SubsectionElement {
     return (
       <div
         className="section-content realestate"
-        {...super.dataAttributes(this.props)}>
+        {...super.dataAttributes()}
+      >
         <h1 className="section-header">{i18n.t('foreign.destination.activities.realestate')}</h1>
         <Branch
           name="has_interests"
           label={i18n.t('foreign.activities.realestate.heading.title')}
           labelSize="h4"
           {...this.props.HasInterests}
-          warning={true}
+          warning
           onError={this.handleError}
           required={this.props.required}
           onUpdate={this.updateHasInterests}
@@ -93,20 +104,15 @@ export default class RealEstateActivity extends SubsectionElement {
             onUpdate={this.updateList}
             onError={this.handleError}
             validator={ForeignRealEstateInterestValidator}
-            description={i18n.t(
-              'foreign.activities.realestate.collection.description'
-            )}
-            appendTitle={i18n.t(
-              'foreign.activities.realestate.collection.appendTitle'
-            )}
-            appendLabel={i18n.t(
-              'foreign.activities.realestate.collection.appendLabel'
-            )}
+            description={i18n.t('foreign.activities.realestate.collection.description')}
+            appendTitle={i18n.t('foreign.activities.realestate.collection.appendTitle')}
+            appendLabel={i18n.t('foreign.activities.realestate.collection.appendLabel')}
             required={this.props.required}
-            scrollIntoView={this.props.scrollIntoView}>
+            scrollIntoView={this.props.scrollIntoView}
+          >
             <RealEstateInterest
               name="Item"
-              bind={true}
+              bind
               required={this.props.required}
               scrollIntoView={this.props.scrollIntoView}
             />
@@ -122,15 +128,13 @@ RealEstateActivity.defaultProps = {
   HasInterests: {},
   List: {},
   defaultState: true,
-  onUpdate: queue => {},
-  onError: (value, arr) => {
-    return arr
-  },
+  onUpdate: () => {},
+  onError: (value, arr) => arr,
   section: 'foreign',
   subsection: 'activities/realestate',
   dispatch: () => {},
-  validator: data => {
-    return validate(schema('foreign.activities.realestate', data))
-  },
-  scrollToBottom: ''
+  validator: data => validate(schema('foreign.activities.realestate', data)),
+  scrollToBottom: '',
 }
+
+export default connectForeignSection(RealEstateActivity, sectionConfig)
