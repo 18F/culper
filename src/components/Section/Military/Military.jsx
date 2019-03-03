6c488acb17ea935172f37397741d52d6e1f2c8b7
Christopher Hui <christopher@truss.works>
2019-03-02 18:20:17 -0800

    minor refactor to military subsection

--- src/components/Section/Military/Military.jsx
+++ src/components/Section/Military/Military.jsx
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types'
 import { Route } from 'react-router'
 import { connect } from 'react-redux'
 import { i18n } from 'config'
-import * as sections from 'constants/sections'
+import { MILITARY } from 'constants/sections'
+import * as formTypes from 'config/formTypes'
 import { ErrorList } from 'components/ErrorList'
 import SectionNavigation from 'components/Section/shared/SectionNavigation'
 import Selective from 'components/Section/Military/Selective'
@@ -13,42 +14,63 @@ import Foreign from 'components/Section/Military/Foreign'
 import Intro from 'components/Section/Military/Intro'
 import Review from 'components/Section/Military/Review'
 
-const Military = ({ subsection, location }) => {
-  const subsectionClasses = `view view-${subsection || 'unknown'}`
+class Military extends React.Component {
+  constructor(props) {
+    super(props)
 
-  const isReview = subsection === 'review'
-  const title = isReview && i18n.t('review.title')
-  const para = isReview && i18n.m('review.para')
+    this.form = formTypes[props.formType]
+    this.section = this.form.find(section => (section.key === MILITARY))
 
-  /** TODO - this should come from Redux store */
-  const formType = 'SF86'
+    // Each key in subsectionLibrary corresponds to the subsection
+    // name in the config file.
+    this.subsectionLibrary = {
+      intro: Intro,
+      selective: Selective,
+      history: History,
+      disciplinary: Disciplinary,
+      foreign: Foreign,
+      review: Review,
+    }
+  }
 
-  return (
-    <div className="section-view">
-      {title && <h1 className="title">{title}</h1>}
-      {para}
-
-      <div className={subsectionClasses}>
-        {isReview && (
-          <div className="top-btns"><ErrorList /></div>
-        )}
-
-        <Route path="/form/military/intro" component={Intro} />
-        <Route path="/form/military/selective" component={Selective} />
-        <Route path="/form/military/history" component={History} />
-        <Route path="/form/military/disciplinary" component={Disciplinary} />
-        <Route path="/form/military/foreign" component={Foreign} />
-        <Route path="/form/military/review" component={Review} />
-
-        <SectionNavigation
-          currentPath={location.pathname}
-          section={sections.MILITARY}
-          subsection={subsection}
-          formType={formType}
-        />
-      </div>
-    </div>
+  getMilitarySubsections = () => (
+    this.section.subsections.map(subsection => (
+      <Route
+        key={subsection.key}
+        path={`/form/${this.section.path}/${subsection.path}`}
+        component={this.subsectionLibrary[subsection.name]}
+      />
+    ))
   )
+
+  render() {
+    const { subsection, location, formType } = this.props
+    const subsectionClasses = `view view-${subsection || 'unknown'}`
+
+    const isReview = subsection === 'review'
+    const title = isReview && i18n.t('review.title')
+    const para = isReview && i18n.m('review.para')
+
+    return (
+      <div className="section-view">
+        {title && <h1 className="title">{title}</h1>}
+        {para}
+
+        <div className={subsectionClasses}>
+          {isReview && (
+            <div className="top-btns"><ErrorList /></div>
+          )}
+
+          {this.getMilitarySubsections()}
+
+          <SectionNavigation
+            currentPath={location.pathname}
+            formType={formType}
+          />
+        </div>
+      </div>
+    )
+  }
 }
 
 function mapStateToProps(state) {
@@ -58,6 +80,7 @@ function mapStateToProps(state) {
   const errors = app.Errors || {}
   const completed = app.Completed || {}
   const addressBooks = app.AddressBooks || {}
+  const auth = state.authentication || {}
 
   return {
     ...section,
@@ -66,6 +89,7 @@ function mapStateToProps(state) {
     Errors: errors.military || [],
     Completed: completed.military || [],
     addressBooks,
+    auth: auth.formType,
   }
 }
 
@@ -77,6 +101,7 @@ Military.defaultProps = {
 Military.propTypes = {
   subsection: PropTypes.string,
   location: PropTypes.object,
+  formType: PropTypes.string.isRequired,
 }
 
 export const MilitarySections = () => <Review />
