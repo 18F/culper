5aeebb893074ee012d7942991a6d75a3b093841b
Suzanne Rozier <suz@truss.works>
2019-03-05 10:32:59 -0500

    Pull validation functions out of CitizenshipMultiple, CitizenshipItem validators

--- src/validators/citizenship-multiple.js
+++ src/validators/citizenship-multiple.js
@@ -1,17 +1,99 @@
 import DateRangeValidator from './daterange'
 import { validAccordion, validGenericTextfield } from './helpers'
 
+/** Helper functions */
+const isUnitedStates = (country) => {
+  if (!country || !country.value) { return true }
+
+  return country.value.includes('United States')
+}
+
+/** Attribute Validators */
+const validateCountry = country => validGenericTextfield(country)
+
+const validateDates = dates => !!dates && new DateRangeValidator(dates).isValid()
+
+const validateHow = how => !!how && validGenericTextfield(how)
+
+const validateRenounced = (renounced, renouncedExplanation) => (
+  !!renounced
+    && (renounced === 'No' || renounced === 'Yes')
+    && validGenericTextfield(renouncedExplanation)
+)
+
+const validateCurrent = (dates, current, currentExplanation) => {
+  if (!dates || dates.present) { return true }
+
+  return !!current
+    && (current === 'No' || current === 'Yes')
+    && validGenericTextfield(currentExplanation)
+}
+
+const validateHasMultiple = hasMultiple => !!hasMultiple
+  && (hasMultiple === 'Yes' || hasMultiple === 'No')
+
+const validateMinimumCitizenships = (citizenships) => {
+  if (citizenships.items && citizenships.items.length < 2) {
+    return false
+  }
+
+  return true
+}
+
+/** Object Validators (as functions) */
+export const validateCitizenshipItem = (data = {}) => {
+  const country = data.Country
+  const dates = data.Dates
+  const how = data.How
+  const renounced = (data.Renounced || {}).value
+  const renouncedExplanation = data.RenouncedExplanation
+  const current = (data.Current || {}).value
+  const currentExplanation = data.CurrentExplanation
+
+  const validCountry = validateCountry(country)
+  const validDates = validateDates(dates)
+  const validCurrent = validateCurrent(dates, current, currentExplanation)
+
+  if (!isUnitedStates(country)) {
+    const validHow = validateHow(how)
+    const validRenounced = validateRenounced(renounced, renouncedExplanation)
+
+    return validCountry && validDates && validCurrent && validHow && validRenounced
+  }
+
+  return validCountry && validDates && validCurrent
+}
+
+const validateCitizenships = citizenships => (
+  validAccordion(citizenships, i => validateCitizenshipItem(i))
+)
+
+export const validateCitizenshipMultiple = (data = {}) => {
+  const hasMultiple = (data.HasMultiple || {}).value
+  const citizenships = data.List || {}
+
+  const validHasMultiple = validateHasMultiple(hasMultiple)
+
+  if (hasMultiple === 'Yes') {
+    const validMinimumCitizenships = validateMinimumCitizenships(citizenships)
+    const validCitizenships = validateCitizenships(citizenships)
+
+    return validHasMultiple && validMinimumCitizenships && validCitizenships
+  }
+
+  return validHasMultiple
+}
+
+/** Object Validators (as classes) - legacy */
 export default class CitizenshipMultipleValidator {
   constructor(data = {}) {
+    this.data = data
     this.hasMultiple = (data.HasMultiple || {}).value
     this.list = data.List || {}
   }
 
   validHasMultiple() {
-    return (
-      !!this.hasMultiple &&
-      (this.hasMultiple === 'Yes' || this.hasMultiple === 'No')
-    )
+    return validateHasMultiple(this.hasMultiple)
   }
 
   validMinimumCitizenships() {
@@ -19,12 +101,7 @@ export default class CitizenshipMultipleValidator {
       return true
     }
 
-    // Must provide a minimum of two countries
-    if (this.list.items && this.list.items.length < 2) {
-      return false
-    }
-
-    return true
+    return validateMinimumCitizenships(this.list)
   }
 
   validCitizenships() {
@@ -32,22 +109,17 @@ export default class CitizenshipMultipleValidator {
       return true
     }
 
-    return validAccordion(this.list, item => {
-      return new CitizenshipItemValidator(item).isValid()
-    })
+    return validateCitizenships(this.list)
   }
 
   isValid() {
-    return (
-      this.validHasMultiple() &&
-      this.validMinimumCitizenships() &&
-      this.validCitizenships()
-    )
+    return validateCitizenshipMultiple(this.data)
   }
 }
 
 export class CitizenshipItemValidator {
   constructor(data = {}) {
+    this.data = data
     this.country = data.Country
     this.dates = data.Dates
     this.how = data.How
@@ -58,19 +130,15 @@ export class CitizenshipItemValidator {
   }
 
   isUnitedStates() {
-    if (!this.country || !this.country.value) {
-      return true
-    }
-
-    return (this.country.value || []).includes('United States')
+    return isUnitedStates(this.country)
   }
 
   validCountry() {
-    return validGenericTextfield(this.country)
+    return validateCountry(this.country)
   }
 
   validDates() {
-    return !!this.dates && new DateRangeValidator(this.dates).isValid()
+    return validateDates(this.dates)
   }
 
   validHow() {
@@ -78,7 +146,7 @@ export class CitizenshipItemValidator {
       return true
     }
 
-    return !!this.how && validGenericTextfield(this.how)
+    return validateHow(this.how)
   }
 
   validRenounced() {
@@ -86,32 +154,14 @@ export class CitizenshipItemValidator {
       return true
     }
 
-    return (
-      !!this.renounced &&
-      (this.renounced === 'No' || this.renounced === 'Yes') &&
-      validGenericTextfield(this.renouncedExplanation)
-    )
+    return validateRenounced(this.renounced, this.renouncedExplanation)
   }
 
   validCurrent() {
-    if (!this.dates || this.dates.present) {
-      return true
-    }
-
-    return (
-      !!this.current &&
-      (this.current === 'No' || this.current === 'Yes') &&
-      validGenericTextfield(this.currentExplanation)
-    )
+    return validateCurrent(this.dates, this.current, this.currentExplanation)
   }
 
   isValid() {
-    return (
-      this.validCountry() &&
-      this.validDates() &&
-      this.validHow() &&
-      this.validRenounced() &&
-      this.validCurrent()
-    )
+    return validateCitizenshipItem(this.data)
   }
 }
