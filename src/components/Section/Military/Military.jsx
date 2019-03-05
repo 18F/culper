2acdb51e8b7c0556071f8f0eeaebaf3257db290a
Christopher Hui <christopher@truss.works>
2019-03-05 11:54:27 -0800

    update military to be pure functional component

--- src/components/Section/Military/Military.jsx
+++ src/components/Section/Military/Military.jsx
@@ -14,63 +14,45 @@ import Foreign from 'components/Section/Military/Foreign'
 import Intro from 'components/Section/Military/Intro'
 import Review from 'components/Section/Military/Review'
 
-class Military extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.form = formTypes[props.formType]
-    this.section = this.form.find(section => (section.key === MILITARY))
-
-    // Each key in subsectionLibrary corresponds to the subsection
-    // name in the config file.
-    this.subsectionLibrary = {
-      intro: Intro,
-      selective: Selective,
-      history: History,
-      disciplinary: Disciplinary,
-      foreign: Foreign,
-      review: Review,
-    }
+const Military = ({ subsection, location, formType }) => {
+  const subsectionLibrary = {
+    intro: Intro,
+    selective: Selective,
+    history: History,
+    disciplinary: Disciplinary,
+    foreign: Foreign,
+    review: Review,
   }
 
-  getMilitarySubsections = () => (
-    this.section.subsections.map(subsection => (
-      <Route
-        key={subsection.key}
-        path={`/form/${this.section.path}/${subsection.path}`}
-        component={this.subsectionLibrary[subsection.name]}
-      />
-    ))
-  )
+  const form = formTypes[formType]
+  const section = form.find(s => (s.key === MILITARY))
+  const subsectionClasses = `view view-${subsection || 'unknown'}`
+  const isReview = subsection === 'review'
+  const title = isReview && i18n.t('review.title')
+  const para = isReview && i18n.m('review.para')
 
-  render() {
-    const { subsection, location, formType } = this.props
-    const subsectionClasses = `view view-${subsection || 'unknown'}`
+  return (
+    <div className="section-view">
+      {title && <h1 className="title">{title}</h1>}
+      {para}
 
-    const isReview = subsection === 'review'
-    const title = isReview && i18n.t('review.title')
-    const para = isReview && i18n.m('review.para')
+      <div className={subsectionClasses}>
+        {isReview && (
+          <div className="top-btns"><ErrorList /></div>
+        )}
 
-    return (
-      <div className="section-view">
-        {title && <h1 className="title">{title}</h1>}
-        {para}
-
-        <div className={subsectionClasses}>
-          {isReview && (
-            <div className="top-btns"><ErrorList /></div>
-          )}
-
-          {this.getMilitarySubsections()}
-
-          <SectionNavigation
-            currentPath={location.pathname}
-            formType={formType}
+        {section.subsections.map(sub => (
+          <Route
+            key={sub.key}
+            path={`/form/${section.path}/${sub.path}`}
+            component={subsectionLibrary[sub.name]}
           />
-        </div>
+        ))}
+
+        <SectionNavigation currentPath={location.pathname} />
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 function mapStateToProps(state) {
