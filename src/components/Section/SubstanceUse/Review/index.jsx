a7c465f10cd8e478432f7636b73133bf2f94be36
Christopher Hui <christopher@truss.works>
2019-03-09 16:23:28 -0800

    update substance use to use config and add review

--- src/components/Section/SubstanceUse/Review/index.jsx
+++ src/components/Section/SubstanceUse/Review/index.jsx
@@ -1,13 +1,49 @@
 import React from 'react'
+import NegativeImpacts from '../Alcohol/NegativeImpacts'
+import OrderedCounselings from '../Alcohol/OrderedCounselings'
+import VoluntaryCounselings from '../Alcohol/VoluntaryCounselings'
+import ReceivedCounselings from '../Alcohol/ReceivedCounselings'
+import DrugUses from '../Drugs/DrugUses'
+import DrugInvolvements from '../Drugs/DrugInvolvements'
+import DrugClearanceUses from '../Drugs/DrugClearanceUses'
+import DrugPublicSafetyUses from '../Drugs/DrugPublicSafetyUses'
+import PrescriptionUses from '../Drugs/PrescriptionUses'
+import OrderedTreatments from '../Drugs/OrderedTreatments'
+import VoluntaryTreatments from '../Drugs/VoluntaryTreatments'
 
 const Review = () => {
   const props = {
-
+    required: true,
+    scrollIntoView: false,
   }
 
+  const sectionDivider = (
+    <hr className="section-divider" />
+  )
+
   return (
-    <div {...props}>
-      hello
+    <div>
+      <DrugUses {...props} />
+      {sectionDivider}
+      <DrugInvolvements {...props} />
+      {sectionDivider}
+      <DrugClearanceUses {...props} />
+      {sectionDivider}
+      <DrugPublicSafetyUses {...props} />
+      {sectionDivider}
+      <PrescriptionUses {...props} />
+      {sectionDivider}
+      <OrderedTreatments {...props} />
+      {sectionDivider}
+      <VoluntaryTreatments {...props} />
+      {sectionDivider}
+      <NegativeImpacts {...props} />
+      {sectionDivider}
+      <OrderedCounselings {...props} />
+      {sectionDivider}
+      <VoluntaryCounselings {...props} />
+      {sectionDivider}
+      <ReceivedCounselings {...props} />
     </div>
   )
 }
