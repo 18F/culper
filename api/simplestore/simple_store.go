e5fc5522e560ef4870fff8589925f83315d187b0
MacRae Linton <55759+macrael@users.noreply.github.com>
2019-04-30 14:43:06 -0700

    Add a transaction to UpdateSection.

--- api/simplestore/simple_store.go
+++ api/simplestore/simple_store.go
@@ -11,6 +11,14 @@ import (
 	"github.com/18F/e-QIP-prototype/api"
 )
 
+// simpleConnection contains all the methods we actually call on the db connection.
+// These functions are callable on both the sqlx.DB *and* the sqlx.Tx so we can have our
+// db functions easily work on either the main connection or a transaction.
+type simpleConnection interface {
+	Exec(query string, args ...interface{}) (sql.Result, error)
+	Get(dest interface{}, query string, args ...interface{}) error
+}
+
 // SimpleStore saves JSON in the db for applications
 type SimpleStore struct {
 	db     *sqlx.DB
@@ -33,9 +41,7 @@ func NewSimpleStore(connectionString string, logger api.LogService) (SimpleStore
 	return store, nil
 }
 
-// CreateaApplication saves an application in the db
-func (s SimpleStore) CreateApplication(app api.Application) error {
-
+func runCreateApplication(conn simpleConnection, app api.Application) error {
 	json, marshalErr := json.Marshal(app)
 	if marshalErr != nil {
 		return errors.Wrap(marshalErr, "Failed to convert application to JSON")
@@ -43,7 +49,7 @@ func (s SimpleStore) CreateApplication(app api.Application) error {
 
 	saveQuery := "INSERT INTO applications (account_id, body) VALUES ($1, $2)"
 
-	_, saveErr := s.db.Exec(saveQuery, app.AccountID, string(json))
+	_, saveErr := conn.Exec(saveQuery, app.AccountID, string(json))
 	if saveErr != nil {
 		return errors.Wrap(saveErr, "Failed to create Application")
 	}
@@ -51,9 +57,12 @@ func (s SimpleStore) CreateApplication(app api.Application) error {
 	return nil
 }
 
-// UpdateApplication updates an existing application
-func (s SimpleStore) UpdateApplication(app api.Application) error {
+// CreateApplication saves an application in the db
+func (s SimpleStore) CreateApplication(app api.Application) error {
+	return runCreateApplication(s.db, app)
+}
 
+func runUpdateApplication(conn simpleConnection, app api.Application) error {
 	json, marshalErr := json.Marshal(app)
 	if marshalErr != nil {
 		return errors.Wrap(marshalErr, "Failed to convert application to JSON")
@@ -61,7 +70,7 @@ func (s SimpleStore) UpdateApplication(app api.Application) error {
 
 	updateQuery := "UPDATE applications SET body = $1 WHERE account_id = $2"
 
-	result, saveErr := s.db.Exec(updateQuery, string(json), app.AccountID)
+	result, saveErr := conn.Exec(updateQuery, string(json), app.AccountID)
 	if saveErr != nil {
 		return errors.Wrap(saveErr, "Failed to save Application")
 	}
@@ -78,10 +87,20 @@ func (s SimpleStore) UpdateApplication(app api.Application) error {
 	return nil
 }
 
+// UpdateApplication updates an existing application
+func (s SimpleStore) UpdateApplication(app api.Application) error {
+	return runUpdateApplication(s.db, app)
+}
+
 // SaveSection saves a single section in a given application
 func (s SimpleStore) SaveSection(section api.Entity, accountID int) error {
-	// TODO: make a transaction.
-	app, loadErr := s.LoadApplication(accountID)
+
+	tx, txErr := s.db.Beginx()
+	if txErr != nil {
+		return txErr
+	}
+
+	app, loadErr := runLoadApplication(tx, accountID)
 	if loadErr != nil {
 		s.logger.WarnError("Unable to load the application before saving", loadErr, api.LogFields{"accountID": accountID})
 		return loadErr
@@ -89,7 +108,18 @@ func (s SimpleStore) SaveSection(section api.Entity, accountID int) error {
 
 	app.SetSection(section)
 
-	return s.UpdateApplication(app)
+	updateErr := runUpdateApplication(tx, app)
+	if updateErr != nil {
+		return updateErr
+	}
+
+	commitErr := tx.Commit()
+	if commitErr != nil {
+		return commitErr
+	}
+
+	return nil
+
 }
 
 type applicationRow struct {
@@ -102,8 +132,7 @@ type applicationAccountRow struct {
 	api.Account
 }
 
-// LoadApplication loads an application from the DB, it will return a NotFound error if it does not exist.
-func (s SimpleStore) LoadApplication(accountID int) (api.Application, error) {
+func runLoadApplication(conn simpleConnection, accountID int) (api.Application, error) {
 
 	selectQuery := `SELECT applications.account_id, applications.body,
 					accounts.id, accounts.form_version, accounts.form_type
@@ -111,7 +140,7 @@ func (s SimpleStore) LoadApplication(accountID int) (api.Application, error) {
 				WHERE applications.account_id = accounts.id AND accounts.id = $1`
 
 	row := applicationAccountRow{}
-	selectErr := s.db.Get(&row, selectQuery, accountID)
+	selectErr := conn.Get(&row, selectQuery, accountID)
 	if selectErr != nil {
 		if selectErr == sql.ErrNoRows {
 			return api.Application{}, api.ErrApplicationDoesNotExist
@@ -126,4 +155,9 @@ func (s SimpleStore) LoadApplication(accountID int) (api.Application, error) {
 	}
 
 	return app, nil
+}
+
+// LoadApplication loads an application from the DB, it will return a NotFound error if it does not exist.
+func (s SimpleStore) LoadApplication(accountID int) (api.Application, error) {
+	return runLoadApplication(s.db, accountID)
 }
