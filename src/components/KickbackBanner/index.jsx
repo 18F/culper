b607534abeeb745cf454234ce212cc2f71e61c99
Christopher Hui <christopher@truss.works>
2019-05-02 09:15:55 -0700

    add kickback text to locale file

--- src/components/KickbackBanner/index.jsx
+++ src/components/KickbackBanner/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { i18n } from 'config'
 
 const KickbackBanner = () => (
   <div className="usa-alert usa-alert-warning kickback-banner">
     <div className="usa-alert-body kickback-banner__wrapper">
       <span className="fa fa-exclamation-triangle kickback-banner__exclamation" />
-      <p className="usa-alert-text kickback-banner__text">Your application needs further edits after review by your agency representative. Please address their comments found in the email provided. Some yes/no questions have also been cleared, please provide your most current answers to these questions to ensure your application is accurate to the present time.</p>
+      <p className="usa-alert-text kickback-banner__text">{i18n.t('review.kickbackMessage')}</p>
     </div>
   </div>
 )
