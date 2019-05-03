664c3aeaf16250d9d82aaffee2f9227cfe7c34e9
MacRae Linton <55759+macrael@users.noreply.github.com>
2019-05-03 15:37:08 -0700

    Add comments to api/store.go

--- api/store.go
+++ api/store.go
@@ -11,8 +11,12 @@ var (
 
 // StorageService stores eapp related data
 type StorageService interface {
+	// CreateApplication creates an application in the db. Errors if already exists.
 	CreateApplication(app Application) error
+	// UpdateApplication replaces an existing application with the passed in one.
 	UpdateApplication(app Application) error
+	// SaveSection replaces a single section in the given application with the passed in section
 	SaveSection(section Section, accountID int) error
+	// LoadApplication returns the given application from the db.
 	LoadApplication(accountID int) (Application, error)
 }
