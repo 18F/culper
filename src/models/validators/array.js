f99a40d095215bf537d7ba00f768405445da12dd
Suzanne Rozier <suz@truss.works>
2019-05-17 11:13:43 -0400

    Use validateModel in array validator

--- src/models/validators/array.js
+++ src/models/validators/array.js
@@ -1,11 +1,11 @@
-import { validate } from 'validate.js'
+import { validateModel } from 'models/validate'
 
 const arrayValidator = (value = [], options) => {
   const { validator } = options
 
   for (let i = 0; i < value.length; i += 1) {
-    const itemErrors = validate({ value: value[i] }, { value: validator })
-    if (itemErrors) return itemErrors
+    const itemErrors = validateModel({ value: value[i] }, { value: validator })
+    if (itemErrors !== true) return itemErrors
   }
 
   return null
