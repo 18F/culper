4cd0f5d671b274e0ffe597fbe2e78ad8010cf991
MacRae Linton <55759+macrael@users.noreply.github.com>
2019-05-21 11:02:38 -0700

    Remove the old interface methods that were used with the old storage system.

--- api/pdf.go
+++ api/pdf.go
@@ -1,7 +1,5 @@
 package api
 
-import "time"
-
 // An ArchivalPdf represents a PDF (Portable Document Format) file that needs to be retained and submitted to e-QIP.
 type ArchivalPdf struct {
 	Name     string // document type name shown to users (e.g, Certification, Medical Release, etc.)
@@ -13,6 +11,4 @@ type ArchivalPdf struct {
 // PdfService declares operations to create and query archival PDFs.
 type PdfService interface {
 	GenerateReleases(account Account, app Application) ([]Attachment, error)
-	CreatePdf(application map[string]interface{}, pdfType ArchivalPdf, hash string) ([]byte, error)
-	SignatureAvailable(application map[string]interface{}, pdfType ArchivalPdf) (*time.Time, bool)
 }
