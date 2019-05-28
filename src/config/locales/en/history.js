81374729658f6f505012aca962b3da22c3b8ab9b
joshmfranklin <josh@truss.works>
2019-05-28 15:54:40 -0400

    shorten link text sothat it doesn't break mobile layout

--- src/config/locales/en/history.js
+++ src/config/locales/en/history.js
@@ -1630,7 +1630,7 @@ export const history = {
         'You are not required to list temporary locations of less than 90 days that did not serve as your permanent or mailing address.',
       name: 'School name',
       address: 'This address is',
-      addressLink: 'For correspondence/distance/extension/online schools, provide the address where the records are maintained. For assistance determining the school address, refer to [https://ope.ed.gov/accreditation/search.aspx](https://ope.ed.gov/accreditation/search.aspx).',
+      addressLink: 'For correspondence/distance/extension/online schools, provide the address where the records are maintained. For assistance determining the school address, refer to [https://ope.ed.gov/accreditation/search](https://ope.ed.gov/accreditation/search).',
       comments: 'Add optional comment',
       type: {
         highschool: 'High school',
@@ -1673,7 +1673,7 @@ export const history = {
         title:
           'For correspondence/distance/extension/online schools, provide the address where the records are maintained',
         message:
-          'For assistance determining the school address, refer to [https://ope.ed.gov/accreditation/search.aspx](https://ope.ed.gov/accreditation/search.aspx).',
+          'For assistance determining the school address, refer to [https://ope.ed.gov/accreditation/search](https://ope.ed.gov/accreditation/search).',
         note:
           'Acronyms: APO - Army Post Office, FPO - Fleet Post Office, DPO - Diplomatic Post Office.',
       },
