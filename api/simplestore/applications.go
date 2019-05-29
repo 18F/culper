c2955a93daad9d4d8a08bfb4eccbe714c889b0e3
MacRae Linton <55759+macrael@users.noreply.github.com>
2019-05-29 10:28:13 -0700

    Rollback the transaction if any of the db calls fail in the middle.

--- api/simplestore/applications.go
+++ api/simplestore/applications.go
@@ -71,6 +71,10 @@ func (s SimpleStore) SaveSection(section api.Section, accountID int) error {
 	app, loadErr := runLoadApplication(tx, s.serializer, accountID, true)
 	if loadErr != nil {
 		s.logger.WarnError("Unable to load the application before saving", loadErr, api.LogFields{"accountID": accountID})
+		rollErr := tx.Rollback()
+		if rollErr != nil {
+			s.logger.WarnError("DB error trying to roll back the transaction", rollErr, api.LogFields{"accountID": accountID})
+		}
 		return loadErr
 	}
 
@@ -78,6 +82,10 @@ func (s SimpleStore) SaveSection(section api.Section, accountID int) error {
 
 	updateErr := runUpdateApplication(tx, s.serializer, app)
 	if updateErr != nil {
+		rollErr := tx.Rollback()
+		if rollErr != nil {
+			s.logger.WarnError("DB error trying to roll back the transaction", rollErr, api.LogFields{"accountID": accountID})
+		}
 		return updateErr
 	}
 
