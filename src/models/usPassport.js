7da347e2bea877ed74722da11256c02a0da52e05
Suzanne Rozier <suz@truss.works>
2019-06-03 11:01:14 -0400

    Model, tests, validator for US Passport

---
+++ src/models/usPassport.js
@@ -0,0 +1,47 @@
+import { hasYesOrNo, checkValue } from 'models/validate'
+import name from 'models/shared/name'
+import { passportPattern } from 'constants/patterns'
+import { cleanDateObject, createDateFromObject } from 'helpers/date'
+
+const usPassport = {
+  HasPassports: {
+    presence: true,
+    hasValue: { validator: hasYesOrNo },
+  },
+  Name: (value, attributes) => (
+    checkValue(attributes.HasPassports, 'No')
+      ? {}
+      : {
+        presence: true,
+        model: { validator: name },
+      }
+  ),
+  Number: (value, attributes) => {
+    const { HasPassports, Dates } = attributes
+    if (checkValue(HasPassports, 'No')) return {}
+
+    const validations = {
+      presence: true,
+      hasValue: { validator: { format: passportPattern } },
+    }
+
+    // If issued in 1990 or after, must be 9 characters long
+    const issuedDate = Dates && Dates.from && createDateFromObject(cleanDateObject(Dates.from))
+    const dateThreshold = createDateFromObject({ day: 1, month: 1, year: 1990 })
+    if (issuedDate && (issuedDate >= dateThreshold)) {
+      validations.hasValue.validator.length = { is: 9 }
+    }
+
+    return validations
+  },
+  Dates: (value, attributes) => (
+    checkValue(attributes.HasPassports, 'No')
+      ? {}
+      : {
+        presence: true,
+        daterange: true,
+      }
+  ),
+}
+
+export default usPassport
