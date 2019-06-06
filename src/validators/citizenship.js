12fbf949525452ec7d9c5de2fc4603a621512c82
Suzanne Rozier <suz@truss.works>
2019-06-06 09:43:19 -0400

    Use selector for require foreign born doc option

--- src/validators/citizenship.js
+++ src/validators/citizenship.js
@@ -1,5 +1,5 @@
 import store from 'services/store'
-import { hasValidUSPassport } from 'validators/passport'
+import { selectValidUSPassport } from 'selectors/misc'
 
 import { validateModel } from 'models/validate'
 import citizenshipStatus, {
@@ -7,11 +7,13 @@ import citizenshipStatus, {
   requireDocumentationFields,
 } from 'models/citizenshipStatus'
 
-export const validateCitizenshipStatus = data => (
-  validateModel(data, citizenshipStatus, {
-    requireForeignBornDocumentation: hasValidUSPassport(store.getState()),
+export const validateCitizenshipStatus = (data) => {
+  const { hasValidUSPassport } = selectValidUSPassport(store.getState())
+
+  return validateModel(data, citizenshipStatus, {
+    requireForeignBornDocumentation: !hasValidUSPassport,
   }) === true
-)
+}
 
 export const isCertificateRequired = (data, requireForeignBornDocumentation) => (
   requireCertificateFields(data, { requireForeignBornDocumentation })
