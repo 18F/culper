9342a0b3fac0937380d005ebd1dd43400f4db44d
Suzanne Rozier <suz@truss.works>
2019-06-11 16:56:34 -0400

    Foreign CoOwner model

--- src/models/shared/__tests__/foreignCoOwner.test.js
+++ src/models/shared/__tests__/foreignCoOwner.test.js
@@ -2,39 +2,71 @@ import { validateModel } from 'models/validate'
 import foreignCoOwner from '../foreignCoOwner'
 
 describe('The foreignCoOwner model', () => {
-  it('Name is required', () => {
+  it('Name, Address, Countries, Relationship nature are required', () => {
+    const testData = {}
+    const expectedErrors = [
+      'Name.required',
+      'Address.required',
+      'Countries.required',
+      'RelationshipNature.required',
+    ]
 
+    expect(validateModel(testData, foreignCoOwner))
+      .toEqual(expect.arrayContaining(expectedErrors))
   })
 
   it('Name must be a valid name', () => {
+    const testData = {
+      Name: { first: 'my', last: 'name' },
+    }
+    const expectedErrors = ['Name.model']
 
-  })
-
-  it('Address is required', () => {
-
+    expect(validateModel(testData, foreignCoOwner))
+      .toEqual(expect.arrayContaining(expectedErrors))
   })
 
   it('Address must be a valid address', () => {
+    const testData = {
+      Address: { city: 'somewhere', state: 'NY' },
+    }
+    const expectedErrors = ['Address.location']
 
-  })
-
-  it('Countries is required', () => {
-
+    expect(validateModel(testData, foreignCoOwner))
+      .toEqual(expect.arrayContaining(expectedErrors))
   })
 
   it('Countries must have at least one value', () => {
+    const testData = {
+      Countries: { value: [] },
+    }
+    const expectedErrors = ['Countries.hasValue']
 
-  })
-
-  it('RelationshipNature is required', () => {
-
+    expect(validateModel(testData, foreignCoOwner))
+      .toEqual(expect.arrayContaining(expectedErrors))
   })
 
   it('RelationshipNature must have a value', () => {
+    const testData = {
+      RelationshipNature: { values: 'invalid' },
+    }
+    const expectedErrors = ['RelationshipNature.hasValue']
 
+    expect(validateModel(testData, foreignCoOwner))
+      .toEqual(expect.arrayContaining(expectedErrors))
   })
 
   it('passes a valid ForeignCoOwner', () => {
-    
+    const testData = {
+      Name: { first: 'my', middle: 'full', last: 'name' },
+      Address: {
+        street: '123 Coowner St',
+        city: 'London',
+        country: 'United Kingdom',
+      },
+      Countries: { value: ['United States', 'Canada'] },
+      RelationshipNature: { value: 'Some test thing' },
+    }
+
+    expect(validateModel(testData, foreignCoOwner)).toEqual(true)
   })
 })
--- src/models/shared/foreignCoOwner.js
+++ src/models/shared/foreignCoOwner.js
@@ -1,5 +1,20 @@
-const foreignCoOwner = {
+import name from 'models/shared/name'
+import address from 'models/shared/locations/address'
 
+const foreignCoOwner = {
+  Name: { presence: true, model: { validator: name } },
+  Address: {
+    presence: true,
+    location: { validator: address },
+  },
+  Countries: {
+    presence: true,
+    hasValue: { validator: { length: { minimum: 1 } } },
+  },
+  RelationshipNature: {
+    presence: true,
+    hasValue: true,
+  },
 }
 
 export default foreignCoOwner
