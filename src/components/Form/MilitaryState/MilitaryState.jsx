680a7e44b6948f064c971153746bdd4528383314
Suzanne Rozier <suz@truss.works>
2019-06-11 11:09:53 -0400

    State component cleanup and eslint fixes

--- src/components/Form/MilitaryState/MilitaryState.jsx
+++ src/components/Form/MilitaryState/MilitaryState.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import ValidationElement from '../ValidationElement'
-import Dropdown from '../Dropdown'
-import State from '../State'
-import { militaryStates } from '../../../validators/location'
+
+import militaryStates from 'constants/enums/militaryStates'
+
+import ValidationElement from 'components/Form/ValidationElement'
+import Dropdown from 'components/Form/Dropdown'
+import State from 'components/Form/State'
 
 export default class MilitaryState extends ValidationElement {
   render() {
@@ -25,36 +27,33 @@ export default class MilitaryState extends ValidationElement {
           additionalStates={militaryStates}
         />
       )
-    } else {
-      return (
-        <Dropdown
-          name={this.props.name}
-          label={this.props.label}
-          placeholder={this.props.placeholder}
-          className={this.props.className}
-          disabled={this.props.disabled}
-          onUpdate={this.props.onUpdate}
-          onError={this.props.onError}
-          onBlur={this.props.onBlur}
-          onFocus={this.props.onFocus}
-          required={this.props.required}
-          tabBack={this.props.tabBack}
-          tabNext={this.props.tabNext}>
-          <option key="aa" value="AA">
-            U.S. Armed Forces - Americas
-          </option>
-          <option key="ae" value="AE">
-            U.S. Armed Forces - Europe
-          </option>
-          <option key="ap" value="AP">
-            U.S. Armed Forces - Pacific
-          </option>
-        </Dropdown>
-      )
     }
+
+    return (
+      <Dropdown
+        name={this.props.name}
+        label={this.props.label}
+        placeholder={this.props.placeholder}
+        className={this.props.className}
+        disabled={this.props.disabled}
+        onUpdate={this.props.onUpdate}
+        onError={this.props.onError}
+        onBlur={this.props.onBlur}
+        onFocus={this.props.onFocus}
+        required={this.props.required}
+        tabBack={this.props.tabBack}
+        tabNext={this.props.tabNext}
+      >
+        {militaryStates.map(s => (
+          <option key={s.postalCode} value={s.postalCode}>
+            {s.name}
+          </option>
+        ))}
+      </Dropdown>
+    )
   }
 }
 
 MilitaryState.defaultProps = {
-  required: false
+  required: false,
 }
