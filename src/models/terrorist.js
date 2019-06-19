cc0bba859a7d8857e6e25949a87a7b9fa6943a0f
Suzanne Rozier <suz@truss.works>
2019-06-19 15:35:59 -0400

    Models and validators for legal assoc terrorist, overthrow, and violence

---
+++ src/models/terrorist.js
@@ -0,0 +1,26 @@
+import address from 'models/shared/locations/address'
+
+const terrorist = {
+  Organization: { presence: true, hasValue: true },
+  Address: { presence: true, location: { validator: address } },
+  Dates: { presence: true, daterange: true },
+  Positions: (value, attributes) => {
+    if (attributes.PositionsNotApplicable
+      && attributes.PositionsNotApplicable.applicable === false) {
+      return {}
+    }
+
+    return { presence: true, hasValue: true }
+  },
+  Contributions: (value, attributes) => {
+    if (attributes.ContributionsNotApplicable
+      && attributes.ContributionsNotApplicable.applicable === false) {
+      return {}
+    }
+
+    return { presence: true, hasValue: true }
+  },
+  Reasons: { presence: true, hasValue: true },
+}
+
+export default terrorist
