02585925656977615bfa30bf96a22d669d800681
joshmfranklin <josh@truss.works>
2019-06-19 12:54:34 -0400

    Changed label to 'Provide current address for this person'

--- src/config/locales/en/reference.js
+++ src/config/locales/en/reference.js
@@ -4,24 +4,24 @@ export const reference = {
     contact: 'Provide date of last contact',
     relationship: 'Relationship',
     phone: {
-      default: "Provide the telephone number for this person",
+      default: 'Provide the telephone number for this person',
       evening: "Provide this person's evening phone number",
       day: "Provide this person's daytime phone number",
-      mobile: "Provide this person's cell/mobile phone number"
+      mobile: "Provide this person's cell/mobile phone number",
     },
     email: "Provide this person's email address",
     correspondence: 'Their contact information',
-    address: "Provide this person's street address",
+    address: 'Provide current address for this person',
     employment: {
-      address: 'Provide the address of this verifier'
-    }
+      address: 'Provide the address of this verifier',
+    },
   },
   para: {
     correspondence:
       'Provide the following contact information for this person.',
     address:
       'Provide street address for this person (including apartment number).',
-    or: 'or'
+    or: 'or',
   },
   label: {
     relationship: {
@@ -37,8 +37,8 @@ export const reference = {
     idk: "I don't know",
     address: 'The address of your contact during this time period',
     employment: {
-      address: 'The address of your verifier'
-    }
+      address: 'The address of your verifier',
+    },
   },
   name: {
     label: {
@@ -61,37 +61,37 @@ export const reference = {
       vii: 'VII',
       viii: 'VIII',
       ix: 'IX',
-      x: 'X'
-    }
+      x: 'X',
+    },
   },
   help: {
     contact: {
       title: 'Estimate the date if unsure',
       message:
         'If you are not sure of the exact date give us your best guess and check the "Estimated" checkbox.',
-      note: ''
+      note: '',
     },
     relationship: {
       title: 'Need help defining your relationship?',
       message:
         'Please define your relationship with the person during this period.',
-      note: ''
+      note: '',
     },
     email: {
       title: 'Please use an active email',
       message: 'Make sure this person can be reached at this email address.',
-      note: ''
+      note: '',
     },
     phone: {
       title: 'Please use an active phone number',
       message: 'Make sure this person can be reached at this number.',
-      note: ''
+      note: '',
     },
     address: {
       title: 'Acronyms:',
       message:
         'APO - Army Post Office, FPO - Fleet Post Office, DPO - Diplomatic Post Office.',
-      note: ''
-    }
-  }
+      note: '',
+    },
+  },
 }
