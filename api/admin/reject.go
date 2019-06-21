e405c15cf88a37571cb6641c437a01d0787c9563
MacRae Linton <55759+macrael@users.noreply.github.com>
2019-06-21 15:10:35 -0700

    Delete all previous signed releases on Reject.

--- api/admin/reject.go
+++ api/admin/reject.go
@@ -1,9 +1,12 @@
 package admin
 
 import (
+	"fmt"
 	"github.com/pkg/errors"
+	"strings"
 
 	"github.com/18F/e-QIP-prototype/api"
+	"github.com/18F/e-QIP-prototype/api/pdf"
 )
 
 // Rejecter is used to reject/kickback an application
@@ -29,12 +32,7 @@ func (r Rejecter) Reject(account api.Account) error {
 		return errors.Wrap(err, "Reject failed to unlock account")
 	}
 
-	// TODO: port over PDF.RemovePdfs.
-	// err = r.PDF.RemovePdfs(account)
-	// if err != nil {
-	// 	return errors.Wrap(err, "Reject failed to remove PDFs")
-	// }
-
+	// Load the application, clear the nos, save the application.
 	app, loadErr := r.store.LoadApplication(account.ID)
 	if loadErr != nil {
 		if loadErr == api.ErrApplicationDoesNotExist {
@@ -51,6 +49,38 @@ func (r Rejecter) Reject(account api.Account) error {
 	saveErr := r.store.UpdateApplication(app)
 	if saveErr != nil {
 		return errors.Wrap(saveErr, "Unable to save application after rejecting it")
+	}
+
+	// load the pfds, remove the signature pdfs.
+	attachmentsMetadata, listErr := r.store.ListAttachmentsMetadata(account.ID)
+	if listErr != nil {
+		return errors.Wrap(listErr, "Failed to load the current attachments in order to delete them")
+	}
+
+	// Get the list of PDF Types that correspond to the signed releases
+	releaseTypeSet := make(map[string]bool)
+	for _, release := range pdf.ReleasePDFs {
+		releaseTypeSet[release.DocType] = true
+	}
+
+	var deletionErrors []error
+	for _, attachment := range attachmentsMetadata {
+		if _, ok := releaseTypeSet[attachment.DocType]; ok {
+			delErr := r.store.DeleteAttachment(account.ID, attachment.ID)
+			if delErr != nil {
+				deletionErrors = append(deletionErrors, delErr)
+			}
+		}
+	}
+
+	if len(deletionErrors) != 0 {
+		var errStrings []string
+		for _, err := range deletionErrors {
+			errStrings = append(errStrings, err.Error())
+		}
+		joinedErrs := strings.Join(errStrings, ", ")
+
+		return errors.New(fmt.Sprintf("Got an error deleting %d documents: [%s]", len(deletionErrors), joinedErrs))
 	}
 
 	return nil
