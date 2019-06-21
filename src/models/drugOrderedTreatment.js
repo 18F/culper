23415b07242dafee9811425423b4fbb21d5e1379
Suzanne Rozier <suz@truss.works>
2019-06-21 12:08:59 -0400

    Model and validator for DrugOrderedTreatments

---
+++ src/models/drugOrderedTreatment.js
@@ -0,0 +1,67 @@
+import { hasYesOrNo, checkValue } from 'models/validate'
+import address from 'models/shared/locations/address'
+import phone from 'models/shared/phone'
+
+const drugOrderedTreatment = {
+  Explanation: { presence: true, hasValue: true },
+  ActionTaken: { presence: true, hasValue: { validator: hasYesOrNo } },
+  OrderedBy: (value) => {
+    const length = { minimum: 1 }
+
+    if (value && value.values && value.values.includes('None')) {
+      length.maximum = 1
+    }
+
+    return {
+      presence: true,
+      array: {
+        validator: { presence: true },
+        length,
+      },
+    }
+  },
+  TreatmentProvider: (value, attributes) => {
+    if (checkValue(attributes.ActionTaken, 'Yes')) {
+      return { presence: true, hasValue: true }
+    }
+    return {}
+  },
+  TreatmentProviderAddress: (value, attributes) => {
+    if (checkValue(attributes.ActionTaken, 'Yes')) {
+      return { presence: true, location: { validator: address } }
+    }
+    return {}
+  },
+  TreatmentProviderTelephone: (value, attributes) => {
+    if (checkValue(attributes.ActionTaken, 'Yes')) {
+      return { presence: true, model: { validator: phone } }
+    }
+    return {}
+  },
+  TreatmentDates: (value, attributes) => {
+    if (checkValue(attributes.ActionTaken, 'Yes')) {
+      return { presence: true, daterange: true }
+    }
+    return {}
+  },
+  TreatmentCompleted: (value, attributes) => {
+    if (checkValue(attributes.ActionTaken, 'Yes')) {
+      return { presence: true, hasValue: { validator: hasYesOrNo } }
+    }
+    return {}
+  },
+  NoTreatmentExplanation: (value, attributes) => {
+    if (checkValue(attributes.TreatmentCompleted, 'No')) {
+      return { presence: true, hasValue: true }
+    }
+    return {}
+  },
+  NoActionTakenExplanation: (value, attributes) => {
+    if (checkValue(attributes.ActionTaken, 'No')) {
+      return { presence: true, hasValue: true }
+    }
+    return {}
+  },
+}
+
+export default drugOrderedTreatment
