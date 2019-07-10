ba63d431384b90de5720704102f9612b1476ab7d
Suzanne Rozier <suz@truss.works>
2019-07-10 10:46:41 -0400

    Pass required prop on the review screen

--- src/components/Section/History/Federal/FederalWrapper.jsx
+++ src/components/Section/History/Federal/FederalWrapper.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import i18n from 'util/i18n'
 
 import ConnectedFederal from './Federal'
 
-const FederalWrapper = () => (
+const FederalWrapper = ({ inReview }) => (
   <div>
     <h1 className="section-header">
       {i18n.t('history.destination.federal')}
     </h1>
 
-    <ConnectedFederal />
+    <ConnectedFederal required={inReview} />
   </div>
 )
+
+FederalWrapper.propTypes = {
+  inReview: PropTypes.bool,
+}
+
+FederalWrapper.defaultProps = {
+  inReview: false,
+}
 
 export default FederalWrapper
