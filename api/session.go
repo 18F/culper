dc53e88897913889d5ab03fdb0d3a89211b8ce3c
Jeri Sommers 🐍 <sojeri@truss.works>
2019-07-15 09:58:50 -0700

    WIP integration test for creating new session

--- api/session.go
+++ api/session.go
@@ -13,7 +13,7 @@ type SessionService interface {
 	// UserDidAuthenticate creates a session for a newly logged in user
 	UserDidAuthenticate(accountID int) (sessionKey string, err error)
 	// GetAccountIfSessionIsValid returns an account if the session is valid, or ErrSessionInvalid otherwise
-	GetAccountIfSessionIsValid(sessionKey string) (accontID int, err error)
+	GetAccountIfSessionIsValid(sessionKey string) (account Account, err error)
 	// UserDidLogout invalidates a sessino for a newly logged out user
 	UserDidLogout(sessionKey string) error
 }
