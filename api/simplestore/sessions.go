a64832dfedf3c1723ea0b1f288566c6c47ea7a40
MacRae Linton <55759+macrael@users.noreply.github.com>
2019-07-15 13:46:26 -0400

    got it building again

--- api/simplestore/sessions.go
+++ api/simplestore/sessions.go
@@ -1,8 +1,23 @@
 package simplestore
 
-import "time"
+import (
+	"time"
+
+	"github.com/18F/e-QIP-prototype/api"
+)
 
 // CreateOrUpdateSession returns session key or error
 func (s SimpleStore) CreateOrUpdateSession(accountID int, sessionKey string, timeout time.Duration) error {
 	// do something
+	return nil
+}
+
+// DeleteSession removes a session record from the db
+func (s SimpleStore) DeleteSession(sessionKey string) error {
+	return nil
+}
+
+// FetchSessionAccount fetches an account and session data from the db
+func (s SimpleStore) FetchSessionAccount(sessionKey string) (api.Account, error) {
+	return api.Account{}, nil
 }
