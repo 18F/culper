842f4a8f611947b1fe9320e2a2a7602cc7cc835f
Suzanne Rozier <suz@truss.works>
2019-07-19 15:57:00 -0400

    Add actions and reducer handlers for updating section errors and complete

--- src/actions/FormActions.test.js
+++ src/actions/FormActions.test.js
@@ -1,5 +1,9 @@
 import * as actionTypes from 'constants/actionTypes'
-import { updateSubsectionData } from './FormActions'
+import {
+  updateSubsectionData,
+  updateSubsectionErrors,
+  updateSubsectionComplete,
+} from './FormActions'
 
 describe('Form actions', () => {
   it('should create an action for updating section data', () => {
@@ -11,6 +15,28 @@ describe('Form actions', () => {
     }
 
     expect(updateSubsectionData('IDENTIFICATION_NAME', 'Name', { first: 'test data' }))
+      .toEqual(expectedAction)
+  })
+
+  it('should create an action for updating a section’s errors', () => {
+    const expectedAction = {
+      type: actionTypes.UPDATE_SUBSECTION_ERRORS,
+      key: 'IDENTIFICATION_NAME',
+      errors: ['test error 1', 'test error 2'],
+    }
+
+    expect(updateSubsectionErrors('IDENTIFICATION_NAME', ['test error 1', 'test error 2']))
+      .toEqual(expectedAction)
+  })
+
+  it('should create an action for updating whether a section is complete', () => {
+    const expectedAction = {
+      type: actionTypes.UPDATE_SUBSECTION_COMPLETE,
+      key: 'IDENTIFICATION_NAME',
+      complete: true,
+    }
+
+    expect(updateSubsectionComplete('IDENTIFICATION_NAME', true))
       .toEqual(expectedAction)
   })
 })
