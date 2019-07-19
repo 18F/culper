2536ff9ddec3836160dac20c4d929ee267d775c5
Suzanne Rozier <suz@truss.works>
2019-07-19 15:57:00 -0400

    Add actions and reducer handlers for updating section errors and complete

--- src/actions/FormActions.js
+++ src/actions/FormActions.js
@@ -1,9 +1,21 @@
 /* eslint import/prefer-default-export: 0 */
 import * as actionTypes from 'constants/actionTypes'
 
-export const updateSubsectionData = (sectionKey, fieldName, fieldValues) => ({
+export const updateSubsectionData = (sectionKey, fieldName, fieldValues = {}) => ({
   type: actionTypes.UPDATE_SUBSECTION_DATA,
   key: sectionKey,
   field: fieldName,
   data: fieldValues,
+})
+
+export const updateSubsectionErrors = (sectionKey, errors = []) => ({
+  type: actionTypes.UPDATE_SUBSECTION_ERRORS,
+  key: sectionKey,
+  errors,
+})
+
+export const updateSubsectionComplete = (sectionKey, complete) => ({
+  type: actionTypes.UPDATE_SUBSECTION_COMPLETE,
+  key: sectionKey,
+  complete,
 })
