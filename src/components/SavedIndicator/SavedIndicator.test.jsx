78960e70b7799451da6c5af6e39fda5a26116234
Suzanne Rozier <suz@truss.works>
2019-07-31 10:13:48 -0400

    Fix conflict, snapshot, tests

--- src/components/SavedIndicator/SavedIndicator.test.jsx
+++ src/components/SavedIndicator/SavedIndicator.test.jsx
@@ -3,13 +3,8 @@ import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import { mount } from 'enzyme'
-<<<<<<< HEAD
-import SavedIndicator from 'components/SavedIndicator/SavedIndicator'
+import ConnectedSavedIndicator from 'components/SavedIndicator/SavedIndicator'
 import { i18n } from 'config'
-=======
-import SavedIndicator from './SavedIndicator'
-import { i18n } from '../../config'
->>>>>>> Provide feedback to user if form fails to save
 
 describe('The saved indicator component', () => {
   // Setup
@@ -20,20 +15,19 @@ describe('The saved indicator component', () => {
   it('catches failed save', () => {
     const store = mockStore({
       authentication: {
-        authenticated: true
+        authenticated: true,
       },
       application: {
         Settings: {
-          saveError: true
-        }
-      }
+          saveError: true,
+        },
+      },
     })
     const component = mount(
       <Provider store={store}>
-        <SavedIndicator />
+        <ConnectedSavedIndicator />
       </Provider>
     )
-<<<<<<< HEAD
     expect(component.find('strong').text()).toContain(
       i18n.t('saved.error.title')
     )
@@ -44,12 +38,11 @@ describe('The saved indicator component', () => {
 
     beforeEach(() => {
       const store = mockStore({ authentication: { authenticated: true } })
-      createComponent = (elapsed = 0) =>
-        mount(
-          <Provider store={store}>
-            <SavedIndicator elapsed={elapsed} />
-          </Provider>
-        )
+      createComponent = (elapsed = 0) => mount(
+        <Provider store={store}>
+          <ConnectedSavedIndicator elapsed={elapsed} />
+        </Provider>
+      )
     })
 
     it('visible when authenticated', () => {
@@ -69,63 +62,6 @@ describe('The saved indicator component', () => {
       expect(component.find('.time').text()).toContain('min')
     })
 
-=======
-    expect(component.find('button').length).toEqual(0)
-    window.token = 'fake-token'
-  })
-
-  it('catches failed save', () => {
-    const store = mockStore({
-      authentication: {
-        authenticated: true
-      },
-      application: {
-        Settings: {
-          saveError: true
-        }
-      }
-    })
-    const component = mount(
-      <Provider store={store}>
-        <SavedIndicator />
-      </Provider>
-    )
-    expect(component.find('strong').text()).toContain(
-      i18n.t('saved.error.title')
-    )
-  })
-
-  describe('when authenticated', () => {
-    let createComponent
-
-    beforeEach(() => {
-      const store = mockStore({ authentication: { authenticated: true } })
-      createComponent = (elapsed = 0) =>
-        mount(
-          <Provider store={store}>
-            <SavedIndicator elapsed={elapsed} />
-          </Provider>
-        )
-    })
-
-    it('visible when authenticated', () => {
-      const component = createComponent()
-      expect(component.find('button').length).toEqual(1)
-    })
-
-    it('displays in seconds if under a minute', () => {
-      const elapsed = 10 * 1000
-      const component = createComponent(elapsed)
-      expect(component.find('.time').text()).toContain('sec')
-    })
-
-    it('displays in minutes if under an hour', () => {
-      const elapsed = 60 * 1000
-      const component = createComponent(elapsed)
-      expect(component.find('.time').text()).toContain('min')
-    })
-
->>>>>>> Provide feedback to user if form fails to save
     it('displays in hours if under a day', () => {
       const elapsed = 60 * 60 * 1000
       const component = createComponent(elapsed)
@@ -139,7 +75,6 @@ describe('The saved indicator component', () => {
     })
 
     it('mouse in and out', () => {
-      const store = mockStore({ authentication: { authenticated: true } })
       const component = createComponent()
       component.find('button').simulate('mouseenter')
       expect(component.find('SavedIndicator').getNode().state.hover).toBe(true)
