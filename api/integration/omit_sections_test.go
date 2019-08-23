9aa3aed98f3447f76496da2be492a4c79eb1b51d
MacRae Linton <55759+macrael@users.noreply.github.com>
2019-08-23 13:26:01 -0700

    Modify templates for SF85 (and some of SF85P)

--- api/integration/omit_sections_test.go
+++ api/integration/omit_sections_test.go
@@ -21,57 +21,68 @@ func Test85SectionsOmitted(t *testing.T) {
 	pdfsvc := pdf.NewPDFService(pdfTemplatePath)
 	submitter := admin.NewSubmitter(services.db, services.store, xmlsvc, pdfsvc)
 
-	// provision an 85
-	account := create85TestAccount(t, services.db)
-	fmt.Println(account)
-
-	// load a complete scenario
-	form := readTestData(t, "../testdata/complete-scenarios/SF85/test3.json")
-	saveFormJSON(t, services, form, account)
-
-	// convert to XML
-	xml, _, xmlErr := submitter.FilesForSubmission(account.ID)
-	if xmlErr != nil {
-		t.Fatal(xmlErr)
+	scenarios := []string{
+		"../testdata/complete-scenarios/SF85/test1.json",
+		"../testdata/complete-scenarios/SF85/test2.json",
+		"../testdata/complete-scenarios/SF85/test3.json",
 	}
 
-	// test that section is missing
-	// fmt.Println("OUT", string(xml))
+	for _, scenario := range scenarios {
 
-	if bytes.Contains(xml, []byte("<ForeignContacts")) {
-		t.Log("Didn't remove ForeignContacts")
-		t.Fail()
-	}
+		// provision an 85
+		account := create85TestAccount(t, services.db)
+		fmt.Println(account)
 
-	if bytes.Contains(xml, []byte("<ForeignTravels")) {
-		t.Log("Didn't remove ForeignTravels")
-		t.Fail()
-	}
+		// load a complete scenario
+		form := readTestData(t, scenario)
+		saveFormJSON(t, services, form, account)
 
-	// If a full complement of XSDs are present in the directory ./xsd, we attempt to use xmllint
-	// to validate that the generated XML matches.
-	_, statErr := os.Stat("xsds")
-	if !os.IsNotExist(statErr) {
-		filePath := "/tmp/sf85_omitted.xml"
-		f, createErr := os.Create(filePath)
-		if createErr != nil {
-			t.Fatal(createErr)
+		// convert to XML
+		xml, _, xmlErr := submitter.FilesForSubmission(account.ID)
+		if xmlErr != nil {
+			t.Fatal(xmlErr)
 		}
-		_, writeErr := f.Write(xml)
-		if writeErr != nil {
-			t.Fatal(writeErr)
-		}
-		f.Close()
 
-		linterArgs := []string{"--schema", "xsds/Incoming/SF85_2017-12-draft7_ImportFormat.xsd",
-			"/tmp/sf85_omitted.xml", "--noout"}
-		fmt.Println(linterArgs)
-		output, err := exec.Command("xmllint", linterArgs...).CombinedOutput()
-		if err != nil {
-			t.Fatal("XML does not validate: " + string(output))
+		// test that section is missing
+		// fmt.Println("OUT", string(xml))
+
+		if bytes.Contains(xml, []byte("<ForeignContacts")) {
+			t.Log("Didn't remove ForeignContacts")
+			t.Fail()
 		}
+
+		if bytes.Contains(xml, []byte("<ForeignTravels")) {
+			t.Log("Didn't remove ForeignTravels")
+			t.Fail()
+		}
+
+		// If a full complement of XSDs are present in the directory ./xsd, we attempt to use xmllint
+		// to validate that the generated XML matches.
+		_, statErr := os.Stat("xsds")
+		if !os.IsNotExist(statErr) {
+			filePath := "/tmp/sf85_omitted.xml"
+			f, createErr := os.Create(filePath)
+			if createErr != nil {
+				t.Fatal(createErr)
+			}
+			_, writeErr := f.Write(xml)
+			if writeErr != nil {
+				t.Fatal(writeErr)
+			}
+			f.Close()
+
+			linterArgs := []string{"--schema", "xsds/Incoming/SF85_2017-12-draft7_ImportFormat.xsd",
+				"/tmp/sf85_omitted.xml", "--noout"}
+			fmt.Println(linterArgs)
+			output, err := exec.Command("xmllint", linterArgs...).CombinedOutput()
+			if err != nil {
+				t.Fatal("XML does not validate: " + string(output))
+			}
+		}
+
+		t.Log("Scenario: ", scenario, "validated sucessfully.")
+
 	}
-
 	t.Fatal("NOPE")
 }
 
