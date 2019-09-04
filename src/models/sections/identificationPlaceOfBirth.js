7e5a2815aef95c75b458156dddff33375e574df7
Edwin <eleong@actionet.com>
2019-09-04 16:43:25 -0400

    EN-3886 fix identificationPlaceOfBirth model

--- src/models/sections/identificationPlaceOfBirth.js
+++ src/models/sections/identificationPlaceOfBirth.js
@@ -2,13 +2,13 @@
 import birthplace from 'models/shared/locations/birthplace'
 
 const identificationPlaceOfBirth = {
-  Location: {
-    presence: true,
-    location: (value, attributes) => {
-      return {
+  Location: (value, attributes) => {
+    return {
+      presence: true,
+      model: {
         validator: birthplace, requireCity: !value.county, requireCounty: !value.city
       }
-    },
+    }
   },
 }
 
