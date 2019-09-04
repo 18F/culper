51101db71106b78433b912f3911e3299b04cba27
Edwin <eleong@actionet.com>
2019-09-04 16:20:08 -0400

    EN-3886 moved validation to src\models

--- src/models/sections/identificationPlaceOfBirth.js
+++ src/models/sections/identificationPlaceOfBirth.js
@@ -4,7 +4,11 @@ import birthplace from 'models/shared/locations/birthplace'
 const identificationPlaceOfBirth = {
   Location: {
     presence: true,
-    location: { validator: birthplace, requireCity: !data.Location.county, requireCounty: !data.Location.city },
+    location: (value, attributes) => {
+      return {
+        validator: birthplace, requireCity: !value.county, requireCounty: !value.city
+      }
+    },
   },
 }
 
--- src/validators/identificationbirthplace.js
+++ src/validators/identificationbirthplace.js
@@ -1,15 +1,8 @@
 /* eslint-disable import/prefer-default-export */
 import { validateModel } from 'models/validate'
-import birthplace from 'models/shared/locations/birthplace'
+import identificationPlaceOfBirth from 'models/sections/identificationPlaceOfBirth'
 
 export const validateIdentificationBirthPlace = (data) => {
 
-  const applicantBirthPlaceModel = {
-    Location: {
-      presence: true,
-      location: { validator: birthplace, requireCity: !data.Location.county, requireCounty: !data.Location.city },
-    },
-  }
-
-  return validateModel(data, applicantBirthPlaceModel)
+  return validateModel(data, identificationPlaceOfBirth)
 }
