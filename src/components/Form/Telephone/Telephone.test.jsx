054392ca3bc88dac8ae0fa3a1b0d81dbb1d57410
Edwin <eleong@actionet.com>
2019-09-04 14:36:01 -0400

    EN-3906 updated test cases

--- src/components/Form/Telephone/Telephone.test.jsx
+++ src/components/Form/Telephone/Telephone.test.jsx
@@ -7,7 +7,6 @@ describe('The Telephone component', () => {
     const component = mount(<Telephone name="phone" type="International" />)
     expect(component.find('.domestic-number').length).toEqual(1)
     expect(component.find('input[name="int_first"]').length).toEqual(1)
-    expect(component.find('input[name="int_second"]').length).toEqual(1)
   })
 
   it('populates domestic fields using number', () => {
@@ -142,7 +141,7 @@ describe('The Telephone component', () => {
     component.find('.nonumber input').simulate('change')
     component.find('.time.day input').simulate('change')
     component.find('.phonetype-option.work input').simulate('change')
-    expect(updated).toBe(6)
+    expect(updated).toBe(5)
   })
 
   it('can autotab forward', () => {
@@ -181,13 +180,7 @@ describe('The Telephone component', () => {
     tabbed = false
     componentInternational
       .find({ type: 'text', name: 'int_first' })
-      .simulate('keydown', { keyCode: 48, target: { value: '123' } })
-    expect(tabbed).toBe(true)
-
-    tabbed = false
-    componentInternational
-      .find({ type: 'text', name: 'int_extension' })
-      .simulate('keydown', { keyCode: 48, target: { value: '1234567890' } })
+      .simulate('keydown', { keyCode: 48, target: { value: '12345678901234567890' } })
     expect(tabbed).toBe(true)
   })
 
@@ -227,12 +220,6 @@ describe('The Telephone component', () => {
     tabbed = false
     componentInternational
       .find({ type: 'text', name: 'int_extension' })
-      .simulate('keydown', { keyCode: 8, target: { value: '' } })
-    expect(tabbed).toBe(true)
-
-    tabbed = false
-    componentInternational
-      .find({ type: 'text', name: 'int_first' })
       .simulate('keydown', { keyCode: 8, target: { value: '' } })
     expect(tabbed).toBe(true)
   })
