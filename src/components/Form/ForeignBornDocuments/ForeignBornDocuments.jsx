ad97b68a994f833767c128f5ce0b9fc80cf5a4fa
Okechukwu Wyce <owyche@solielcom.com>
2019-09-05 13:50:49 -0400

    Fixed [EN-3702] Foreign Born Spouse ID Doc Can't Expire In The Future by setting noMaxDate to true

--- src/components/Form/ForeignBornDocuments/ForeignBornDocuments.jsx
+++ src/components/Form/ForeignBornDocuments/ForeignBornDocuments.jsx
@@ -55,6 +55,7 @@ export default class ForeignBornDocuments extends ValidationElement {
   }
 
   updateDocumentExpiration(values) {
+    values['noMaxDate'] = true
     this.update({
       DocumentExpiration: values,
     })
