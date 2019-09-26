ec0861afc718677a6e3fb7607b25fff292efd7e6
Suzanne Rozier <suz@truss.works>
2019-09-26 08:54:13 -0500

    Remove artificial date range from alcohol received counseling model

--- src/validators/alcoholreceivedcounseling.js
+++ src/validators/alcoholreceivedcounseling.js
@@ -2,24 +2,6 @@
 import { validateModel } from 'models/validate'
 import substanceAlcoholReceivedCounselingModel from 'models/sections/substanceAlcoholReceivedCounseling'
 
-// TODO remove
-export const validateReceivedCounselings = (data, formType, options = {}) => {
-  // We need to make actual date ranges for consistent validation
-  const modelData = { ...data }
-  if (data.List && data.List.items) {
-    const newItems = data.List.items.map(i => ({
-      ...i,
-      Item: {
-        ...i.Item,
-        TreatmentDates: {
-          from: i.Item && i.Item.TreatmentBeganDate,
-          to: i.Item && i.Item.TreatmentEndDate,
-        },
-      },
-    }))
-
-    modelData.List.items = newItems
-  }
-
-  return validateModel(modelData, substanceAlcoholReceivedCounselingModel, options)
-}
+export const validateReceivedCounselings = (data, formType, options = {}) => (
+  validateModel(data, substanceAlcoholReceivedCounselingModel, options)
+)
